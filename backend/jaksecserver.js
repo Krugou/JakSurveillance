d61ef4a252860cdc689cd0c50d68ce6415a3cb6a
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-24 21:33:23 +0300

    Create jaksecserver.js

---
+++ backend/jaksecserver.js
@@ -0,0 +1,27 @@
+'use strict';
+/* simple chat app */
+
+import express from 'express';
+import {createServer} from 'http';
+const app = express();
+const http = createServer(app);
+
+// server's adjustable settings
+const port = 3001;
+const startTime = new Date();
+
+console.log(
+	' Backend chat/frontend server start time: ' + startTime.toLocaleString()
+);
+app.use(express.static('JakSec'));
+app.get('*', (req, res) => {
+	res.sendFile('index.html', {root: 'JakSec'});
+});
+
+http.listen(port, () => {
+	console.log(
+		'JakSec backend frontend server Started at: http://localhost:' +
+			port +
+			'/index.html '
+	);
+});
