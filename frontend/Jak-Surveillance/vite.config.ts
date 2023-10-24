37c56885c27cdcbec63e2b9699ed7258e19f3b35
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-24 12:05:42 +0300

    Update vite.config.ts

--- frontend/Jak-Surveillance/vite.config.ts
+++ frontend/Jak-Surveillance/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Jak Surveillance',
+        short_name: 'JakSurveillance',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
+
