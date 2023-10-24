a5cd0585841eccf4f6aebd427380f0c9ee56a4c1
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-24 18:28:13 +0300

    Update StartView.tsx

--- frontend/Jak-Surveillance/src/views/StartView.tsx
+++ frontend/Jak-Surveillance/src/views/StartView.tsx
@@ -6,7 +6,8 @@ const StartView = () => {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mb-8">Welcome to JakSec</h1>
+            <h1 className="text-4xl font-bold mb-8">Welcome</h1>
+            <img src="/JakSec.png" alt="logo" className="w-auto h-1/4 mb-8" />
             <div className="flex flex-col items-center">
                 <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-lg text-xl mb-4"
