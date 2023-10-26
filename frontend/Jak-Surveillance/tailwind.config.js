d2cacffba31251e58fdf66a4e3e07aade722e57f
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-26 23:06:48 +0300

    Update tailwind.config.js

--- frontend/Jak-Surveillance/tailwind.config.js
+++ frontend/Jak-Surveillance/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				metropoliaMainOrange: '#ff5000',
+				metropoliaMainGrey: '#53565a',
+				metropoliaSupportWhite: '#ffffff',
+				metropoliaSupportBlack: '#000000',
+				metropoliaSupportRed: '#cb2228',
+				metropoliaSupportBlue: '#4046a8',
+				metropoliaSupportYellow: '#fff000',
+				metropoliaTrendPink: '#e384c4',
+				metropoliaTrendLightBlue: '#5db1e4',
+				metropoliaTrendGreen: '#3ba88f',
+			},
+		},
 	},
 	plugins: [],
 };
