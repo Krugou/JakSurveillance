0756f0f21863d6adcb9a5f877b8bc3438a739109
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-27 10:31:27 +0300

    Update Login.tsx

--- frontend/Jak-Surveillance/src/views/main/Login.tsx
+++ frontend/Jak-Surveillance/src/views/main/Login.tsx
@@ -64,7 +64,7 @@ const Login: React.FC<LoginProps> = ({ userType, onLogin }) => {
             <button
               className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
               type='button'
-              onClick={() => navigate(`/${userType}/mainview`)}
+              onClick={() => navigate(`/${userType.toLowerCase()}/mainview`)}
             >
               Sign In
             </button>
