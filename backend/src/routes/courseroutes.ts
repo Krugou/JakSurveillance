b7c6a75288dd659832ec1dcd66a0255cf74529aa
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-28 18:30:47 +0300

    exceljs testailua 3

--- backend/src/routes/courseroutes.ts
+++ backend/src/routes/courseroutes.ts
@@ -1,25 +1,38 @@
 import ExcelJS from 'exceljs';
 import express, { Request, Response, Router } from 'express';
 import multer from 'multer';
+
 const upload = multer();
 const router: Router = express.Router();
-router.get('/', (req: Request, res: Response) => { res.send('Hello, TypeScript with Express!'); });
+
+router.get('/', (req: Request, res: Response) => {
+    res.send('Hello, TypeScript with Express!');
+});
+
 router.post('/create', upload.single('file'), async (req, res) => {
+    console.log('Received request'); // Debugging line
+
     const { courseName, courseCode, studentGroup } = req.body;
+    console.log('Request body:', req.body); // Debugging line
 
     // Create a new workbook
     const workbook = new ExcelJS.Workbook();
 
     // Read the Excel file from the buffer
     await workbook.xlsx.load(req.file.buffer);
-
+    console.log(req.file.buffer); // Debugging line
+    console.log('Loaded workbook'); // Debugging line
+    console.log(workbook.worksheets.map(ws => ws.name));
     // Get the first worksheet
-    const worksheet = workbook.getWorksheet(1);
+    const worksheet = workbook.getWorksheet('Sheet0');
+
     if (!worksheet) {
         console.error('Worksheet not found');
         res.status(500).send('Internal server error');
         return;
     }
+    console.log('Got worksheet'); // Debugging line
+
     // Convert the worksheet to JSON
     const jsonData = worksheet.getRows(2, worksheet.rowCount).map(row => {
         let rowValue = {};
@@ -31,11 +44,12 @@ router.post('/create', upload.single('file'), async (req, res) => {
         });
         return rowValue;
     });
+    console.log('Converted worksheet to JSON'); // Debugging line
 
     console.log('Course Name:', courseName);
     console.log('Course Code:', courseCode);
     console.log('Student Group:', studentGroup);
-    console.log('File Data:', jsonData);
+    console.table(jsonData);
 
     res.status(200).send('File uploaded and data logged successfully');
 });
