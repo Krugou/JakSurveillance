11d8a61ce82ae3cb855f3299a42b329bf3b01db2
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-30 19:09:35 +0200

    Update StudentAttendance.tsx

--- frontend/src/views/main/student/StudentAttendance.tsx
+++ frontend/src/views/main/student/StudentAttendance.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 
-interface Course {
+interface Part {
     id: number;
-    name: string;
-    attendance: number;
+    course: string;
+    part: string;
+    date: string;
+    attendance: string;
+    length?: number;
 }
 
-const courses: Course[] = [
-    { id: 1, name: 'Course 1', attendance: 90 },
-    { id: 2, name: 'Course 2', attendance: 85 },
-    { id: 3, name: 'Course 3', attendance: 70 },
-    { id: 4, name: 'Course 4', attendance: 46 },
+const PartAttendance: Part[] = [
+    { id: 1, course: 'Programming', part: 'Math', date: '27.10.2023', attendance: "Present", length: 3 },
+    { id: 2, course: 'Programming', part: 'Physics', date: '28.10.2023', attendance: "Not Present", length: 3 },
+    { id: 3, course: 'Programming', part: 'English', date: '28.10.2023', attendance: "Not Present", length: 3 },
+    { id: 4, course: 'Programming', part: 'Chemistry', date: '29.10.2023', attendance: "Present", length: 3 },
     // add more courses as needed
 ];
 
-const getAttendanceColorClass = (attendance: number) => {
-    if (attendance >= 90) {
+const getAttendanceColorClass = (attendance: string) => {
+    if (attendance == "Present") {
         return 'bg-metropoliaTrendGreen';
-    } else if (attendance >= 50) {
-        return 'bg-metropoliaMainOrange';
-    } else {
+    }
+    else {
         return 'bg-metropoliaSupportRed';
     }
 };
@@ -27,11 +29,14 @@ const getAttendanceColorClass = (attendance: number) => {
 const StudentAttendance: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center h-1/2 p-8 bg-gray-100">
-            <h1 className="text-xl sm:text-4xl font-bold mb-8">Student Course Attendance</h1>
-            {courses.map((course) => (
-                <div key={course.id} className="text-md sm:text-xl mb-4">
-                    <p className="mb-5"><strong>Course Name:</strong> <span className="profileStat">{course.name}</span></p>
-                    <p className="mb-4"><strong>Attendance:</strong> <span className={`profileStat ${getAttendanceColorClass(course.attendance)}`}>{course.attendance}%</span></p>
+            <h1 className="text-xl sm:text-4xl font-bold mb-8">Part Attendance</h1>
+            {PartAttendance.map((part) => (
+                <div key={part.id} className="flex align-start flex-row text-md border border-black rounded sm:text-xl m-4 p-4">
+                    <p className="p-2 m-2"><strong>Date:</strong> <span className="profileStat">{part.date}</span></p>
+                    <p className="p-2 m-2"><strong>Course:</strong> <span className="profileStat">{part.course}</span></p>
+                    <p className="p-2 m-2"><strong>Part:</strong> <span className="profileStat">{part.part}</span></p>
+                    <p className="p-2 m-2"><strong>Length:</strong> <span className="profileStat">{part.length}h</span></p>
+                    <p className="p-2 m-2"><strong>Attendance:</strong> <span className={`profileStat ${getAttendanceColorClass(part.attendance)}`}>{part.attendance}</span></p>
                 </div>
             ))}
         </div>
