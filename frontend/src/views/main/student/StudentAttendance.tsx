46d6f0bb91881f88cccc7db600d0a84fa7366e1b
KaarleH <kaarleh@metropolia.fi>
2023-10-30 16:39:14 +0200

    studentAttendance

--- frontend/src/views/main/student/StudentAttendance.tsx
+++ frontend/src/views/main/student/StudentAttendance.tsx
@@ -9,9 +9,20 @@ interface Course {
 const courses: Course[] = [
     { id: 1, name: 'Course 1', attendance: 90 },
     { id: 2, name: 'Course 2', attendance: 85 },
+    { id: 3, name: 'Course 3', attendance: 70 },
+    { id: 4, name: 'Course 4', attendance: 46 },
     // add more courses as needed
 ];
 
+const getAttendanceColorClass = (attendance: number) => {
+    if (attendance >= 90) {
+        return 'bg-metropoliaTrendGreen';
+    } else if (attendance >= 50) {
+        return 'bg-metropoliaMainOrange';
+    } else {
+        return 'bg-metropoliaSupportRed';
+    }
+};
 
 const StudentAttendance: React.FC = () => {
     return (
@@ -20,7 +31,7 @@ const StudentAttendance: React.FC = () => {
             {courses.map((course) => (
                 <div key={course.id} className="text-md sm:text-xl mb-4">
                     <p className="mb-5"><strong>Course Name:</strong> <span className="profileStat">{course.name}</span></p>
-                    <p className="mb-4"><strong>Attendance:</strong> <span className="profileStat">{course.attendance}%</span></p>
+                    <p className="mb-4"><strong>Attendance:</strong> <span className={`profileStat ${getAttendanceColorClass(course.attendance)}`}>{course.attendance}%</span></p>
                 </div>
             ))}
         </div>
