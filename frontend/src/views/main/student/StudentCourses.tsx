137faeb3a3bae4e8b9f8bd033d854924831759ae
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-30 19:15:54 +0200

    Update StudentCourses.tsx

--- frontend/src/views/main/student/StudentCourses.tsx
+++ frontend/src/views/main/student/StudentCourses.tsx
@@ -32,13 +32,19 @@ const getAttendanceColorClass = (attendance: number) => {
 const StudentCourses: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center h-1/2 p-8 bg-gray-100">
-            <h1 className="text-xl sm:text-4xl font-bold mb-8">Student Course Attendance</h1>
-            {courses.map((course) => (
-                <div key={course.id} className=" flex flex-row text-md sm:text-xl mb-4">
-                    <p className="mb-5"><strong>Course Name:</strong> <span className="profileStat">{course.name}</span></p>
-                    <p className="mb-4"><strong>Attendance:</strong> <span className={`profileStat ${getAttendanceColorClass(course.attendance)}`}>{course.attendance}%</span></p>
-                </div>
-            ))}
+            <h1 className="text-2xl sm:text-5xl font-bold mb-8 text-center">Student Course Attendance</h1>
+            <div className='flex flex-row'>
+                {courses.map((course) => (
+                    <div key={course.id} className="w-full max-w-md p-6 m-2 bg-white shadow-md rounded-lg">
+                        <h2 className="text-xl font-bold mb-2 text-indigo-600">{course.name}</h2>
+                        <p className="mb-1"><strong>Start Date:</strong> {course.startDate}</p>
+                        <p className="mb-1"><strong>End Date:</strong> {course.endDate}</p>
+                        <p className="mb-1"><strong>Code:</strong> {course.code}</p>
+                        <p className="mb-1"><strong>Student Group ID:</strong> {course.studentGroupId}</p>
+                        <p className="mb-1"><strong>Attendance:</strong> <span className={`inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ${getAttendanceColorClass(course.attendance)}`}>{course.attendance}%</span></p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
