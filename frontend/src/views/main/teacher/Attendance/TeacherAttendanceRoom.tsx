bda3f3997a5008b43feacbf1e724bd090489325f
KaarleH <kaarleh@metropolia.fi>
2023-10-31 12:31:29 +0200

    attendanceRoom

---
+++ frontend/src/views/main/teacher/Attendance/TeacherAttendanceRoom.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const AttendanceRoom: React.FC = () => {
+
+    return (
+        <div className="flex flex-col items-center justify-center h-1/2 p-10 bg-gray-100">
+            <h1 className="text-xl sm:text-4xl font-bold mb-8 mt-5">Student Profile</h1>
+            <div className="text-md sm:text-xl mb-4">
+            </div>
+        </div>
+    );
+};
+
+export default AttendanceRoom;
