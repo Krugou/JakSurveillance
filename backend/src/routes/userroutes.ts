aa2b57e38328af52a4df7d8361f2448bc7be812f
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-01 23:43:52 +0200

    Update userroutes.ts

--- backend/src/routes/userroutes.ts
+++ backend/src/routes/userroutes.ts
@@ -7,14 +7,14 @@ const router: Router = express.Router();
 
 // Define your route handlers with TypeScript types
 router.get('/', (_req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express! this is users route calling');
+  res.send('Hello, TypeScript with Express! This is the users route calling');
 });
 
 router.post('/', async (req: Request, res: Response) => {
   // Get username and password from the request body
   const { username, password } = req.body;
 
-  // if the user is admin return the admin account
+  // if the user is admin, return the admin account
   if (username === process.env.devaccount && password === process.env.devpass) {
     res.json({
       staff: true,
@@ -26,50 +26,6 @@ router.post('/', async (req: Request, res: Response) => {
     return;
   }
 
-  /*
-  if (username === 'admin' && password === 'admin') {
-    res.json({
-      "staff": true,
-      "user": "admin",
-      "firstname": "Admin",
-      "lastname": "Admin",
-      "email": "admin@metropolia.fi"
-    });
-  } else if (username === 'teacher' && password === 'teacher') {
-    res.json({
-      "staff": true,
-      "user": "teacher",
-      "firstname": "Teacher",
-      "lastname": "Teacher",
-      "email": "teacher@metropolia.fi"
-    });
-  } else if (username === 'counselor' && password === 'counselor') {
-    res.json({
-      "staff": true,
-      "user": "counselor",
-      "firstname": "Counselor",
-      "lastname": "Counselor",
-      "email": "counselor@metropolia.fi"
-    });
-  } else if (username === 'student' && password === 'student') {
-    res.json({
-      "staff": false,
-      "user": "student",
-      "firstname": "Student",
-      "lastname": "Student",
-      "email": "student@metropolia.fi"
-    });
-  } else if(username === 'mranderson'&& password === 'student') {
-    res.json({
-      "staff": false,
-      "user": "MrAnderson",
-      "firstname": "Mr",
-      "lastname": "Anderson",
-      "email": "mr.anderson@example.com"
-    });
-  } else {
-    */
-
   const options = {
     method: 'POST',
     headers: {
@@ -78,27 +34,6 @@ router.post('/', async (req: Request, res: Response) => {
     body: JSON.stringify({ username, password }),
   };
 
-  // TRY TO FIND USER FROM DATABASE FIRST
-
-  try {
-    console.log('🚀 ~ file: userroutes.ts:72 ~ router.post ~ userInfo');
-    const userInfo = await usermodel.getAllUserInfo(username);
-
-    if (userInfo) {
-      console.log('User information:', userInfo);
-    } else {
-      console.log('User not found.');
-      return res.status(403).json({
-        error: 'User has not been added to any courses, contact your teacher',
-      });
-    }
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error('Database error:', error.message);
-    } else {
-      // Handle the case where error is not an Error object
-    }
-  }
   interface ResponseData {
     message: string;
     staff: boolean;
@@ -106,14 +41,11 @@ router.post('/', async (req: Request, res: Response) => {
     firstname: string;
     lastname: string;
     email: string;
-    
-    // Add other properties as needed
   }
-  // TRY TO FIND USER IN METROPOLIA DATABASE
+
   try {
     const response = await fetch(loginUrl, options);
-
-    const responseData = await response.json() as ResponseData;;
+    const responseData = (await response.json()) as ResponseData;
 
     if (responseData.message === 'invalid username or password') {
       return res.status(403).json({
@@ -125,10 +57,30 @@ router.post('/', async (req: Request, res: Response) => {
         error: 'Failed to connect to Metropolia servers',
       });
     }
-    res.json(responseData);
 
-    // if logged in user is metropolia staff
-    if (responseData.staff) {
+    // TRY TO FIND USER FROM DATABASE
+    let userInfo;
+    try {
+      userInfo = await usermodel.getAllUserInfo(username);
+
+      if (userInfo) {
+        console.log('User information:', userInfo);
+      } else if (!userInfo && responseData.staff === false) {
+        console.log('User not found.');
+        return res.status(403).json({
+          error: 'User has not been added to any courses, contact your teacher',
+        });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Database error:', error.message);
+      } else {
+        // Handle the case where error is not an Error object
+      }
+    }
+
+    // If the logged-in user is Metropolia staff and they don't exist in the DB yet, add them to the DB
+    if (responseData.staff === true && !userInfo) {
       try {
         const userData = {
           username: responseData.user,
@@ -145,11 +97,12 @@ router.post('/', async (req: Request, res: Response) => {
         res.status(500).json({ error: 'Internal server error' });
       }
     }
+
+    res.json(responseData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
-  //}
 });
 
 export default router;
