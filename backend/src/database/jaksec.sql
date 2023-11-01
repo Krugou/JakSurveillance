c666824747c946df84c0a5f30bbcaca11325b156
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-01 15:59:28 +0200

    topicgroups

--- backend/src/database/jaksec.sql
+++ backend/src/database/jaksec.sql
@@ -104,17 +104,15 @@ CREATE TABLE IF NOT EXISTS `roles` (
   `roleid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   PRIMARY KEY (`roleid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Inserting data into jaksec.roles
-INSERT INTO roles ( name) VALUES
-('student'),
-('counselor'),
-('teacher'),
-('admin');
-
--- Dumping data for table jaksec.roles: ~0 rows (suunnilleen)
+-- Dumping data for table jaksec.roles: ~4 rows (suunnilleen)
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` (`roleid`, `name`) VALUES
+	(1, 'student'),
+	(2, 'counselor'),
+	(3, 'teacher'),
+	(4, 'admin');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 
 -- Dumping structure for taulu jaksec.studentgroups
@@ -128,6 +126,17 @@ CREATE TABLE IF NOT EXISTS `studentgroups` (
 /*!40000 ALTER TABLE `studentgroups` DISABLE KEYS */;
 /*!40000 ALTER TABLE `studentgroups` ENABLE KEYS */;
 
+-- Dumping structure for taulu jaksec.topicgroups
+CREATE TABLE IF NOT EXISTS `topicgroups` (
+  `topicgroupid` int(11) NOT NULL,
+  `topicgroupname` varchar(100) NOT NULL,
+  PRIMARY KEY (`topicgroupid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table jaksec.topicgroups: ~0 rows (suunnilleen)
+/*!40000 ALTER TABLE `topicgroups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `topicgroups` ENABLE KEYS */;
+
 -- Dumping structure for taulu jaksec.topics
 CREATE TABLE IF NOT EXISTS `topics` (
   `topicid` int(11) NOT NULL AUTO_INCREMENT,
@@ -135,16 +144,23 @@ CREATE TABLE IF NOT EXISTS `topics` (
   PRIMARY KEY (`topicid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `topicgroup` (
-  `topicgroupid` int(11) NOT NULL AUTO_INCREMENT,
-  `topicgroupname` varchar(64) NOT NULL,
-  `topicid` int(11) NOT NULL,
-  PRIMARY KEY (`topicgroupid`),
-  FOREIGN KEY (`topicid`) REFERENCES `topics`(`topicid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 -- Dumping data for table jaksec.topics: ~0 rows (suunnilleen)
 /*!40000 ALTER TABLE `topics` DISABLE KEYS */;
 /*!40000 ALTER TABLE `topics` ENABLE KEYS */;
+
+-- Dumping structure for taulu jaksec.topicsingroup
+CREATE TABLE IF NOT EXISTS `topicsingroup` (
+  `topicgroupid` int(11) NOT NULL,
+  `topicid` int(11) NOT NULL,
+  KEY `topicgroupid` (`topicgroupid`),
+  KEY `topicid` (`topicid`),
+  CONSTRAINT `topicsingroup_ibfk_1` FOREIGN KEY (`topicgroupid`) REFERENCES `topicgroups` (`topicgroupid`),
+  CONSTRAINT `topicsingroup_ibfk_2` FOREIGN KEY (`topicid`) REFERENCES `topics` (`topicid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table jaksec.topicsingroup: ~0 rows (suunnilleen)
+/*!40000 ALTER TABLE `topicsingroup` DISABLE KEYS */;
+/*!40000 ALTER TABLE `topicsingroup` ENABLE KEYS */;
 
 -- Dumping structure for taulu jaksec.usercourses
 CREATE TABLE IF NOT EXISTS `usercourses` (
@@ -187,31 +203,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_at` timestamp NOT NULL,
   `studentnumber` int(11) DEFAULT NULL,
   `studentgroupid` int(11) DEFAULT NULL,
-  `roleid` int(11) NOT NULL DEFAULT '1',
-  `GDPR` int(11) NOT NULL DEFAULT '0',
+  `roleid` int(11) NOT NULL DEFAULT 1,
+  `GDPR` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`userid`),
   KEY `studentgroupid` (`studentgroupid`),
   KEY `roleid` (`roleid`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`studentgroupid`) REFERENCES `studentgroups` (`studentgroupid`),
   CONSTRAINT `users_ibfk_2` FOREIGN KEY (`roleid`) REFERENCES `roles` (`roleid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Inserting data into jaksec.users
-INSERT INTO `users` (`username`, `email`, `staff`, `first_name`, `last_name`, `created_at`, `studentnumber`, `studentgroupid`, `roleid`, `GDPR`) 
-VALUES ('MrAnderson', 'mr.anderson@example.com', 1, 'Mr', 'Anderson', NOW(), NULL, NULL, 4, 1);
--- Dumping data for table jaksec.users: ~0 rows (suunnilleen)
+-- Dumping data for table jaksec.users: ~6 rows (suunnilleen)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`userid`, `username`, `email`, `staff`, `first_name`, `last_name`, `created_at`, `studentnumber`, `studentgroupid`, `roleid`, `GDPR`) VALUES
+	(1, 'MrAnderson', 'mr.anderson@example.com', 1, 'Mr', 'Anderson', NOW(), NULL, NULL, 4, 1),
+	(2, 'admin', 'admin@metropolia.fi', 1, 'Admin', 'Admin', NOW(), NULL, NULL, 4, 1),
+	(3, 'teacher', 'teacher@metropolia.fi', 1, 'Teacher', 'Teacher', NOW(), NULL, NULL, 3, 1),
+	(4, 'counselor', 'counselor@metropolia.fi', 1, 'Counselor', 'Counselor', NOW(), NULL, NULL, 2, 1),
+	(5, 'student', 'student@metropolia.fi', 0, 'Student', 'Student', NOW(), NULL, NULL, 1, 1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
-
--- Test users
-INSERT INTO `users` (`username`, `email`, `staff`, `first_name`, `last_name`, `created_at`, `roleid`, `GDPR`) 
-VALUES 
-('admin', 'admin@metropolia.fi', 1, 'Admin', 'Admin', NOW(), 4, 1),
-('teacher', 'teacher@metropolia.fi', 1, 'Teacher', 'Teacher', NOW(), 3, 1),
-('counselor', 'counselor@metropolia.fi', 1, 'Counselor', 'Counselor', NOW(), 2, 1),
-('student', 'student@metropolia.fi', 0, 'Student', 'Student', NOW(),1, 1);
