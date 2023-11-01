1959e0cd96168bcf85c0c9154ead8707c4c1325c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-01 13:37:09 +0200

    add

--- extra/generateScores.js
+++ extra/generateScores.js
@@ -2,20 +2,31 @@ import {Octokit} from '@octokit/rest';
 import dotenv from 'dotenv';
 import fs from 'fs';
 dotenv.config();
+const owner = 'Krugou'; // replace with repository owner
+const repo = 'JakSurveillance'; // replace with repository name
 // console.log(process.env.GITHUBTOKEN);
 const octokit = new Octokit({auth: process.env.GITHUBTOKEN});
+const getContributorsStats = async () => {
+	while (true) {
+		const response = await octokit.rest.repos.getContributorsStats({
+			owner,
+			repo,
+		});
 
+		if (response.status === 200 && Array.isArray(response.data)) {
+			return response.data;
+		}
+
+		console.log('Waiting for stats to be ready...');
+		await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for 5 seconds before retrying
+	}
+};
 const generateScores = async () => {
-	const owner = 'Krugou'; // replace with repository owner
-	const repo = 'JakSurveillance'; // replace with repository name
-
-	const contributors = await octokit.rest.repos.getContributorsStats({
-		owner,
-		repo,
-	});
+	const contributors = await getContributorsStats();
 	console.log(contributors);
 	const scores = {};
 
+	// @ts-ignore
 	for (const contributor of contributors.data) {
 		console.log('Processing contributor:', contributor.author.login);
 
