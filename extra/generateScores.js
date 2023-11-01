2d2cbb87991686814005b0dfb28b8483b2d2ded1
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-01 13:22:14 +0200

    github actions test

---
+++ extra/generateScores.js
@@ -0,0 +1,43 @@
+import {Octokit} from '@octokit/rest';
+import dotenv from 'dotenv';
+import fs from 'fs';
+dotenv.config();
+// console.log(process.env.GITHUBTOKEN);
+const octokit = new Octokit({auth: process.env.GITHUBTOKEN});
+
+const generateScores = async () => {
+	const owner = 'Krugou'; // replace with repository owner
+	const repo = 'JakSurveillance'; // replace with repository name
+
+	const contributors = await octokit.rest.repos.getContributorsStats({
+		owner,
+		repo,
+	});
+
+	const scores = {};
+
+	for (const contributor of contributors.data) {
+		const login = contributor.author.login;
+		const commits = contributor.total;
+		const additions = contributor.weeks.reduce(
+			(total, week) => total + week.a,
+			0
+		);
+		const deletions = contributor.weeks.reduce(
+			(total, week) => total + week.d,
+			0
+		);
+		const totalChanges = additions - deletions;
+
+		scores[login] = {
+			commits: commits,
+			additions: additions,
+			deletions: deletions,
+			totalChanges: totalChanges,
+		};
+	}
+
+	fs.writeFileSync('scores.json', JSON.stringify(scores, null, 2));
+};
+
+generateScores().catch(console.error);
