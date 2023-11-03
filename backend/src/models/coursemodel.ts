14e1072cd89c76fd030115219449710c111eebef
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-03 18:17:11 +0200

    Refactor course model interface and methods

--- backend/src/models/coursemodel.ts
+++ backend/src/models/coursemodel.ts
@@ -3,36 +3,54 @@ import pool from '../database/db.js';
 
 interface Course {
 	courseid: number;
-	coursename: string;
+	name: string;
+	start_date: Date;
+	created_at: Date;
+	end_date: Date;
+	code: string;
+	studentgroupid: number | null;
+	// other fields...
 }
 
 interface CourseModel {
-	fetchAll(): Promise<[RowDataPacket[], FieldPacket[]]>;
-	findById(id: number): Promise<Course | null>;
-	findByTeacherId(teacherId: number): Promise<[RowDataPacket[], FieldPacket[]]>;
+	fetchAllCourses(): Promise<[RowDataPacket[], FieldPacket[]]>;
+	findByCourseId(id: number): Promise<Course | null>;
+	insertIntoCourse(
+		name: string,
+		start_date: Date,
+		end_date: Date,
+		code: string,
+		studentgroupid: number | null,
+	): Promise<void>;
+	deleteByCourseId(id: number): Promise<void>;
+	// other methods...
 }
 
 const Course: CourseModel = {
-	fetchAll() {
+	fetchAllCourses() {
 		return pool.promise().query<RowDataPacket[]>('SELECT * FROM courses');
 	},
 
-	async findById(id) {
+	async findByCourseId(id) {
 		const [rows] = await pool
 			.promise()
 			.query<RowDataPacket[]>('SELECT * FROM courses WHERE courseid = ?', [id]);
 		return (rows[0] as Course) || null;
 	},
 
-	findByTeacherId(teacherId) {
-		return pool.promise().query<RowDataPacket[]>(
-			`SELECT courses.* 
-       FROM courses 
-       JOIN courseinstructors ON courses.courseid = courseinstructors.courseid 
-       WHERE courseinstructors.userid = ?`,
-			[teacherId]
-		);
+	async insertIntoCourse(name, start_date, end_date, code, studentgroupid) {
+		await pool
+			.promise()
+			.query(
+				'INSERT INTO courses (name, start_date, end_date, code, studentgroupid) VALUES (?, ?, ?, ?, ?)',
+				[name, start_date, end_date, code, studentgroupid],
+			);
 	},
+
+	async deleteByCourseId(id) {
+		await pool.promise().query('DELETE FROM courses WHERE courseid = ?', [id]);
+	},
+	// other methods...
 };
 
 export default Course;
