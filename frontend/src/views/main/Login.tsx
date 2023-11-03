a63d30d31e0651e1e78b80100f84e475274bf1f3
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-03 19:56:27 +0200

    Update Login.tsx

--- frontend/src/views/main/Login.tsx
+++ frontend/src/views/main/Login.tsx
@@ -1,101 +1,100 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import ErrorAlert from '../../components/main/ErrorAlert.tsx';
 import apiHooks from '../../hooks/ApiHooks.ts';
-
+import {UserContext} from '../../contexts/UserContext.tsx';
 interface LoginProps {
-  userType: 'Student' | 'Teacher' | 'Admin' | 'Counselor';
-
+	userType: 'Student' | 'Teacher' | 'Admin' | 'Counselor';
 }
 
-const Login: React.FC<LoginProps> = ({ userType }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [alert, setAlert] = useState<string>('');
-  const navigate = useNavigate();
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
+const Login: React.FC<LoginProps> = ({userType}) => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [alert, setAlert] = useState<string>('');
+	const navigate = useNavigate();
+	const handleSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
 
-    console.log(userType, username, password);
-    const inputs = { username, password };
+		console.log(userType, username, password);
+		const inputs = {username, password};
 
-    try {
-      const response = await apiHooks.postLogin(inputs);
-      // this navigates to the mainview of the user type
-      if (response) {
-        navigate(`/${userType.toLowerCase()}/mainview`);
-      }
-      console.log(response, 'LOGIN RESPONSE');
-    } catch (error) {
-      setAlert(error.message);
-      console.log(error);
-    }
+		try {
+			const response = await apiHooks.postLogin(inputs);
+			// this navigates to the mainview of the user type
+			if (response) {
+				navigate(`/${userType.toLowerCase()}/mainview`);
+			}
+			console.log(response, 'LOGIN RESPONSE');
+		} catch (error) {
+			setAlert(error.message);
+			console.log(error);
+		}
 
-    console.log('🚀 ~ file: App.tsx:41 ~ App ~ inputs:', inputs);
-  };
+		console.log('🚀 ~ file: App.tsx:41 ~ App ~ inputs:', inputs);
+	};
 
-  //const navigate = useNavigate();
-  return (
-    <div className='flex justify-center items-center h-1/2'>
-      {alert && <ErrorAlert onClose={() => setAlert(null)} alert={alert} />}
-      <div className='md:w-1/2 w-2/3'>
-        <h1 className='sm:text-2xl text-xl font-bold mb-4 mt-4 sm:mt-0 text-center'>
-          {userType} Login
-        </h1>
-        <form
-          onSubmit={handleSubmit}
-          className='bg-white md:w-3/4 xl:w-1/2 w-full shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-auto'
-        >
-          <div className='mb-4'>
-            <label
-              className='block text-gray-700 text-sm font-bold mb-2'
-              htmlFor='username'
-            >
-              {userType} Username
-            </label>
-            <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-              id='username'
-              type='text'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className='mb-6'>
-            <label
-              className='block text-gray-700 text-sm font-bold mb-2'
-              htmlFor='password'
-            >
-              {userType} Password
-            </label>
-            <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
-              id='password'
-              type='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className='flex items-center justify-between'>
-            {/* <button
+	//const navigate = useNavigate();
+	return (
+		<div className="flex justify-center items-center h-1/2">
+			{alert && <ErrorAlert onClose={() => setAlert(null)} alert={alert} />}
+			<div className="md:w-1/2 w-2/3">
+				<h1 className="sm:text-2xl text-xl font-bold mb-4 mt-4 sm:mt-0 text-center">
+					{userType} Login
+				</h1>
+				<form
+					onSubmit={handleSubmit}
+					className="bg-white md:w-3/4 xl:w-1/2 w-full shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-auto"
+				>
+					<div className="mb-4">
+						<label
+							className="block text-gray-700 text-sm font-bold mb-2"
+							htmlFor="username"
+						>
+							{userType} Username
+						</label>
+						<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="username"
+							type="text"
+							value={username}
+							onChange={e => setUsername(e.target.value)}
+						/>
+					</div>
+					<div className="mb-6">
+						<label
+							className="block text-gray-700 text-sm font-bold mb-2"
+							htmlFor="password"
+						>
+							{userType} Password
+						</label>
+						<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+							id="password"
+							type="password"
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+						/>
+					</div>
+					<div className="flex items-center justify-between">
+						{/* <button
               className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
               type='submit'
             >
               Sign In
             </button> */}
-            <button
-              className='bg-metropoliaMainOrange hover:bg-metropoliaSecondaryOrange text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-              type='button'
-              //  onClick={() => navigate(`/${userType.toLowerCase()}/mainview`)}
-              onClick={handleSubmit}
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+						<button
+							className="bg-metropoliaMainOrange hover:bg-metropoliaSecondaryOrange text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+							type="button"
+							//  onClick={() => navigate(`/${userType.toLowerCase()}/mainview`)}
+							onClick={handleSubmit}
+						>
+							Sign In
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
