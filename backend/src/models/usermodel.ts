0d65224bd654772ccac403d8e9b8e8b47f105ac1
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-04 00:10:31 +0200

    Reorder imports and add new methods to UserModel

--- backend/src/models/usermodel.ts
+++ backend/src/models/usermodel.ts
@@ -1,7 +1,11 @@
-import {ResultSetHeader, FieldPacket, RowDataPacket} from 'mysql2';
+import {FieldPacket, ResultSetHeader, RowDataPacket} from 'mysql2';
 
 import pool from '../database/db.js'; // Adjust the path to your pool file
 
+/**
+ * @interface UserInfo
+ * @description Defines the structure of the UserInfo object.
+ */
 // Define the structure of the UserInfo object
 interface UserInfo {
 	username: string;
@@ -10,13 +14,29 @@ interface UserInfo {
 	first_name: string;
 	last_name: string;
 }
-
-// Create a User Model object literal
+/**
+ * @interface User
+ * @description Defines the structure of the User object.
+ */
+interface User {
+	username: string;
+	email: string;
+	staff: number;
+	first_name: string;
+	last_name: string;
+}
+/**
+ * @description Creates a User Model object literal.
+ */
 const UserModel = {
 	pool: pool,
 
-	// A method to retrieve user information based on a username
-
+	/**
+	 * @method getAllUserInfo
+	 * @description A method to retrieve user information based on a username.
+	 * @param {string} username - The username of the user.
+	 * @returns {Promise<UserInfo | null>} A promise that resolves to the user's information or null if the user is not found.
+	 */
 	getAllUserInfo: async (username: string): Promise<UserInfo | null> => {
 		try {
 			const [rows] = await UserModel.pool
@@ -36,7 +56,12 @@ const UserModel = {
 		}
 	},
 
-	// For example, a method to update user information
+	/**
+	 * Updates the email of a user.
+	 * @param {number} userId - The ID of the user to update.
+	 * @param {string} newEmail - The new email to set for the user.
+	 * @returns {Promise<boolean>} A promise that resolves to a boolean indicating whether the update was successful.
+	 */
 	updateUserInfo: async (
 		userId: number,
 		newEmail: string,
@@ -53,7 +78,11 @@ const UserModel = {
 			throw new Error('Database error');
 		}
 	},
-
+	/**
+	 * Adds a new user to the database.
+	 * @param {User} user - The user to add.
+	 * @returns {Promise<boolean>} A promise that resolves to a boolean indicating whether the addition was successful.
+	 */
 	addUser: async (user: User): Promise<boolean> => {
 		try {
 			const {username, email, staff, first_name, last_name} = user;
@@ -69,15 +98,60 @@ const UserModel = {
 			throw new Error('Database error');
 		}
 	},
-};
+	/**
+	 * Deletes a user by their ID.
+	 * @param userId The ID of the user to delete.
+	 * @returns A promise that resolves to a boolean indicating whether the deletion was successful.
+	 */
+	async deleteUser(userId: number): Promise<boolean> {
+		try {
+			const [rows] = (await UserModel.pool.execute(
+				'DELETE FROM users WHERE Userid = ?',
+				[userId],
+			)) as unknown as [ResultSetHeader, FieldPacket[]];
 
-// Define the structure of the UserInfo object
-interface User {
-	username: string;
-	email: string;
-	staff: number;
-	first_name: string;
-	last_name: string;
-}
+			return rows.affectedRows > 0;
+		} catch (error) {
+			console.error(error);
+			throw new Error('Database error');
+		}
+	},
+	/**
+	 * Finds all users with a certain staff status.
+	 * @param staff The staff status to search for.
+	 * @returns A promise that resolves to an array of users with the given staff status.
+	 */
+	async findUsersByStaffStatus(staff: number): Promise<UserInfo[]> {
+		try {
+			const [rows] = await UserModel.pool
+				.promise()
+				.query<RowDataPacket[]>('SELECT * FROM users WHERE staff = ?', [staff]);
+
+			return rows as UserInfo[];
+		} catch (error) {
+			console.error(error);
+			throw new Error('Database error');
+		}
+	},
+	/**
+	 * Checks if a username already exists in the database.
+	 * @param username The username to check.
+	 * @returns A promise that resolves to a boolean indicating whether the username exists.
+	 */
+	async checkUsernameExists(username: string): Promise<boolean> {
+		try {
+			const [rows] = await UserModel.pool
+				.promise()
+				.query<RowDataPacket[]>('SELECT * FROM users WHERE username = ?', [
+					username,
+				]);
+
+			return rows.length > 0;
+		} catch (error) {
+			console.error(error);
+			throw new Error('Database error');
+		}
+	},
+};
 
 export default UserModel;
