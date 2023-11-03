81216c915cc321bc8d216544f9a3a0641b9f8ca6
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-03 13:33:33 +0200

    fix

--- backend/src/models/usermodel.ts
+++ backend/src/models/usermodel.ts
@@ -1,6 +1,7 @@
-import {ResultSetHeader, FieldPacket} from 'mysql2';
+import {ResultSetHeader, FieldPacket, RowDataPacket} from 'mysql2';
 
 import pool from '../database/db.js'; // Adjust the path to your pool file
+
 // Define the structure of the UserInfo object
 interface UserInfo {
 	username: string;
@@ -9,17 +10,20 @@ interface UserInfo {
 	first_name: string;
 	last_name: string;
 }
+
 // Create a User Model object literal
 const UserModel = {
 	pool: pool,
 
 	// A method to retrieve user information based on a username
+
 	getAllUserInfo: async (username: string): Promise<UserInfo | null> => {
 		try {
-			const [rows] = await UserModel.pool.execute<RowDataPacket[]>(
-				'SELECT * FROM users WHERE Username = ?',
-				[username]
-			);
+			const [rows] = await UserModel.pool
+				.promise()
+				.query<RowDataPacket[]>('SELECT * FROM users WHERE Username = ?', [
+					username,
+				]);
 
 			if (rows.length > 0) {
 				return rows.pop() as UserInfo;
@@ -27,6 +31,7 @@ const UserModel = {
 				return null;
 			}
 		} catch (error) {
+			console.error(error);
 			throw new Error('Database error');
 		}
 	},
@@ -34,16 +39,17 @@ const UserModel = {
 	// For example, a method to update user information
 	updateUserInfo: async (
 		userId: number,
-		newEmail: string
+		newEmail: string,
 	): Promise<boolean> => {
 		try {
 			const [rows] = (await UserModel.pool.execute(
 				'UPDATE users SET Useremail = ? WHERE Userid = ?',
-				[newEmail, userId]
+				[newEmail, userId],
 			)) as unknown as [ResultSetHeader, FieldPacket[]];
 
 			return rows.affectedRows > 0;
 		} catch (error) {
+			console.error(error);
 			throw new Error('Database error');
 		}
 	},
@@ -54,18 +60,18 @@ const UserModel = {
 
 			const [rows] = (await UserModel.pool.execute(
 				'INSERT INTO users (username, email, staff, first_name, last_name) VALUES (?, ?, ?, ?, ?)',
-				[username, email, staff, first_name, last_name]
+				[username, email, staff, first_name, last_name],
 			)) as unknown as [ResultSetHeader, FieldPacket[]];
 
 			return rows.affectedRows > 0;
 		} catch (error) {
+			console.error(error);
 			throw new Error('Database error');
 		}
 	},
 };
 
 // Define the structure of the UserInfo object
-
 interface User {
 	username: string;
 	email: string;
