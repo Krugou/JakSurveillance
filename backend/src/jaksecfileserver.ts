363162c9d671c0a381f2f7c0b7a6218079e87ceb
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-04 00:21:22 +0200

    docs

--- backend/src/jaksecfileserver.ts
+++ backend/src/jaksecfileserver.ts
@@ -1,23 +1,42 @@
-import express, { Request, Response } from 'express';
-import { Server, createServer } from 'http';
+import express, {Request, Response} from 'express';
+import {Server, createServer} from 'http';
 
+/**
+ * Initialize Express application
+ */
 const app = express();
+
+/**
+ * Create HTTP server
+ */
 const http: Server = createServer(app);
 
-// server's adjustable settings
+// Server's adjustable settings
 const port: number = 3001;
 const startTime: Date = new Date();
 
+/**
+ * Use static middleware for Express to serve static files from 'JakSec' directory
+ */
 app.use(express.static('JakSec'));
+
+/**
+ * Setup route to serve index.html for all unmatched routes
+ */
 app.get('*', (_req: Request, res: Response) => {
-  res.sendFile('index.html', { root: 'JakSec' });
+	res.sendFile('index.html', {root: 'JakSec'});
 });
 
+/**
+ * Start the server
+ */
 http.listen(port, () => {
-  console.log(
-    'JakSec FILE SERVER started at: http://localhost:' +
-    port +
-    '/index.html start time:' + startTime.toLocaleString()
-  );
+	console.log(
+		'JakSec FILE SERVER started at: http://localhost:' +
+			port +
+			'/index.html start time:' +
+			startTime.toLocaleString(),
+	);
 });
+
 export default app;
