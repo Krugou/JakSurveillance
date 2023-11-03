802cae191d3f8bd5141b9e877a26a3b44a5ca290
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-03 19:54:52 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { useRegisterSW } from 'virtual:pwa-register/react';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {useRegisterSW} from 'virtual:pwa-register/react';
 import AdminRoutes from './routes/AdminRoutes';
 import CounselorRoutes from './routes/CounselorRoutes';
 import StudentRoutes from './routes/StudentRoutes';
@@ -8,54 +8,40 @@ import TeacherRoutes from './routes/TeacherRoutes';
 import Footer from './views/Footer.tsx';
 import Header from './views/Header.tsx';
 import StartView from './views/main/StartView.tsx';
-
+import {UserProvider} from './contexts/UserContext.tsx';
 const intervalMS = 60 * 60 * 1000;
 
 const App = () => {
+	useRegisterSW({
+		onRegistered(r) {
+			if (r) {
+				console.log('Service worker registered successfully');
+				setInterval(() => {
+					r.update();
+				}, intervalMS);
+			} else {
+				console.log('Service worker registration failed');
+			}
+		},
+	});
 
-
-  useRegisterSW({
-    onRegistered(r) {
-      if (r) {
-        console.log('Service worker registered successfully');
-        setInterval(() => {
-          r.update();
-        }, intervalMS);
-      } else {
-        console.log('Service worker registration failed');
-      }
-    },
-  });
-
-  return (
-    <Router basename={import.meta.env.BASE_URL}>
-      <Header title='Attendance App' />
-      <main>
-
-
-        <Routes>
-          <Route path='/' element={<StartView />} />
-          <Route
-            path='student/*'
-            element={<StudentRoutes />}
-          />
-          <Route
-            path='admin/*'
-            element={<AdminRoutes />}
-          />
-          <Route
-            path='counselor/*'
-            element={<CounselorRoutes />}
-          />
-          <Route
-            path='teacher/*'
-            element={<TeacherRoutes />}
-          />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
-  );
+	return (
+		<UserProvider>
+			<Router basename={import.meta.env.BASE_URL}>
+				<Header title="Attendance App" />
+				<main>
+					<Routes>
+						<Route path="/" element={<StartView />} />
+						<Route path="student/*" element={<StudentRoutes />} />
+						<Route path="admin/*" element={<AdminRoutes />} />
+						<Route path="counselor/*" element={<CounselorRoutes />} />
+						<Route path="teacher/*" element={<TeacherRoutes />} />
+					</Routes>
+				</main>
+				<Footer />
+			</Router>
+		</UserProvider>
+	);
 };
 
 export default App;
