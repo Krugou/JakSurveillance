5e675f0509eb9c4c9a42151b609bf7222cb0df55
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-06 13:40:23 +0200

    Add multi-step form for creating courses

--- frontend/src/views/main/teacher/Courses/TeacherCreateCourse.tsx
+++ frontend/src/views/main/teacher/Courses/TeacherCreateCourse.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import apiHooks from '../../../../hooks/ApiHooks';
 // this is view for teacher to create the course
 const TeacherCreateCourse: React.FC = () => {
+	const [currentStep, setCurrentStep] = useState(1);
 	const [courseName, setCourseName] = useState('');
 	const [file, setFile] = useState<File | null>(null);
 	const [courseTopics, setCourseTopics] = useState<string[]>([]);
@@ -111,100 +112,146 @@ const TeacherCreateCourse: React.FC = () => {
 				onSubmit={handleSubmit}
 				className="w-full max-w-md mx-auto bg-white p-6 rounded shadow-md"
 			>
-				<input
-					type="text"
-					placeholder="Course Name"
-					value={courseName}
-					onChange={e => setCourseName(e.target.value)}
-					className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-					title="Enter the course name here example: Mediapalvelut-projekti"
-				/>
-				<input
-					type="text"
-					placeholder="Course Code"
-					value={courseCode}
-					onChange={e => setCourseCode(e.target.value)}
-					className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-					title="Enter the course code here example: TX00CG61-3009"
-				/>
-				<input
-					type="text"
-					placeholder="Student Group"
-					value={studentGroup}
-					onChange={e => setStudentGroup(e.target.value)}
-					className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-					title="Enter the student group here example: tvt21-m"
-				/>
-				<select
-					title="Select Course Topic Group"
-					value={courseTopicGroup}
-					onChange={e => setCourseTopicGroup(e.target.value)}
-					className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-				>
-					{topicData.map((group, index) => (
-						<option key={index} value={group.topicgroupname}>
-							{group.topicgroupname}
-						</option>
-					))}
-				</select>
-				<select
-					title="Select Course Topics"
-					multiple
-					value={courseTopics}
-					onChange={handleTopicChange}
-					className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-				>
-					{selectedGroupTopics.map((topic, index) => (
-						<option key={index} value={topic}>
-							{topic}
-						</option>
-					))}
-				</select>
+				{currentStep === 1 && (
+					<fieldset>
+						<legend>Course Details</legend>
+						<input
+							type="text"
+							placeholder="Course Name"
+							value={courseName}
+							onChange={e => setCourseName(e.target.value)}
+							className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+							title="Enter the course name here example: Mediapalvelut-projekti"
+						/>
+						<input
+							type="text"
+							placeholder="Course Code"
+							value={courseCode}
+							onChange={e => setCourseCode(e.target.value)}
+							className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+							title="Enter the course code here example: TX00CG61-3009"
+						/>
+						<input
+							type="text"
+							placeholder="Student Group"
+							value={studentGroup}
+							onChange={e => setStudentGroup(e.target.value)}
+							className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+							title="Enter the student group here example: tvt21-m"
+						/>
+					</fieldset>
+				)}
 
-				{customTopics.map((topic, index) => (
-					<input
-						key={index}
-						type="text"
-						placeholder="Custom Topic"
-						value={topic}
-						onChange={e => handleCustomTopicChange(index, e.target.value)}
-						className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-						title='add custom topics here example: "exam"'
-					/>
-				))}
-				<button
-					type="button"
-					onClick={addCustomTopic}
-					className="mb-3 w-full p-2 bg-metropoliaMainOrange text-white rounded hover:bg-metropoliaSecondaryOrange"
-				>
-					Add Custom Topic
-				</button>
-				<label className="w-full mb-2 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
-					<svg
-						className="w-8 h-8"
-						fill="currentColor"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-					>
-						<path d="M10 4a2 2 0 00-2 2v4a2 2 0 104 0V6a2 2 0 00-2-2zm0 12a6 6 0 100-12 6 6 0 000 12z" />
-					</svg>
-					<span className="mt-2 text-base leading-normal">Select a file</span>
-					<input type="file" className="hidden" onChange={handleFileChange} />
-				</label>
-				<label>
-					<span>Check Course Details</span>
+				{currentStep === 2 && (
+					<fieldset>
+						<legend>Topic Details</legend>
+						<select
+							title="Select Course Topic Group"
+							value={courseTopicGroup}
+							onChange={e => setCourseTopicGroup(e.target.value)}
+							className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+						>
+							{topicData.map((group, index) => (
+								<option key={index} value={group.topicgroupname}>
+									{group.topicgroupname}
+								</option>
+							))}
+						</select>
+						<select
+							title="Select Course Topics"
+							multiple
+							value={courseTopics}
+							onChange={handleTopicChange}
+							className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+						>
+							{selectedGroupTopics.map((topic, index) => (
+								<option key={index} value={topic}>
+									{topic}
+								</option>
+							))}
+						</select>
+						{customTopics.map((topic, index) => (
+							<input
+								key={index}
+								type="text"
+								placeholder="Custom Topic"
+								value={topic}
+								onChange={e => handleCustomTopicChange(index, e.target.value)}
+								className="w-full mb-3 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+								title='add custom topics here example: "exam"'
+							/>
+						))}
+						<button
+							type="button"
+							onClick={addCustomTopic}
+							className="mb-3 w-full p-2 bg-metropoliaMainOrange text-white rounded hover:bg-metropoliaSecondaryOrange"
+						>
+							Add Custom Topic
+						</button>
+					</fieldset>
+				)}
+
+				{currentStep === 3 && (
+					<fieldset>
+						<legend>File Upload</legend>
+						<label className="w-full mb-2 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
+							<svg
+								className="w-8 h-8"
+								fill="currentColor"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+							>
+								<path d="M10 4a2 2 0 00-2 2v4a2 2 0 104 0V6a2 2 0 00-2-2zm0 12a6 6 0 100-12 6 6 0 000 12z" />
+							</svg>
+							<span className="mt-2 text-base leading-normal">
+								Select a file
+							</span>
+							<input
+								type="file"
+								className="hidden"
+								onChange={handleFileChange}
+							/>
+						</label>
+					</fieldset>
+				)}
+
+				<label className="flex items-center space-x-3">
 					<input
 						type="checkbox"
 						checked={shouldCheckDetails}
 						onChange={() => setShouldCheckDetails(prev => !prev)}
+						className="form-checkbox h-5 w-5 text-blue-600"
 					/>
+					<span className="text-gray-900 font-medium">
+						Check Course Details
+					</span>
 				</label>
-				<button
-					type="submit"
-					className="w-full p-2 mt-2 bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-				>
-					Create Course
-				</button>
+				{currentStep > 1 && (
+					<button
+						type="button"
+						onClick={() => setCurrentStep(prevStep => prevStep - 1)}
+						className="w-full p-2 mt-2 bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+					>
+						Previous
+					</button>
+				)}
+				{currentStep < 3 && (
+					<button
+						type="button"
+						className="w-full p-2 mt-2 bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+						onClick={() => setCurrentStep(prevStep => prevStep + 1)}
+					>
+						Next
+					</button>
+				)}
+				{currentStep === 3 && (
+					<button
+						type="submit"
+						className="w-full p-2 mt-2 bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+					>
+						Create Course
+					</button>
+				)}
 			</form>
 		</div>
 	);
