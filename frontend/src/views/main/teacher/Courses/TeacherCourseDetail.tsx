a506ac63bf7837c409f8d37281dd0f215a978311
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-06 19:42:38 +0200

    Refactor TeacherCourseDetail component to fetch course details from API

--- frontend/src/views/main/teacher/Courses/TeacherCourseDetail.tsx
+++ frontend/src/views/main/teacher/Courses/TeacherCourseDetail.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import MainViewButton from "../../../../components/main/buttons/MainViewButton";
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import MainViewButton from '../../../../components/main/buttons/MainViewButton';
+import CourseData from '../../../../components/main/course/CourseData';
+import apihooks from '../../../../hooks/ApiHooks';
 // this is view for teacher to see the single course details
 const TeacherCourseDetail: React.FC = () => {
-   const { id } = useParams<{ id: string; }>();
+	const [courseDetail, setCourseDetail] = useState<any>([]);
+	const {id} = useParams<{id: string}>();
 
-    // Replace with actual data fetching
-    const course = {
-        name: `Course ${id}`,
-        description: 'Some description about the course.',
-    };
+	// Replace with actual data fetching
+	useEffect(() => {
+		console.log(id);
+		const fetchCourses = async () => {
+			const courseDetail = await apihooks.getCourseDetailByCourseId(id);
+			console.log(
+				'🚀 ~ file: TeacherCourses.tsx:14 ~ fetchCourses ~ courses:',
+				courseDetail,
+			);
+			setCourseDetail(courseDetail);
+		};
 
-    return (
-        <div className="m-4 bg-white rounded shadow-lg w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto">
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{course.name}</div>
-                <p className="text-gray-700 text-base">
-                    {course.description}
-                </p>
-                <MainViewButton path='/teacher/courses/:id/modify' text='Modify details' />
-            </div>
-        </div>
-    );
+		fetchCourses();
+	}, [id]);
+
+	return (
+		<div className="m-4 bg-white rounded shadow-lg w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto">
+			<div className="px-6 py-4">
+				<div className="font-bold text-xl mb-2">My Single Course</div>
+				<CourseData courseData={courseDetail} />
+			</div>
+		</div>
+	);
 };
 
 export default TeacherCourseDetail;
