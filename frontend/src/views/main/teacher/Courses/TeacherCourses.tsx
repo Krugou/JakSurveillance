2b4f3bbeab475ccaa8e8d5e94564dc0787360f03
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-06 19:42:48 +0200

    Refactor TeacherCourses component to fetch and display all courses taught by the instructor.

--- frontend/src/views/main/teacher/Courses/TeacherCourses.tsx
+++ frontend/src/views/main/teacher/Courses/TeacherCourses.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import MainViewButton from "../../../../components/main/buttons/MainViewButton";
-// this is view for teacher to see the own course details
-const TeacherCourseDetail: React.FC = () => {
-    const { id } = useParams<{ id: string; }>();
+import React, {useEffect, useState} from 'react';
+import MainViewButton from '../../../../components/main/buttons/MainViewButton';
+import CourseData from '../../../../components/main/course/CourseData';
+import apihooks from '../../../../hooks/ApiHooks';
 
-    // Replace with actual data fetching
-    const course = {
-        name: `Course ${id}`,
-        description: 'Some description about the course.',
-    };
+const TeacherCourses: React.FC = () => {
+	const [courses, setCourses] = useState([]);
+	const teacheremail = 'teacher@metropolia.fi';
 
-    return (
-        <div className="m-4 bg-white rounded shadow-lg w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto">
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{course.name}</div>
-                <p className="text-gray-700 text-base">
-                    {course.description}
-                </p>
-                <MainViewButton path='/teacher/courses/:id' text='Course details' />
-            </div>
-            
-        </div>
-    );
+	useEffect(() => {
+		const fetchCourses = async () => {
+			const courses = await apihooks.getAllCoursesByInstructorEmail(
+				teacheremail,
+			);
+			console.log(
+				'🚀 ~ file: TeacherCourses.tsx:14 ~ fetchCourses ~ courses:',
+				courses,
+			);
+			setCourses(courses);
+		};
+
+		fetchCourses();
+	}, [teacheremail]);
+
+	return (
+		<div className="m-4 bg-white rounded shadow-lg w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto">
+			<div className="px-6 py-4">
+				<div className="font-bold text-xl mb-2">My courses</div>
+				<CourseData courseData={courses} />
+				
+			</div>
+		</div>
+	);
 };
 
-export default TeacherCourseDetail;
+export default TeacherCourses;
