34e2954c67c325602f96d9d1275bf7d9383469cd
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-06 19:42:28 +0200

    Update API hooks to include new functions for retrieving courses by instructor email and course detail by course ID

--- frontend/src/hooks/ApiHooks.ts
+++ frontend/src/hooks/ApiHooks.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-//const baseUrl = 'https://jaksec.northeurope.cloudapp.azure.com/backend/';
+//const baseUrl = 'https://jaksec.northeurope.cloudapp.azure.com/api/v1/';
 const baseUrl = 'http://localhost:3002/';
 
 const doFetch = async (url: string, options: RequestInit) => {
@@ -64,8 +64,34 @@ const checkIfCourseExists = async (inputs: CourseCheckInputs) => {
 	return await doFetch(url, options);
 };
 
+const getAllCoursesByInstructorEmail = async (inputs: any) => {
+	const email = inputs;
+	console.log(
+		'🚀 ~ file: ApiHooks.ts:69 ~ getAllCoursesByInstructorEmail ~ inputs:',
+		inputs,
+	);
+
+	const options = {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+	const url = `${baseUrl}courses/instructor/${email}`;
+	return await doFetch(url, options);
+};
+
 const getAllTopicGroupsAndTopicsInsideThem = async () => {
 	const response = await doFetch(baseUrl + 'courses/topics', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	return response;
+};
+const getCourseDetailByCourseId = async (courseId: string) => {
+	const response = await doFetch(baseUrl + 'courses/coursesbyid/' + courseId, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
@@ -79,5 +105,7 @@ const apiHooks = {
 	createCourse,
 	checkIfCourseExists,
 	getAllTopicGroupsAndTopicsInsideThem,
+	getAllCoursesByInstructorEmail,
+	getCourseDetailByCourseId,
 };
 export default apiHooks;
