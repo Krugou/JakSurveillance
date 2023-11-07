179f50f677bd0dd559ca301cd867cf8cff547357
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-07 19:13:26 +0200

    Add socket connection and prevent multiple scans in StudentQrScanner component

--- frontend/src/views/main/student/StudentQrScanner.tsx
+++ frontend/src/views/main/student/StudentQrScanner.tsx
@@ -1,24 +1,43 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import {io} from 'socket.io-client';
 import Html5QrcodePlugin from '../../../components/Html5QrcodePlugin';
-const socket = io('http://localhost:3002/');
 
 const StudentQrScanner: React.FC = () => {
-	const onNewScanResult = useCallback((decodedText: string) => {
-		// Handle the scan result here
-		console.log(decodedText);
-		const [secureHash, classid] = decodedText.split('/');
-		// Emit the 'inputThatStudentHasArrivedToClass' event to the server
-		const studentId = '123123'; // replace with your student id
-		const unixtime = Date.now();
-		socket.emit(
-			'inputThatStudentHasArrivedToClass',
-			secureHash,
-			studentId,
-			unixtime,
-			classid,
-		);
-	}, []);
+	const socketRef = useRef<Socket | null>(null);
+	const [scanned, setScanned] = useState(false);
+
+	const onNewScanResult = useCallback(
+		(decodedText: string) => {
+			// Handle the scan result here
+			console.log(decodedText);
+
+			// Create a new socket connection if one does not already exist
+			if (!socketRef.current) {
+				socketRef.current = io('http://localhost:3002/');
+				socketRef.current.on('connect', () => {
+					console.log('Socket connected');
+				});
+			}
+
+			// Emit the 'inputThatStudentHasArrivedToClass' event to the server
+			if (!scanned) {
+				const secureHash = decodedText; // replace with your secure hash
+				const studentId = '123123'; // replace with your student id
+				const unixtime = Date.now();
+				const classid = '456456'; // replace with your class id
+				socketRef.current.emit(
+					'inputThatStudentHasArrivedToClass',
+					secureHash,
+					studentId,
+					unixtime,
+					classid,
+				);
+				console.log('ok');
+				setScanned(true);
+			}
+		},
+		[scanned],
+	);
 
 	return (
 		<div>
@@ -27,6 +46,7 @@ const StudentQrScanner: React.FC = () => {
 				qrbox={250}
 				disableFlip={false}
 				qrCodeSuccessCallback={onNewScanResult}
+				aspectRatio={1}
 			/>
 		</div>
 	);
