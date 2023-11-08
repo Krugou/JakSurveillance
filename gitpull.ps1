cbe96bd01fe5d08e0acce41e324860b0bc17030e
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-08 12:08:27 +0200

    Add current time output to console

--- gitpull.ps1
+++ gitpull.ps1
@@ -8,5 +8,6 @@ while ($true) {
     }
     $randomSeconds = Get-Random -Minimum 20 -Maximum 50
     Write-Host "Next pull in $randomSeconds seconds"
+    Write-Host "Current time is $(Get-Date)"
     Start-Sleep -Seconds $randomSeconds
 }
