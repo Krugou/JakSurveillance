e3553b15da9c925afe7fa65ae069c244eafb9969
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-08 12:18:06 +0200

    Increase randomSeconds range for Git pull

--- gitpull.ps1
+++ gitpull.ps1
@@ -6,7 +6,7 @@ while ($true) {
     else {
         Write-Host "Git pull failed"
     }
-    $randomSeconds = Get-Random -Minimum 20 -Maximum 50
+    $randomSeconds = Get-Random -Minimum 20 -Maximum 400
     Write-Host "Next pull in $randomSeconds seconds"
     Write-Host "Current time is $(Get-Date)"
     Start-Sleep -Seconds $randomSeconds
