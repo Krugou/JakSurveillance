880f41324a4a46366de0307b7f89a46fbc007d7f
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-08 17:57:37 +0200

    Update Header.tsx

--- frontend/src/views/Header.tsx
+++ frontend/src/views/Header.tsx
@@ -51,7 +51,10 @@ const Header: React.FC<HeaderProps> = ({title}) => {
 			</a>
 			<div className="flex items-center m-2 p-2">
 				{user && (
-					<button className="mx-2 px-2 w-full bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange">
+					<button
+						className="mx-2 px-2 w-full bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+						onClick={() => navigate(`/${user.role.toLowerCase()}/profile`)}
+					>
 						{user.username}
 					</button>
 				)}
