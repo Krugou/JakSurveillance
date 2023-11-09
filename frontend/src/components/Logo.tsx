2fc1750655563a2ff22f6c59a5740edbda2d056f
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-09 12:02:34 +0200

    something stupid

---
+++ frontend/src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Logo = () => {
+	return (
+		<div className="logo">
+			<div className="rounded-shape">
+				<div className="eye left-eye"></div>
+				<div className="eye right-eye"></div>
+				<div className="eyebrow left-eyebrow"></div>
+				<div className="eyebrow right-eyebrow"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Logo;
--- frontend/src/views/main/StartView.tsx
+++ frontend/src/views/main/StartView.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../../assets/images/JakSec.png';
+import Logo from '../../components/Logo';
 import StartViewButton from '../../components/main/buttons/StartViewButton';
 
 const StartView = () => {
 	return (
 		<div className="flex flex-col items-center justify-center ">
 			<img src={logo} alt="logo" className="w-auto sm:h-60 h-36  sm:mb-8 mb-0" />
+			<Logo />
 			<div className="flex flex-col md:flex-row items-center m-4 p-4">
 				<StartViewButton />
 			</div>
