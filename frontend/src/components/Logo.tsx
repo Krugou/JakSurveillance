e5ea2c9d0ba135aa2e8431584fe13b79cf4388dc
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-11 00:25:13 +0200

    Update Logo component styling

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-
+import '../css/logo.css';
 const Logo = () => {
 	return (
 		<>
-			<div className="logo">
-				<div className="rounded-shape">
-					<div className="eye left-eye"></div>
-					<div className="eye right-eye"></div>
-					<div className="eyebrow left-eyebrow"></div>
-					<div className="eyebrow right-eyebrow"></div>
-					<div className="horn left-horn"></div>
-					<div className="horn right-horn"></div>
-					<div className="mouth"></div>
-					<div className="nose"></div>
-					
+			<div className="logo m-4 p-2">
+				<div className="oval-shape">
+					<div className="big-brother-eye">
+						<div className="eye"></div>
+					</div>
 				</div>
 			</div>
-			<p className="text-4xl m-2 p-2 font-bold text-center tracking-widest subpixel-antialiased ">
+			<p className="text-4xl m-2 p-2 font-bold text-center tracking-widest subpixel-antialiased">
 				JakSec
 			</p>
 		</>
