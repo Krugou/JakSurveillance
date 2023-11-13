402b03d6b7405a21c999a335dbc9cea680ad46ee
KaarleH <kaarleh@metropolia.fi>
2023-11-13 14:43:01 +0200

    teacherMainview

---
+++ frontend/src/components/main/cards/Card.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface CardProps {
+    path: string;
+    title: string;
+    description: string;
+}
+
+const Card: React.FC<CardProps> = ({ path, title, description }) => {
+    const navigate = useNavigate();
+
+    const handleCardClick = () => {
+        navigate(path);
+    };
+
+    return (
+        <div onClick={handleCardClick} className="card-link m-5 cursor-pointer">
+            <div className="relative bg-white p-4 rounded-md shadow-md transition-transform transform hover:scale-105 group md:w-[15rem] h-[8rem]">
+                <h2 className="text-lg font-semibold mb-2">{title}</h2>
+                <div className="hover-bg flex items-center justify-between p-2 absolute bottom-0 left-0 w-full h-1/2 bg-metropoliaMainOrange rounded-b-md opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+                    <p className="text-white inline-block">{description}</p>
+                    <span className="text-white text-xl ml-1 inline-block">&#8594;</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- frontend/src/views/main/teacher/TeacherMainView.tsx
+++ frontend/src/views/main/teacher/TeacherMainView.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import MainViewButton from '../../../components/main/buttons/MainViewButton';
-// this is Mainview for teacher
-const MainView: React.FC = () => {
+import background from '../../../assets/images/tausta2.png';
+import Card from "../../../components/main/cards/Card";
 
+const MainView: React.FC = () => {
     return (
-        <div className="flex flex-col bg-gray-100 p-5 items-center justify-center h-1/2">
+        <div
+            className="flex flex-col bg-gray-100 p-10 items-center"
+            style={{
+                backgroundImage: `url(${background})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
             <h1 className="text-4xl font-bold mb-8">Teacher Dashboard</h1>
-            <MainViewButton path='/teacher/attendance' text='View Attendance' />
-            <MainViewButton path='/teacher/students' text='Manage Students' />
-            <MainViewButton path='/teacher/courses/createcourse' text='Create Course' />
-            <MainViewButton path='/teacher/courses/' text='View Courses' />
-            <MainViewButton path='/teacher/attendance/createattendance' text='Create new attendance' />
-            <MainViewButton path='/teacher/helpvideos' text='Instructions' />
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <Card path="/teacher/attendance" title="View Attendance" description="View your current attendance for a lesson" />
+
+                <Card path="/teacher/students" title="Manage Students" description="Manage your students details" />
+
+                <Card path="/teacher/courses/createcourse" title="Create Course" description="Create a course for your students" />
+
+                <Card path="/teacher/courses/" title="View Courses" description="View all of your courses" />
+
+                <Card path="/teacher/attendance/createattendance" title="Create new attendance" description="Create a new attendance for your lesson" />
+
+                <Card path="/teacher/helpvideos" title="Instructions" description="See instructions for all your tasks" />
+            </div>
         </div>
     );
 };
