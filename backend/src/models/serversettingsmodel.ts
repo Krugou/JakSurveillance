1ac8f3a767e9d7fa52481821743ab3c41db3b91c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-13 13:29:24 +0200

    serverSettingsModel

---
+++ backend/src/models/serversettingsmodel.ts
@@ -0,0 +1,18 @@
+import {RowDataPacket} from 'mysql2';
+import pool from '../config/adminDBPool.js';
+
+// server settings model
+const serverSettings = {
+	async getServerSettings() {
+		try {
+			return await pool
+				.promise()
+				.query<RowDataPacket[]>('SELECT * FROM serversettings');
+		} catch (error) {
+			console.error(error);
+			return Promise.reject(error);
+		}
+	},
+};
+
+export default serverSettings;
