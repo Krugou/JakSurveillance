f2ceea77f98a51d0985540448182bf14c1917f98
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-14 20:30:57 +0200

    Update TeacherAttendanceRoutes to use TeacherCreateClass instead of TeacherCreateAttendance

--- frontend/src/routes/teacher/TeacherAttendanceRoutes.tsx
+++ frontend/src/routes/teacher/TeacherAttendanceRoutes.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import TeacherAttendanceRoom from '../../views/main/teacher/Attendance/TeacherAttendanceRoom.tsx';
-import TeacherCreateAttendance from '../../views/main/teacher/Attendance/TeacherCreateAttendance.tsx';
+import TeacherCreateClass from '../../views/main/teacher/Attendance/TeacherCreateClass.tsx';
 
 const TeacherAttendanceRoutes: React.FC = () => {
     return (
-        <Routes>
-            <Route path='createclass' element={<TeacherCreateAttendance />} />
-            <Route path=':classid' element={<TeacherAttendanceRoom />} />
-        </Routes>
-    );
+					<Routes>
+						<Route path="createclass" element={<TeacherCreateClass />} />
+						<Route path=":classid" element={<TeacherAttendanceRoom />} />
+					</Routes>
+				);
 };
 
 export default TeacherAttendanceRoutes;
--- frontend/src/views/main/teacher/Attendance/TeacherCreateClass.tsx
+++ frontend/src/views/main/teacher/Attendance/TeacherCreateClass.tsx
@@ -3,10 +3,10 @@ import React, {useContext, useEffect, useRef, useState} from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import {useNavigate} from 'react-router-dom';
+import BackgroundContainer from '../../../../components/main/background/background';
 import {UserContext} from '../../../../contexts/UserContext';
 import apihooks from '../../../../hooks/ApiHooks';
-import BackgroundContainer from "../../../../components/main/background/background";
-const CreateAttendance: React.FC = () => {
+const CreateClass: React.FC = () => {
 	const {user} = useContext(UserContext);
 	const navigate = useNavigate();
 	const [courses, setCourses] = useState<Course[]>([]);
@@ -153,122 +153,123 @@ const CreateAttendance: React.FC = () => {
 
 	return (
 		<BackgroundContainer>
-		<div className="flex flex-col items-center rounded-lg justify-center h-1/2 sm:p-5 p-2 bg-gray-100">
-			<h1 className="text-lg sm:text-2xl font-bold p-2 mb-8 mt-5">
-				Fill in to open the attendance collection
-			</h1>
-			<div className="flex w-full justify-center m-auto">
-			<div className="flex sm:w-1/4 w-1/2 flex-col gap-5">
-				<label className="text-xl flex justify-end" htmlFor="course">Course :</label>
-				<label className="text-xl flex justify-end" htmlFor="topic">Topic :</label>
-			</div>
-			<div className="w-full sm:w-1/2 lg:w-2/3 flex flex-col gap-3">
-				<select
-					id="course"
-					className="block w-3/4 h-8 ml-5"
-					value={selectedSession}
-					onChange={e => {
-						const selectedCourseId = e.target.value;
-						console.log(
-							'🚀 ~ file: TeacherCreateAttendance.tsx:110 ~ selectedCourseId:',
-							selectedCourseId,
-						);
-						const newValue = parseInt(selectedCourseId, 10) - 1;
-						setSelectedSession(selectedCourseId);
-						setSelectedCourse(courses[newValue] || null);
-					}}
-				>
-					{Array.isArray(courses) &&
-						courses.map(course => {
-							// Ensure courseid is a string or number
-							const courseId =
-								typeof course.courseid === 'function'
-									? course.courseid()
-									: course.courseid;
+			<div className="flex flex-col items-center rounded-lg justify-center h-1/2 sm:p-5 p-2 bg-gray-100">
+				<h1 className="text-lg sm:text-2xl font-bold p-2 mb-8 mt-5">
+					Fill in to open the attendance collection
+				</h1>
+				<div className="flex w-full justify-center m-auto">
+					<div className="flex sm:w-1/4 w-1/2 flex-col gap-5">
+						<label className="text-xl flex justify-end" htmlFor="course">
+							Course :
+						</label>
+						<label className="text-xl flex justify-end" htmlFor="topic">
+							Topic :
+						</label>
+					</div>
+					<div className="w-full sm:w-1/2 lg:w-2/3 flex flex-col gap-3">
+						<select
+							id="course"
+							className="block w-3/4 h-8 ml-5"
+							value={selectedSession}
+							onChange={e => {
+								const selectedCourseId = e.target.value;
 
-							// Ensure course has name and code properties
-							const courseName = course.name || 'No Name';
-							const courseCode = course.code || 'No Code';
+								const newValue = parseInt(selectedCourseId, 10) - 1;
+								setSelectedSession(selectedCourseId);
+								setSelectedCourse(courses[newValue] || null);
+							}}
+						>
+							{Array.isArray(courses) &&
+								courses.map(course => {
+									// Ensure courseid is a string or number
+									const courseId =
+										typeof course.courseid === 'function'
+											? course.courseid()
+											: course.courseid;
 
-							// Ensure courseId, courseName, and courseCode are not empty
-							if (!courseId || !courseName || !courseCode) {
-								console.error('Invalid course data:', course);
-								return null; // Skip this course
-							}
+									// Ensure course has name and code properties
+									const courseName = course.name || 'No Name';
+									const courseCode = course.code || 'No Code';
 
-							return (
-								<option key={courseId} value={courseId}>
-									{courseName + ' | ' + courseCode}
+									// Ensure courseId, courseName, and courseCode are not empty
+									if (!courseId || !courseName || !courseCode) {
+										console.error('Invalid course data:', course);
+										return null; // Skip this course
+									}
+
+									return (
+										<option key={courseId} value={courseId}>
+											{courseName + ' | ' + courseCode}
+										</option>
+									);
+								})}
+						</select>
+						<select
+							id="topic"
+							className="block w-3/4 h-8 ml-5 mt-1"
+							value={selectedParticipant}
+							onChange={e => {
+								setSelectedParticipant(e.target.value);
+							}}
+						>
+							{selectedCourse &&
+								selectedCourse.topic_names &&
+								selectedCourse.topic_names.split(',').map((topic: string) => (
+									<option key={topic} value={topic}>
+										{topic}
+									</option>
+								))}
+						</select>
+					</div>
+				</div>
+				<div className="w-4/5 mt-10 h-1 bg-metropoliaMainOrange rounded-md"></div>
+				<h2 className="mt-2 text-xl p-4">Select desired date</h2>
+				<div className="text-md sm:text-xl mb-5">
+					<div className="relative">
+						<input
+							ref={inputRef}
+							type="text"
+							aria-label="Date"
+							className="py-2 text-center pl-4 pr-4 rounded-xl border focus:ring focus:ring-metropoliaSecondaryOrange focus:outline-none"
+							value={Array.isArray(date) ? 'Multiple Dates' : date.toDateString()}
+							onClick={toggleCalendar}
+							onChange={e => setDate(new Date(e.target.value))}
+						/>
+						{calendarOpen && (
+							<div className="absolute top-12 left-0 z-10">
+								<Calendar
+									onChange={handleDateChangeCalendar}
+									tileClassName={tileClassName}
+								/>
+							</div>
+						)}
+					</div>
+
+					<div className="relative mt-5">
+						<select
+							title="Time of Day" // Add this line
+							value={selectedLocation}
+							onChange={e => setSelectedLocation(e.target.value)}
+							className="block text-center appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 shadow leading-tight focus:outline-none focus:shadow-outline"
+						>
+							<option value="">Time of day</option>
+							{timeOfDay.map(option => (
+								<option key={option} value={option}>
+									{option}
 								</option>
-							);
-						})}
-				</select>
-				<select
-					id="topic"
-					className="block w-3/4 h-8 ml-5 mt-1"
-					value={selectedParticipant}
-					onChange={e => {
-						setSelectedParticipant(e.target.value);
-					}}
+							))}
+						</select>
+					</div>
+				</div>
+				<button
+					className="bg-metropoliaMainOrange w-2/4 hover:hover:bg-metropoliaSecondaryOrange text-white font-bold py-2 px-4 m-4 rounded focus:outline-none focus:shadow-outline"
+					onClick={handleOpenAttendance}
 				>
-					{selectedCourse &&
-						selectedCourse.topic_names &&
-						selectedCourse.topic_names.split(',').map((topic: string) => (
-							<option key={topic} value={topic}>
-								{topic}
-							</option>
-						))}
-				</select>
+					Open
+				</button>
 			</div>
-			</div>
-			<div className="w-4/5 mt-10 h-1 bg-metropoliaMainOrange rounded-md"></div>
-			<h2 className="mt-2 text-xl p-4">Select desired date</h2>
-			<div className="text-md sm:text-xl mb-5">
-				<div className="relative">
-					<input
-						ref={inputRef}
-						type="text"
-						aria-label="Date"
-						className="py-2 text-center pl-4 pr-4 rounded-xl border focus:ring focus:ring-metropoliaSecondaryOrange focus:outline-none"
-						value={Array.isArray(date) ? 'Multiple Dates' : date.toDateString()}
-						onClick={toggleCalendar}
-						onChange={e => setDate(new Date(e.target.value))}
-					/>
-					{calendarOpen && (
-						<div className="absolute top-12 left-0 z-10">
-							<Calendar
-								onChange={handleDateChangeCalendar}
-								tileClassName={tileClassName}
-							/>
-						</div>
-					)}
-				</div>
-
-				<div className="relative mt-5">
-					<select
-						title="Time of Day" // Add this line
-						value={selectedLocation}
-						onChange={e => setSelectedLocation(e.target.value)}
-						className="block text-center appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 shadow leading-tight focus:outline-none focus:shadow-outline"
-					>
-						<option value="">Time of day</option>
-						{timeOfDay.map(option => (
-							<option key={option} value={option}>
-								{option}
-							</option>
-						))}
-					</select>
-				</div>
-			</div>
-			<button
-				className="bg-metropoliaMainOrange w-2/4 hover:hover:bg-metropoliaSecondaryOrange text-white font-bold py-2 px-4 m-4 rounded focus:outline-none focus:shadow-outline"
-				onClick={handleOpenAttendance}
-			>
-				Open
-			</button>
-		</div>
 		</BackgroundContainer>
 	);
 };
 
-export default CreateAttendance;
+export default CreateClass;
