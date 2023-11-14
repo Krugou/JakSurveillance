8a3e3d5e15030a4633398b6848021bd549a46864
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-14 14:20:09 +0200

    Commented out console logs in class controller

--- backend/src/controllers/classcontroller.ts
+++ backend/src/controllers/classcontroller.ts
@@ -12,10 +12,10 @@ const classController = {
 	) {
 		try {
 			const topicId = await topicModel.findTopicIdUsingTopicName(topicname);
-			console.log('🚀 ~ file: classmodel.ts:63 ~ topicRows:', topicId);
+			// console.log('🚀 ~ file: classmodel.ts:63 ~ topicRows:', topicId);
 
 			const courseRows = await course.findCourseIdUsingCourseCode(coursecode);
-			console.log('🚀 ~ file: classmodel.ts:70 ~ courseRows:', courseRows);
+			// console.log('🚀 ~ file: classmodel.ts:70 ~ courseRows:', courseRows);
 
 			if (topicId.length === 0 || courseRows.length === 0) {
 				console.error(`Topic or course does not exist`);
@@ -23,9 +23,9 @@ const classController = {
 			}
 
 			const topicid = topicId[0].topicid;
-			console.log('🚀 ~ file: classmodel.ts:78 ~ topicid:', topicid);
+			// console.log('🚀 ~ file: classmodel.ts:78 ~ topicid:', topicid);
 			const courseid = courseRows[0].courseid;
-			console.log('🚀 ~ file: classmodel.ts:80 ~ courseid:', courseid);
+			// console.log('🚀 ~ file: classmodel.ts:80 ~ courseid:', courseid);
 
 			const result = await classModel.insertIntoClass(
 				start_date,
