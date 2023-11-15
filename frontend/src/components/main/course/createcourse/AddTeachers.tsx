3efb9c27c3f964624763a133f0fba537e7a67e43
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-16 00:24:51 +0200

    Add AddTeachers component for creating courses

---
+++ frontend/src/components/main/course/createcourse/AddTeachers.tsx
@@ -0,0 +1,37 @@
+const AddTeachers = ({
+	instructors,
+	handleInputChange,
+	deleteInstructor,
+	addInstructor,
+	instructorEmail,
+}) => {
+	return (
+		<fieldset>
+			<legend className="text-xl mb-3">Add teachers</legend>
+			{instructors.map((instructor, index) => (
+				<div key={index} className="flex justify-between items-center mb-3">
+					<input
+						className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+						value={instructor.email}
+						onChange={event => handleInputChange(index, event)}
+					/>
+					{instructors.length > 1 && instructor.email !== instructorEmail && (
+						<button
+							className="ml-2 p-2 bg-red-500 text-white font-bold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+							onClick={() => deleteInstructor(index)}
+						>
+							x
+						</button>
+					)}
+				</div>
+			))}
+			<button
+				className="w-full p-2 mt-2 bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
+				onClick={addInstructor}
+			>
+				Add another teacher
+			</button>
+		</fieldset>
+	);
+};
+export default AddTeachers;
