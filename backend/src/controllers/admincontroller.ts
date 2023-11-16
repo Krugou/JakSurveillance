65b780112318211dead9f77758e5631fcb45d71f
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-17 00:35:19 +0200

    Add updateServerSettings method to AdminController

--- backend/src/controllers/admincontroller.ts
+++ backend/src/controllers/admincontroller.ts
@@ -2,13 +2,28 @@ import {Request, Response} from 'express';
 import pool from '../config/adminDBPool.js';
 import ServerSettingsModel from '../models/serversettingsmodel.js';
 interface AdminController {
-	getServerSettings: (_req: Request, res: Response) => Promise<any>;
+    getServerSettings: (_req: Request, res: Response) => Promise<any>;
+    updateServerSettings: (req: Request, res: Response) => Promise<any>;
 }
 const adminController: AdminController = {
 	async getServerSettings(_req: Request, res: Response) {
 		try {
 			const serverSettings = await ServerSettingsModel.getServerSettings(pool);
 			res.status(200).send(serverSettings);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({message: 'Internal server error'});
+		}
+	},
+	async updateServerSettings(req: Request, res: Response) {
+		try {
+			const {speedofhash, leewayspeed} = req.body;
+			await ServerSettingsModel.updateServerSettings(
+				pool,
+				speedofhash,
+				leewayspeed,
+			);
+			res.status(200).send({message: 'Server settings updated successfully'});
 		} catch (error) {
 			console.error(error);
 			res.status(500).json({message: 'Internal server error'});
