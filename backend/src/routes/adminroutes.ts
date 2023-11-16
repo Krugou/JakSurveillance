dc5e71c874ca71dac12f65418b60d1f3cd033ff0
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-17 00:12:04 +0200

    Add admin route for server settings

---
+++ backend/src/routes/adminroutes.ts
@@ -0,0 +1,14 @@
+import express, {Request, Response, Router} from 'express';
+import adminController from '../controllers/admincontroller.js';
+
+const router: Router = express.Router();
+router.get('/', async (_req: Request, res: Response) => {
+	try {
+		return await adminController.getServerSettings(_req as any, res as any);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({message: 'Internal server error'});
+	}
+});
+
+export default router;
