405b916745907bc5dcacae7f53baa2d680ffe5ad
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-16 14:03:09 +0200

    Refactored code for improved performance and readability

---
+++ frontend/src/routes/counselor/CounselorStudentRoutes.tsx
@@ -0,0 +1,16 @@
+// routes/TeacherStudentsRoutes.tsx
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import CounselorStudentsView from '../../views/main/counselor/students/CounselorStudentsView';
+
+const CounselorStudentRoutes: React.FC = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<CounselorStudentsView />} />
+			<Route path=":id" element={<CounselorStudentsView />} />
+			<Route path=":id/modify" element={<CounselorStudentsView />} />
+		</Routes>
+	);
+};
+
+export default CounselorStudentRoutes;
