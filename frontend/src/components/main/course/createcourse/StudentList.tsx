1268fc25c2bdf83b8a5e22d535772afdc900ae93
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-16 12:26:44 +0200

    Refactor StudentList component to use label tag and add type attribute to button element

--- frontend/src/components/main/course/createcourse/StudentList.tsx
+++ frontend/src/components/main/course/createcourse/StudentList.tsx
@@ -34,35 +34,40 @@ const StudentList = ({studentList, setStudentList, deleteStudent}) => {
 						</div>
 					))}
 			</div>
-			{studentList.map((student, index) => (
-				<div key={index} className="flex">
-					{Object.entries(student).map(([key, value]) => (
-						<div className="p-2 w-full  md:w-auto">
-							<input
-								className="border rounded py-2 px-3 text-grey-800 w-full"
-								type="text"
-								name={key}
-								value={value}
-								onChange={e => {
-									const newStudentList = [...studentList];
-									newStudentList[index][key] = e.target.value;
-									setStudentList(newStudentList);
-								}}
-							/>
-						</div>
-					))}
-					{studentList.length > 1 && (
-						<div className="p-2 w-full md:w-auto">
-							<button
-								className="p-2 bg-red-500 text-white font-bold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
-								onClick={() => deleteStudent(index)}
-							>
-								x
-							</button>
-						</div>
-					)}
-				</div>
-			))}
+			{studentList.map(
+				(student: Record<string, string | number>, index: number) => (
+					<div key={index} className="flex">
+						{Object.entries(student).map(([key, value]) => (
+							<div className="p-2 w-full  md:w-auto">
+								<label htmlFor={key}>
+									<input
+										className="border rounded py-2 px-3 text-grey-800 w-full"
+										type="text"
+										name={key}
+										value={value.toString()}
+										onChange={e => {
+											const newStudentList = [...studentList];
+											newStudentList[index][key] = e.target.value;
+											setStudentList(newStudentList);
+										}}
+									/>
+								</label>
+							</div>
+						))}
+						{studentList.length > 1 && (
+							<div className="p-2 w-full md:w-auto">
+								<button
+									type="button"
+									className="p-2 bg-red-500 text-white font-bold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+									onClick={() => deleteStudent(index)}
+								>
+									x
+								</button>
+							</div>
+						)}
+					</div>
+				),
+			)}
 			<button
 				className="p-2 bg-green-500 text-white font-bold rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 mb-4"
 				onClick={addStudent}
