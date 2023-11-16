4d2cba58476f96dea3808b2d1fa542423fe26385
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-17 00:35:45 +0200

    Add POST route for updating server settings in adminroutes.ts

--- backend/src/routes/adminroutes.ts
+++ backend/src/routes/adminroutes.ts
@@ -10,5 +10,13 @@ router.get('/', async (_req: Request, res: Response) => {
 		res.status(500).json({message: 'Internal server error'});
 	}
 });
+router.post('/', async (req: Request, res: Response) => {
+	try {
+		return await adminController.updateServerSettings(req as any, res as any);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({message: 'Internal server error'});
+	}
+});
 
 export default router;
