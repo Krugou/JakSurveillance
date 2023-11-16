188b2e881e174f046200e2f6e7095a407aa28e62
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-16 16:31:25 +0200

    show old courses toggler

--- frontend/src/views/main/student/StudentCourses.tsx
+++ frontend/src/views/main/student/StudentCourses.tsx
@@ -19,6 +19,7 @@ const StudentCourses: React.FC = () => {
 	const [error, setError] = useState<string | null>(null);
 	const {user} = useContext(UserContext);
 	const [courses, setCourses] = useState<Course[]>([]);
+	const [showEndedCourses, setShowEndedCourses] = useState(true);
 	const navigate = useNavigate();
 	useEffect(() => {
 		const fetchCourses = async () => {
@@ -55,59 +56,86 @@ const StudentCourses: React.FC = () => {
 			<h1 className="text-2xl sm:text-4xl font-bold mb-8 text-center">
 				Your Courses
 			</h1>
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-				{courses.map(course => {
-					const startDate = new Date(course.startDate).toLocaleDateString();
-					const endDate = new Date(course.endDate);
-					const endDateString = endDate.toLocaleDateString();
-					const topics = course.topic_names.replace(/,/g, ', ');
-					const isCourseEnded =
-						endDate.setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0);
+			<label className="flex items-center relative w-max cursor-pointer select-none">
+				<span className="text-lg font-bold mr-3">Show old courses</span>
+				<input
+					type="checkbox"
+					className="appearance-none transition-colors cursor-pointer w-14 h-7 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black focus:ring-blue-500 bg-white"
+					checked={showEndedCourses}
+					onChange={() => setShowEndedCourses(!showEndedCourses)}
+				/>
+				<span className="absolute font-medium text-xs uppercase right-1 text-white">
+					{' '}
+				</span>
+				<span className="absolute font-medium text-xs uppercase right-8 text-white">
+					{' '}
+				</span>
+				<span
+					className={`w-7 h-7 right-7 absolute rounded-full transform transition-transform ${
+						showEndedCourses ? ' bg-green-400 translate-x-7' : 'bg-red-500'
+					}`}
+				/>
+			</label>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
+				{courses
+					.filter(course => {
+						const endDate = new Date(course.endDate);
+						const isCourseEnded =
+							endDate.setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0);
+						return !isCourseEnded || showEndedCourses;
+					})
+					.map(course => {
+						const startDate = new Date(course.startDate).toLocaleDateString();
+						const endDate = new Date(course.endDate);
+						const endDateString = endDate.toLocaleDateString();
+						const topics = course.topic_names.replace(/,/g, ', ');
+						const isCourseEnded =
+							endDate.setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0);
 
-					return (
-						<Tooltip placement="top" title={isCourseEnded ? 'Course has ended' : ''}>
-							<div
-								key={course.courseid}
-								className={`p-6 bg-white shadow-md rounded-lg relative ${
-									isCourseEnded ? 'opacity-50' : ''
-								}`}
-							>
-								{isCourseEnded && (
-									<div className="absolute top-2 right-2">
-										<ReportIcon style={{color: 'red'}} />
-									</div>
-								)}
-								<h2 className="text-2xl underline font-bold mb-2 text-black">
-									{course.course_name + ' ' + course.code}
-								</h2>
-								<p className="mb-1">
-									<strong>Topics:</strong> {topics}
-								</p>
-								<p className="mb-1">
-									<strong>Start Date:</strong> {startDate}
-								</p>
-								<p className="mb-1">
-									<strong>End Date:</strong> {endDateString}
-								</p>
-								<p className="mb-1">
-									<strong>Course Instructors:</strong> {course.instructor_name}
-								</p>
-								<button
-									className={`mt-4 font-bold py-2 px-4 rounded ${
-										isCourseEnded
-											? 'bg-metropoliaSupportRed hover:bg-red-900'
-											: 'bg-metropoliaTrendGreen hover:bg-green-700'
-									} text-white`}
-									onClick={() =>
-										navigate(`/student/courses/attendance/${course.usercourseid}`)
-									}
+						return (
+							<Tooltip placement="top" title={isCourseEnded ? 'Course has ended' : ''}>
+								<div
+									key={course.courseid}
+									className={`p-6 bg-white shadow-md rounded-lg relative ${
+										isCourseEnded ? 'opacity-50' : ''
+									}`}
 								>
-									View your attendance
-								</button>
-							</div>
-						</Tooltip>
-					);
-				})}
+									{isCourseEnded && (
+										<div className="absolute top-2 right-2">
+											<ReportIcon style={{color: 'red'}} />
+										</div>
+									)}
+									<h2 className="text-2xl underline font-bold mb-2 text-black">
+										{course.course_name + ' ' + course.code}
+									</h2>
+									<p className="mb-1">
+										<strong>Topics:</strong> {topics}
+									</p>
+									<p className="mb-1">
+										<strong>Start Date:</strong> {startDate}
+									</p>
+									<p className="mb-1">
+										<strong>End Date:</strong> {endDateString}
+									</p>
+									<p className="mb-1">
+										<strong>Course Instructors:</strong> {course.instructor_name}
+									</p>
+									<button
+										className={`mt-4 font-bold py-2 px-4 rounded ${
+											isCourseEnded
+												? 'bg-metropoliaSupportRed hover:bg-red-900'
+												: 'bg-metropoliaTrendGreen hover:bg-green-700'
+										} text-white`}
+										onClick={() =>
+											navigate(`/student/courses/attendance/${course.usercourseid}`)
+										}
+									>
+										View your attendance
+									</button>
+								</div>
+							</Tooltip>
+						);
+					})}
 			</div>
 		</div>
 	);
