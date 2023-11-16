438a4db3497646ddf981f0129f61ee77364ee049
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-16 13:34:30 +0200

    Add hidden columns feature to StudentList component

--- frontend/src/components/main/course/createcourse/StudentList.tsx
+++ frontend/src/components/main/course/createcourse/StudentList.tsx
@@ -1,7 +1,11 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const StudentList = ({studentList, setStudentList}) => {
-	const addStudent = () => {
+	const [hiddenColumns, setHiddenColumns] = useState<Record<string, boolean>>(
+		{},
+	);
+	const addStudent = event => {
+		event.preventDefault();
 		const newStudent = {
 			first_name: 'example',
 			last_name: 'example',
@@ -25,57 +29,73 @@ const StudentList = ({studentList, setStudentList}) => {
 
 	useEffect(() => {
 		if (studentList.length === 0) {
-			addStudent();
+			addStudent(event);
 		}
 	}, []); // Empty dependency array means this effect runs once on mount
 
 	return (
 		<div className="overflow-hidden h-1/2">
-			<div className="flex">
-				{studentList.length > 0 &&
-					Object.keys(studentList[0]).map(key => (
-						<div className="p-2 w-full md:w-auto">
-							<label className="block">{key}</label>
-						</div>
-					))}
-			</div>
-			{studentList.map(
-				(student: Record<string, string | number>, index: number) => (
-					<div key={index} className="flex">
-						{Object.entries(student).map(([key, value]) => (
-							<div className="p-2 w-full  md:w-auto">
-								<label htmlFor={key}>
-									<input
-										className="border rounded py-2 px-3 text-grey-800 w-full"
-										type="text"
-										name={key}
-										value={value.toString()}
-										onChange={e => {
-											const newStudentList = [...studentList];
-											newStudentList[index][key] = e.target.value;
-											setStudentList(newStudentList);
-										}}
-									/>
-								</label>
-							</div>
-						))}
-						{studentList.length > 1 && (
-							<div className="p-2 w-full md:w-auto">
-								<button
-									type="button"
-									className="p-2 bg-red-500 text-white font-bold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
-									onClick={() => deleteStudent(index)}
-								>
-									x
-								</button>
-							</div>
-						)}
-					</div>
-				),
-			)}
+			<table className="table-auto w-full">
+				<thead>
+					<tr>
+						{studentList.length > 0 &&
+							Object.keys(studentList[0]).map(
+								(key, index) =>
+									!hiddenColumns[key] && (
+										<th key={index} className="px-4 py-2">
+											{key}
+											<button
+												className="ml-2"
+												onClick={() => setHiddenColumns({...hiddenColumns, [key]: true})}
+											>
+												H
+											</button>
+										</th>
+									),
+							)}
+						{studentList.length > 1 && <th className="px-4 py-2">Actions</th>}
+					</tr>
+				</thead>
+				<tbody>
+					{studentList.map(
+						(student: Record<string, string | number>, index: number) => (
+							<tr key={index}>
+								{Object.entries(student).map(
+									([key, value], innerIndex) =>
+										!hiddenColumns[key] && (
+											<td key={innerIndex} className="border px-4 py-2">
+												<input
+													className="border rounded py-2 px-3 text-grey-800 w-full"
+													type="text"
+													value={value.toString()}
+													onChange={e => {
+														const newStudentList = [...studentList];
+														newStudentList[index][key] = e.target.value;
+														setStudentList(newStudentList);
+													}}
+												/>
+											</td>
+										),
+								)}
+								{studentList.length > 1 && (
+									<td className="border px-4 py-2">
+										<button
+											type="button"
+											className="p-2 bg-red-500 text-white font-bold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+											onClick={() => deleteStudent(index)}
+										>
+											x
+										</button>
+									</td>
+								)}
+							</tr>
+						),
+					)}
+				</tbody>
+			</table>
 			<button
 				className="p-2 bg-green-500 text-white font-bold rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 mb-4"
-				onClick={addStudent}
+				onClick={event => addStudent(event)}
 			>
 				Add Student
 			</button>
