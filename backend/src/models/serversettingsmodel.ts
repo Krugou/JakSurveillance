51c8fef2915547772a25ad9a0b0f0d272c3581f9
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-17 00:35:32 +0200

    Add updateServerSettings function to serversettingsmodel.ts

--- backend/src/models/serversettingsmodel.ts
+++ backend/src/models/serversettingsmodel.ts
@@ -12,6 +12,23 @@ const serverSettingsModel = {
 			return Promise.reject(error);
 		}
 	},
+	async updateServerSettings(
+		pool: Pool,
+		speedofhash: number,
+		leewayspeed: number,
+	) {
+		try {
+			return await pool
+				.promise()
+				.query('UPDATE serversettings SET speedofhash = ?, leewayspeed = ?', [
+					speedofhash,
+					leewayspeed,
+				]);
+		} catch (error) {
+			console.error(error);
+			return Promise.reject(error);
+		}
+	},
 };
 
 export default serverSettingsModel;
