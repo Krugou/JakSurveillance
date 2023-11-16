aba2506b212ba9f80f64a4202ac04f50a56bc843
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-16 10:51:17 +0200

    Add MainViewTitle component for displaying dashboard title

---
+++ frontend/src/components/main/titles/MainViewTitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainViewTitle = ({role}) => {
+	return (
+		<h1 className="text-4xl font-bold text-gray-700 mb-5 underline">
+			{role} Dashboard
+		</h1>
+	);
+};
+
+export default MainViewTitle;
