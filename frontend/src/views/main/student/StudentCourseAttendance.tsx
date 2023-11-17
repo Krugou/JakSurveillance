52b693c8c32647e247a62342157a5f2be6d5ab19
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-17 12:50:55 +0200

    no data available

--- frontend/src/views/main/student/StudentCourseAttendance.tsx
+++ frontend/src/views/main/student/StudentCourseAttendance.tsx
@@ -86,80 +86,84 @@ const StudentCourseAttendance: React.FC = () => {
 			new Date(attendance.start_date).toLocaleDateString().includes(searchTerm) &&
 			(sortOption === 'All' || attendance.topicname === sortOption),
 	);
-	return (
-		<div className="overflow-x-auto flex flex-col border-x border-t">
-			<h1 className="text-xl sm:text-4xl font-bold mt-2 mb-8 text-center">
-				Attendance for Course {attendanceData[0].name}
-			</h1>
-			<div className="flex items-center justify-around flex-wrap">
-				<input
-					type="text"
-					placeholder="Search by date"
-					value={searchTerm}
-					onChange={handleSearchChange}
-					className="w-1/6 mt-10 p-4 m-2 border border-black rounded"
-				/>
-				<FormControl className="md:w-1/4 mt-2 md:mt-0">
-					<label>Sort Topics:</label>
-					<Select
-						className="favorite-selector"
-						value={sortOption}
-						onChange={handleChange}
-					>
-						<MenuItem value="All">
-							<div className="item-selector">
-								<AutorenewIcon className="highest-star-selector-icon" />
-								<span className="selector-text">All</span>
-							</div>
-						</MenuItem>
-						{uniqueTopics.map(topic => (
-							<MenuItem value={topic}>
+	if (attendanceData.length > 0) {
+		return (
+			<div className="overflow-x-auto flex flex-col border-x border-t">
+				<h1 className="text-xl sm:text-4xl font-bold mt-2 mb-8 text-center">
+					Attendance for Course {attendanceData[0].name}
+				</h1>
+				<div className="flex items-center justify-around flex-wrap">
+					<input
+						type="text"
+						placeholder="Search by date"
+						value={searchTerm}
+						onChange={handleSearchChange}
+						className="w-1/6 mt-10 p-4 m-2 border border-black rounded"
+					/>
+					<FormControl className="md:w-1/4 mt-2 md:mt-0">
+						<label>Sort Topics:</label>
+						<Select
+							className="favorite-selector"
+							value={sortOption}
+							onChange={handleChange}
+						>
+							<MenuItem value="All">
 								<div className="item-selector">
 									<AutorenewIcon className="highest-star-selector-icon" />
-									<span className="selector-text">{topic}</span>
+									<span className="selector-text">All</span>
 								</div>
 							</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-			</div>
-			<div className="overflow-x-auto border-x border-t m-6">
-				<table className="table-auto w-full">
-					<thead className="border-b">
-						<tr className="bg-gray-100">
-							<th className="text-left p-4 font-medium underline">Date</th>
-							<th className="text-left p-4 font-medium underline">Teacher</th>
-							<th className="text-left p-4 font-medium underline">Time of Day</th>
-							<th className="text-left p-4 font-medium underline">Topic</th>
-							<th className="text-left p-4 font-medium underline">Status</th>
-						</tr>
-					</thead>
-					<tbody>
-						{filteredAttendanceData.map((attendance, index) => (
-							<tr
-								key={index}
-								className={`border-b hover:bg-gray-50 ${
-									attendance.status === 0
-										? 'bg-metropoliaSupportRed'
-										: 'bg-metropoliaTrendGreen'
-								}`}
-							>
-								<td className="p-4">
-									{new Date(attendance.start_date).toLocaleDateString()}
-								</td>
-								<td className="p-4">{attendance.teacher}</td>
-								<td className="p-4">{attendance.timeofday}</td>
-								<td className="p-4">{attendance.topicname}</td>
-								<td className="p-4">
-									{attendance.status === 1 ? 'Present' : 'Absent'}
-								</td>
+							{uniqueTopics.map(topic => (
+								<MenuItem value={topic}>
+									<div className="item-selector">
+										<AutorenewIcon className="highest-star-selector-icon" />
+										<span className="selector-text">{topic}</span>
+									</div>
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</div>
+				<div className="overflow-x-auto border-x border-t m-6">
+					<table className="table-auto w-full">
+						<thead className="border-b">
+							<tr className="bg-gray-100">
+								<th className="text-left p-4 font-medium underline">Date</th>
+								<th className="text-left p-4 font-medium underline">Teacher</th>
+								<th className="text-left p-4 font-medium underline">Time of Day</th>
+								<th className="text-left p-4 font-medium underline">Topic</th>
+								<th className="text-left p-4 font-medium underline">Status</th>
 							</tr>
-						))}
-					</tbody>
-				</table>
+						</thead>
+						<tbody>
+							{filteredAttendanceData.map((attendance, index) => (
+								<tr
+									key={index}
+									className={`border-b hover:bg-gray-50 ${
+										attendance.status === 0
+											? 'bg-metropoliaSupportRed'
+											: 'bg-metropoliaTrendGreen'
+									}`}
+								>
+									<td className="p-4">
+										{new Date(attendance.start_date).toLocaleDateString()}
+									</td>
+									<td className="p-4">{attendance.teacher}</td>
+									<td className="p-4">{attendance.timeofday}</td>
+									<td className="p-4">{attendance.topicname}</td>
+									<td className="p-4">
+										{attendance.status === 1 ? 'Present' : 'Absent'}
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return <div className="text-center text-3xl m-10">No Data available</div>;
+	}
 };
 
 export default StudentCourseAttendance;
