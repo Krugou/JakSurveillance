8df33b78beb9fb36c2899606232c807f3c43d4d9
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-18 20:17:44 +0200

    Refactor TeacherCoursesRoutes component

--- frontend/src/routes/teacher/TeacherCoursesRoutes.tsx
+++ frontend/src/routes/teacher/TeacherCoursesRoutes.tsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
 import TeacherCourseDetail from '../../views/main/teacher/Courses/TeacherCourseDetail.tsx';
 import TeacherCourseModify from '../../views/main/teacher/Courses/TeacherCourseModify.tsx';
 import TeacherCourses from '../../views/main/teacher/Courses/TeacherCourses.tsx';
-import TeacherCreateCourse from '../../views/main/teacher/Courses/TeacherCreateCourse.tsx';
+import TeacherCreateCourseRoutes from './create/TeacherCreateCourseRoutes.tsx';
 
 const TeacherCoursesRoutes: React.FC = () => {
-    return (
-        <Routes>
-            <Route path='/' element={<TeacherCourses />} />
-            <Route path='create' element={<TeacherCreateCourse />} />
-            <Route path=':id' element={<TeacherCourseDetail />} />
-            <Route path=':id/modify' element={<TeacherCourseModify />} />
-        </Routes>
-    );
+	return (
+		<Routes>
+			<Route path="/" element={<TeacherCourses />} />
+			<Route path="create/*" element={<TeacherCreateCourseRoutes />} />
+			<Route path=":id" element={<TeacherCourseDetail />} />
+			<Route path=":id/modify" element={<TeacherCourseModify />} />
+		</Routes>
+	);
 };
 
 export default TeacherCoursesRoutes;
