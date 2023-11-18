f42bbc763a2ab94f3cd44278d0cd19dc338beb4f
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-18 20:18:08 +0200

    Refactor course type buttons

--- frontend/src/views/main/teacher/Courses/TeacherCreateCourse.tsx
+++ frontend/src/views/main/teacher/Courses/TeacherCreateCourse.tsx
@@ -1,32 +1,18 @@
 import React, {useState} from 'react';
 
 import BackgroundContainer from '../../../../components/main/background/background';
-import CreateCourseCustom from '../../../../components/main/course/CreateCourseCustom';
-import CreateCourseEasy from '../../../../components/main/course/CreateCourseEasy';
+import MainViewButton from '../../../../components/main/buttons/MainViewButton';
+
 const TeacherCreateCourse: React.FC = () => {
-	const [courseType, setCourseType] = useState<'easy' | 'custom' | null>(null);
 	return (
 		<BackgroundContainer>
 			<div className="w-full pt-10 pb-10">
 				<h1 className="text-4xl text-center font-bold mb-8">Create Course</h1>
-				{!courseType && (
-					<div className="flex justify-center space-x-4">
-						<button
-							className="bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-							onClick={() => setCourseType('easy')}
-						>
-							Easy
-						</button>
-						<button
-							className="bg-metropoliaMainOrange text-white font-bold rounded hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange"
-							onClick={() => setCourseType('custom')}
-						>
-							Custom
-						</button>
-					</div>
-				)}
-				{courseType === 'easy' && <CreateCourseEasy />}
-				{courseType === 'custom' && <CreateCourseCustom />}
+
+				<div className="flex justify-center space-x-4">
+					<MainViewButton path="easy" text="Easy" />
+					<MainViewButton path="custom" text="Custom" />
+				</div>
 			</div>
 		</BackgroundContainer>
 	);
