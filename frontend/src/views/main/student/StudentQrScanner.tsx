e90eee3f21963bfa72d84b74d934b7c861c59a5b
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-20 19:51:36 +0200

    Refactor QR scanner component

--- frontend/src/views/main/student/StudentQrScanner.tsx
+++ frontend/src/views/main/student/StudentQrScanner.tsx
@@ -1,15 +1,14 @@
 import {QrScanner} from '@yudiel/react-qr-scanner';
 import React, {useCallback, useContext, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
 import io, {Socket} from 'socket.io-client';
 import {UserContext} from '../../../contexts/UserContext.tsx';
 const StudentQrScanner: React.FC = () => {
+	const navigate = useNavigate();
 	const {user} = useContext(UserContext);
-	const [successState, setSuccessState] = useState(false);
 	const [scanned, setScanned] = useState(false);
 	const [socket, setSocket] = useState<Socket | null>(null);
-	const [counter, setCounter] = useState(20);
-	const [errorMessage, setErrorMessage] = useState('Error');
-	console.log('errorMessage', errorMessage);
 	const onNewScanResult = useCallback(
 		(decodedText: string) => {
 			// Handle the scan result here
@@ -53,27 +52,20 @@ const StudentQrScanner: React.FC = () => {
 					setScanned(true);
 				}
 				newSocket.on('youhavebeensavedintoclass', studentId => {
-					setSuccessState(true);
-					setShouldRender(false);
+					toast.success(`You have been saved into ${classid} lecture`);
 					console.log('You have been saved into class', studentId);
+					toast.success('redirecting to mainview');
+					navigate('/student/mainview');
 				});
 				newSocket.on('inputThatStudentHasArrivedToClassTooSlow', studentId2 => {
-					setSuccessState(false);
-					setShouldRender(true);
+					toast.error('You were too slow, try again');
 					console.log('inputThatStudentHasArrivedToClassTooSlow', studentId2);
 				});
 			}
 		},
 		[scanned],
 	);
-	const [shouldRender, setShouldRender] = useState(true);
 
-	const onResetClick = useCallback(() => {
-		setScanned(false);
-		setShouldRender(false);
-		setTimeout(() => setShouldRender(true), 0);
-		setSuccessState(false);
-	}, []);
 	useEffect(() => {
 		return () => {
 			// Disconnect the socket when the component unmounts
@@ -82,55 +74,19 @@ const StudentQrScanner: React.FC = () => {
 			}
 		};
 	}, [socket]);
-	useEffect(() => {
-		if (successState && counter > 0) {
-			const timer = setTimeout(() => setCounter(counter - 1), 1000);
-			return () => clearTimeout(timer);
-		}
-		if (counter === 0) {
-			window.location.href = 'mainview';
-		}
-		// Return an empty function when there's nothing to clean up
-		return () => {};
-	}, [successState, counter]);
+
 	const handleError = (error: any) => {
 		console.log('error', error);
-		setErrorMessage(error.message);
 	};
 	return (
 		<>
 			{user && user.studentnumber && (
-				<div className="flex flex-col items-center justify-center w-1/2 m-auto  ">
-					{successState ? (
-						<p className="text-3xl font-bold mb-4">
-							You can leave this site now redirecting in {counter} seconds
-						</p>
-					) : (
-						<p className="text-xl font-bold mb-4">Scan QR code to get attendance</p>
-					)}
-					<div className="flex justify-center">
-						{successState && <p className="text-green-500 font-bold">Success</p>}
-						{!successState && scanned && (
-							<p className="text-red-500 font-bold">Failure</p>
-						)}
-					</div>
-
-					{shouldRender && (
-						<QrScanner
-							onDecode={onNewScanResult}
-							onError={handleError}
-							scanDelay={2000}
-							hideCount={false}
-						/>
-					)}
-
-					<button
-						className="bg-blue-500 m-2 p-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-						onClick={onResetClick}
-					>
-						Try again or reset
-					</button>
-				</div>
+				<QrScanner
+					onDecode={onNewScanResult}
+					onError={handleError}
+					scanDelay={2000}
+					hideCount={false}
+				/>
 			)}
 		</>
 	);
