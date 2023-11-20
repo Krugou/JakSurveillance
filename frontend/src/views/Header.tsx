42c6eb2dd0e3fe1321cb97569e029aa3fc03a303
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-20 20:14:42 +0200

    Add Link component to Header

--- frontend/src/views/Header.tsx
+++ frontend/src/views/Header.tsx
@@ -1,10 +1,11 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {useLocation, useNavigate} from 'react-router-dom';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
 import logo from '../assets/images/metropolia_s_oranssi_en.png';
 import ErrorAlert from '../components/main/ErrorAlert';
 import NavigationButton from '../components/main/buttons/NavigationButton';
 import {UserContext} from '../contexts/UserContext';
 import apiHooks from '../hooks/ApiHooks';
+
 interface HeaderProps {
 	title: string;
 }
@@ -45,44 +46,32 @@ const Header: React.FC<HeaderProps> = () => {
 		getUserInfo();
 	}, [location]); // jos taulukko tyhjä, ajetaan vain kerran
 
-	if (!user) {
-		return (
-			<header className="flex items-center sm:p-4 p-0 m-4 justify-between">
-				{alert && <ErrorAlert onClose={() => setAlert(null)} alert={alert} />}
-
-				<a href="/">
-					<img
-						src={logo}
-						alt="Logo"
-						className="w-24 sm:w-32 md:w-48 lg:w-64 h-auto mr-4"
-					/>
-				</a>
-			</header>
-		);
-	}
-
 	return (
-		<header className="flex items-center sm:flex-row flex-col sm:p-4 p-0 m-4 justify-between">
+		<header
+			className={`flex items-center ${
+				user ? 'sm:flex-row flex-col' : ''
+			} sm:p-4 p-0 m-4 justify-between`}
+		>
 			{alert && <ErrorAlert onClose={() => setAlert(null)} alert={alert} />}
-			<a href={`/${user.role.toLowerCase()}/mainview`}>
+			<Link to={user ? `/${user.role.toLowerCase()}/mainview` : '/'}>
 				<img
 					src={logo}
 					alt="Logo"
-					className="w-48 sm:w-32 mb-5 md:w-48 lg:w-64 h-auto mr-4"
+					className={`w-48 mb-5 sm:w-32 md:w-48 lg:w-64 h-auto mr-4 ${
+						user ? '' : 'w-24'
+					}`}
 				/>
-			</a>
-			<div className="flex w-full gap-10 justify-center sm:w-fit items-center m-2 p-2">
-				{user && (
-					<>
-						<NavigationButton
-							user={user}
-							path={`/${user.role.toLowerCase()}/profile`}
-							label={user.username}
-						/>
-						<NavigationButton user={user} path="/logout" label="Logout" />
-					</>
-				)}
-			</div>
+			</Link>
+			{user && (
+				<div className="flex w-full gap-10 justify-center sm:w-fit items-center m-2 p-2">
+					<NavigationButton
+						user={user}
+						path={`/${user.role.toLowerCase()}/profile`}
+						label={user.username}
+					/>
+					<NavigationButton user={user} path="/logout" label="Logout" />
+				</div>
+			)}
 		</header>
 	);
 };
