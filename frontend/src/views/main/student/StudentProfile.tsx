957a22062632d6a057f164ffac44bb2dc41dc800
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-20 23:53:08 +0200

    button colors

--- frontend/src/views/main/student/StudentProfile.tsx
+++ frontend/src/views/main/student/StudentProfile.tsx
@@ -34,7 +34,7 @@ const StudentProfile: React.FC = () => {
 				</p>
 			</div>
 			<button
-				className="px-4 py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-700"
+				className="px-4 py-2 mt-4 bg-metropoliaSupportBlue text-white rounded hover:bg-blue-700"
 				onClick={() => navigate('/student/courses')} // Navigate to /student/courses when the button is clicked
 			>
 				My Courses
--- frontend/src/views/main/teacher/TeacherProfile.tsx
+++ frontend/src/views/main/teacher/TeacherProfile.tsx
@@ -20,7 +20,7 @@ const TeacherProfile: React.FC = () => {
 				<ProfileInfo user={user} />
 			</div>
 			<button
-				className="px-4 py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-700"
+				className="px-4 py-2 mt-4 bg-metropoliaSupportBlue text-white rounded hover:bg-blue-700"
 				onClick={() => navigate('/teacher/courses')}
 			>
 				My Courses
