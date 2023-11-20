d8f655a741a58097da17f48a28dd74c3fc4539f3
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-20 13:23:23 +0200

    Update InputField component styling

--- frontend/src/components/main/course/createcourse/coursedetails/InputField.tsx
+++ frontend/src/components/main/course/createcourse/coursedetails/InputField.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const InputField = ({label, type, name, value, onChange}) => (
+const InputField = ({label = '', type, name, value, onChange}) => (
 	<div className="flex flex-col mb-3">
 		<label className="mb-2 font-bold text-gray-900" htmlFor={name}>
 			{label}
 		</label>
 		<input
-			className="border rounded py-2 px-3 text-grey-800"
+			className="border rounded py-2 px-3 text-grey-800 w-full"
 			type={type}
 			name={name}
 			id={name}
