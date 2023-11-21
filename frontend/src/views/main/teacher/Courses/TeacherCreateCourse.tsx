15604989ce51ca3ea24f3091e8b7e4876bd2cfb0
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-21 20:07:28 +0200

    Update button text in TeacherCreateCourse component

--- frontend/src/views/main/teacher/Courses/TeacherCreateCourse.tsx
+++ frontend/src/views/main/teacher/Courses/TeacherCreateCourse.tsx
@@ -8,10 +8,10 @@ const TeacherCreateCourse: React.FC = () => {
 		<BackgroundContainer>
 			<div className="w-full pt-10 pb-10">
 				<h1 className="text-4xl text-center font-bold mb-8">Create Course</h1>
-
+				<p className="text-center mb-4">Choose between routes</p>
 				<div className="flex justify-center space-x-4">
-					<MainViewButton path="easy" text="Easy" />
-					<MainViewButton path="custom" text="Custom" />
+					<MainViewButton path="easy" text="Excel Mode" />
+					<MainViewButton path="custom" text="Custom Mode" />
 				</div>
 			</div>
 		</BackgroundContainer>
