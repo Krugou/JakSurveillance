2d21cf5abd7337b310510f59359102263ef20db3
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-22 11:01:41 +0200

    Update DeleteModal.tsx

--- frontend/src/components/main/modals/DeleteModal.tsx
+++ frontend/src/components/main/modals/DeleteModal.tsx
@@ -18,8 +18,8 @@ const DeleteModal: React.FC<DeleteModalProps> = ({
 
 	return (
 		<div className="fixed inset-0 flex items-center justify-center z-50">
-			<div className="fixed inset-0 bg-black opacity-50"></div>
-			<div className="bg-white w-10/12 lg:w-1/3 sm:w-1/2 p-6 rounded-lg shadow-lg z-10">
+			<div className="fixed inset-0 bg-black opacity-70"></div>
+			<div className="bg-white w-10/12 lg:w-3/12 sm:w-1/2 p-6 rounded-lg shadow-lg z-10 border-4 border-red-700">
 				<h3 className="text-xl leading-6 font-medium text-gray-900">
 					Confirmation
 				</h3>
@@ -28,7 +28,7 @@ const DeleteModal: React.FC<DeleteModalProps> = ({
 						Are you sure you want to delete this course?
 					</p>
 				</div>
-				<div className="bg-white px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+				<div className="bg-white mt-2  sm:px-6 sm:flex sm:flex-row-reverse">
 					<button
 						onClick={onClose}
 						className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-500 text-base font-medium text-white hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
@@ -37,7 +37,7 @@ const DeleteModal: React.FC<DeleteModalProps> = ({
 					</button>
 					<button
 						onClick={onDelete}
-						className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+						className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-red-700 text-base font-medium text-white hover:text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
 					>
 						DELETE
 					</button>
