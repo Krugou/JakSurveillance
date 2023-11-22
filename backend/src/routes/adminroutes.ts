bb414b53623de33ee8c7e5d66ed459a756c436d6
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-22 22:46:58 +0200

    Add rolemodel and usermodel imports, and create routes for fetching special roles and changing user roles

--- backend/src/routes/adminroutes.ts
+++ backend/src/routes/adminroutes.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response, Router} from 'express';
 import adminController from '../controllers/admincontroller.js';
-
+import rolemodel from '../models/rolemodel.js';
+import usermodel from '../models/usermodel.js';
 const router: Router = express.Router();
 router.get('/', async (_req: Request, res: Response) => {
 	try {
@@ -13,6 +14,26 @@ router.get('/', async (_req: Request, res: Response) => {
 router.post('/', async (req: Request, res: Response) => {
 	try {
 		return await adminController.updateServerSettings(req as any, res as any);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({message: 'Internal server error'});
+	}
+});
+router.get('/rolesspecial', async (_req: Request, res: Response) => {
+	try {
+		const roles = await rolemodel.fetchTeacherAndCounselorRoles();
+		res.send(roles);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({message: 'Internal server error'});
+	}
+});
+
+router.post('/change-role', async (req: Request, res: Response) => {
+	try {
+		const {email, roleId} = req.body;
+		await usermodel.changeRoleId(email, roleId);
+		res.send({message: 'Role changed successfully'});
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({message: 'Internal server error'});
