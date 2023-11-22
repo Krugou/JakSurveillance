cdf241f302712c4628d140019851fa62f6770da3
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-22 14:10:27 +0200

    Update build date in .env file

--- frontend/build-date.cjs
+++ frontend/build-date.cjs
@@ -4,4 +4,8 @@ const path = require('path');
 const date = new Date().toISOString();
 const envFilePath = path.resolve(__dirname, '.env');
 
-fs.appendFileSync(envFilePath, `\nVITE_REACT_APP_BUILD_DATE=${date}`);
+let envContent = fs.readFileSync(envFilePath, 'utf8');
+envContent = envContent.replace(/VITE_REACT_APP_BUILD_DATE=.*\n/g, '');
+envContent += `VITE_REACT_APP_BUILD_DATE=${date}\n`;
+
+fs.writeFileSync(envFilePath, envContent);
