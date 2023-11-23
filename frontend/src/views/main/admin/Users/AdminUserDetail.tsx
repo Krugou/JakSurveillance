68a4625131fa474b4b978e525fbe0d132fca59bd
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-23 19:53:03 +0200

    Fix user ID type and convert to number

--- frontend/src/views/main/admin/Users/AdminUserDetail.tsx
+++ frontend/src/views/main/admin/Users/AdminUserDetail.tsx
@@ -5,58 +5,60 @@ import {UserContext} from '../../../../contexts/UserContext';
 import apiHooks from '../../../../hooks/ApiHooks';
 
 const AdminUserDetail: React.FC = () => {
-	const {userid} = useParams<{userid: number}>();
-	const {user} = useContext(UserContext);
-	const [fetchUser, setFetchUser] = useState<any[]>([]);
+    const {userid} = useParams<{userid: string}>(); // Change the type to string
+    const {user} = useContext(UserContext);
+    const [fetchUser, setFetchUser] = useState<any>(null);
 
-	useEffect(() => {
-		if (user) {
-			// Get token from local storage
-			const token: string | null = localStorage.getItem('userToken');
-			if (!token) {
-				throw new Error('No token available');
-			}
+    useEffect(() => {
+        if (user) {
+            // Get token from local storage
+            const token: string | null = localStorage.getItem('userToken');
+            if (!token) {
+                throw new Error('No token available');
+            }
 
-			// Create an async function inside the effect
-			const fetchUser = async () => {
-				const fetchedUser = await apiHooks.fetchUserById(userid, token);
-				setFetchUser(fetchedUser[0]);
-				console.log(fetchedUser[0]);
-			};
+            // Create an async function inside the effect
+            const fetchUserData = async () => {
+                const fetchedUser = await apiHooks.fetchUserById(Number(userid), token); // Convert userid to number
+                setFetchUser(fetchedUser[0]);
+                console.log(fetchedUser[0]);
+            };
 
-			// Call the async function
-			fetchUser();
-		}
-	}, [user]);
+            // Call the async function
+            fetchUserData();
+        }
+    }, [user, userid]);
 
-	return (
-		<div className="md:flex md:space-x-4">
-			<div className="md:w-1/2">
-				{fetchUser.first_name && (
-					<Typography variant="h6">First Name: {fetchUser.first_name}</Typography>
-				)}
-				{fetchUser.last_name && (
-					<Typography variant="h6">Last Name: {fetchUser.last_name}</Typography>
-				)}
-				{fetchUser.email && (
-					<Typography variant="h6">Email: {fetchUser.email}</Typography>
-				)}
-			</div>
-			<div className="md:w-1/2">
-				{fetchUser.role && (
-					<Typography variant="h6">Role: {fetchUser.role}</Typography>
-				)}
-				{fetchUser.studentnumber && (
-					<Typography variant="h6">
-						Student Number: {fetchUser.studentnumber}
-					</Typography>
-				)}
-				{fetchUser.userid && (
-					<Typography variant="h6">User ID: {fetchUser.userid}</Typography>
-				)}
-			</div>
-		</div>
-	);
+    return (
+        fetchUser && (
+            <div className="md:flex md:space-x-4">
+                <div className="md:w-1/2">
+                    {fetchUser.first_name && (
+                        <Typography variant="h6">First Name: {fetchUser.first_name}</Typography>
+                    )}
+                    {fetchUser.last_name && (
+                        <Typography variant="h6">Last Name: {fetchUser.last_name}</Typography>
+                    )}
+                    {fetchUser.email && (
+                        <Typography variant="h6">Email: {fetchUser.email}</Typography>
+                    )}
+                </div>
+                <div className="md:w-1/2">
+                    {fetchUser.role && (
+                        <Typography variant="h6">Role: {fetchUser.role}</Typography>
+                    )}
+                    {fetchUser.studentnumber && (
+                        <Typography variant="h6">
+                            Student Number: {fetchUser.studentnumber}
+                        </Typography>
+                    )}
+                    {fetchUser.userid && (
+                        <Typography variant="h6">User ID: {fetchUser.userid}</Typography>
+                    )}
+                </div>
+            </div>
+        )
+    );
 };
 
 export default AdminUserDetail;
--- frontend/src/views/main/admin/Users/AdminUserModify.tsx
+++ frontend/src/views/main/admin/Users/AdminUserModify.tsx
@@ -7,26 +7,8 @@ const AdminUserModify: React.FC = () => {
 	// const {id} = useParams<{id: string}>();
 
 	// Replace with actual data fetching
-	const Teacher = {
-		name: `Teacher ${id}`,
-		course: 'Course Name',
-	};
 
-	return (
-		<div className="bg-gray-100 p-5">
-			{/* <TeacherStudentDetail></TeacherStudentDetail> */}
-			<div className="m-4 bg-white rounded shadow-lg w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto">
-				<div className="px-6 py-4">
-					<div className="font-bold text-xl mb-2">{Teacher.name}</div>
-					<p className="text-gray-700 text-base">Enrolled in: {Teacher.course}</p>
-					{/* <MainViewButton
-						path={`/adming/users/${id}/modify`}
-						text="Modify details"
-					/> */}
-				</div>
-			</div>
-		</div>
-	);
+	return <div className="bg-gray-100 p-5">sacd</div>;
 };
 
 export default AdminUserModify;
