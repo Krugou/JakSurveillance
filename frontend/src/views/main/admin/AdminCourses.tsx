a9d46f73685323c208ec6c64bb16b4ee9ef61090
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-23 21:06:52 +0200

    Add user context and fetch courses from API

--- frontend/src/views/main/admin/AdminCourses.tsx
+++ frontend/src/views/main/admin/AdminCourses.tsx
@@ -1,42 +1,81 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import MainViewButton from '../../../components/main/buttons/MainViewButton';
-
+import {UserContext} from '../../../contexts/UserContext';
+import apiHooks from '../../../hooks/apiHooks';
 const AdminCourses: React.FC = () => {
+	const {user} = useContext(UserContext);
 	const courseOptions = ['Course 1', 'Course 2', 'Course 3']; // Replace with your course options
 	const [selectedCourse, setSelectedCourse] = useState(courseOptions[0]);
-
+	const [courses, setCourses] = useState<Course[]>([]);
 	const handleCourseChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
 		setSelectedCourse(event.target.value);
 	};
 
+	useEffect(() => {
+		if (user) {
+			// Get token from local storage
+			const token: string | null = localStorage.getItem('userToken');
+			if (!token) {
+				throw new Error('No token available');
+			}
+
+			// Create an async function inside the effect
+			const fetchCourses = async () => {
+				const fetchedCourses = await apiHooks.getCourses(token);
+				console.log(
+					'🚀 ~ file: AdminCourses.tsx:25 ~ fetchCourses ~ fetchedCourses:',
+					fetchedCourses,
+				);
+				setCourses(fetchedCourses);
+			};
+
+			// Call the async function
+			fetchCourses();
+		}
+	}, [user]);
+
 	return (
-		<div className="bg-gray-100 p-5">
-			<div className="container m-auto w-1/3">
-				<h1 className="text-2xl font-semibold mb-4">School Courses</h1>
-				<div className="mb-4">
-					<label htmlFor="courseSelect">Select a Course:</label>
-					<select
-						id="courseSelect"
-						value={selectedCourse}
-						onChange={handleCourseChange}
-						className="border p-2 rounded-md w-full"
-					>
-						{courseOptions.map((course, index) => (
-							<option key={index} value={course}>
-								{course}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="p-4 bg-white rounded-md">
-					<h2 className="text-xl font-semibold mb-2">Selected Course</h2>
-					<div className="p-2 border flex justify-between items-center rounded-md">
-						{selectedCourse}
-						<div>
-							<MainViewButton path="/admin/courses/:id" text="See details" />
-							<MainViewButton path="/admin/courses/:id/modify" text="Modify details" />
-						</div>
-					</div>
+		<div className="relative">
+			<div className="h-1/2 relative overflow-x-scroll">
+				<div className="max-h-96 h-96 overflow-y-scroll relative">
+					<table className="table-auto w-full">
+						<thead className="sticky top-0 bg-white z-10">
+							<tr>
+								{[
+									'name',
+									'code',
+									'start_date',
+									'end_date',
+									'student_group',
+									'topics',
+									'instructors',
+								].map((key, index) => (
+									<th key={index} className="px-4 py-2">
+										{key}
+									</th>
+								))}
+							</tr>
+						</thead>
+						<tbody>
+							{courses.map((course, index) => (
+								<tr key={index} className="cursor-pointer hover:bg-gray-200">
+									{[
+										'name',
+										'code',
+										'start_date',
+										'end_date',
+										'student_group',
+										'topics',
+										'instructors',
+									].map((key, innerIndex) => (
+										<td key={innerIndex} className="border px-2 py-2">
+											{Array.isArray(course[key]) ? course[key].join(', ') : course[key]}
+										</td>
+									))}
+								</tr>
+							))}
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
