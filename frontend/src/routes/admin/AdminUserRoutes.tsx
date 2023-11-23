0d90d06c2871dc88b50dc7fd2853877bd54a26ea
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-23 18:58:44 +0200

    Add routes for AdminUserDetail and AdminUserModify

--- frontend/src/routes/admin/AdminUserRoutes.tsx
+++ frontend/src/routes/admin/AdminUserRoutes.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import AdminMainView from '../../views/main/admin/AdminMainView';
 import AdminUsers from '../../views/main/admin/AdminUsers';
+import AdminUserDetail from '../../views/main/admin/Users/AdminUserDetail';
+import AdminUserModify from '../../views/main/admin/Users/AdminUserModify';
 
 const AdminUserRoutes: React.FC = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<AdminUsers />} />
+			<Route path="/:id" element={<AdminUserDetail />} />
+			<Route path="/:id/modify" element={<AdminUserModify />} />
 			<Route path="*" element={<AdminMainView />} />
 		</Routes>
 	);
