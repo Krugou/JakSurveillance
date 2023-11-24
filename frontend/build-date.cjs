fc96cf3dd6c0de8f9b5a2c4ad7f8c3ed33afa255
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-24 14:59:41 +0200

    Add check for .env file existence and create it if necessary

--- frontend/build-date.cjs
+++ frontend/build-date.cjs
@@ -4,6 +4,11 @@ const path = require('path');
 const date = new Date().toISOString();
 const envFilePath = path.resolve(__dirname, '.env');
 
+// Check if .env file exists, if not, create it
+if (!fs.existsSync(envFilePath)) {
+	fs.writeFileSync(envFilePath, '');
+}
+
 let envContent = fs.readFileSync(envFilePath, 'utf8');
 envContent = envContent.replace(/VITE_REACT_APP_BUILD_DATE=.*\n/g, '');
 envContent += `VITE_REACT_APP_BUILD_DATE=${date}\n`;
