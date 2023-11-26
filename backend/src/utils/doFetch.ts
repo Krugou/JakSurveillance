9ec2f9b9b2efd164a43e838536f846a6cef7664b
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-26 22:20:52 +0200

    Add doFetch utility function

---
+++ backend/src/utils/doFetch.ts
@@ -0,0 +1,41 @@
+import fetch, {RequestInit, Response} from 'node-fetch';
+
+const doFetch = async (
+	url: string,
+	options: RequestInit = {},
+): Promise<any> => {
+	if (!url) {
+		throw new Error('URL is required');
+	}
+	console.log('Fetching', url);
+	let response: Response;
+	try {
+		response = await fetch(url, options);
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			console.error(`Fetch failed: ${error.message}`);
+		}
+		throw error;
+	}
+
+	if (!response.ok) {
+		const errorText = `HTTP error! status: ${response.status}`;
+		console.error(errorText);
+		throw new Error(errorText);
+	}
+
+	let data: any;
+	try {
+		data = await response.json();
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			console.error(`Failed to parse response as JSON: ${error.message}`);
+		}
+		throw error;
+	}
+
+	return data;
+};
+
+
+export default doFetch;
