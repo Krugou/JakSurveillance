e307340a33e6f2135bd8fc7b5b94185bda05524c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-27 21:06:15 +0200

    Refactor CourseStudents component and add comments

--- frontend/src/components/main/course/attendance/CourseStudents.tsx
+++ frontend/src/components/main/course/attendance/CourseStudents.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Socket} from 'socket.io-client';
+// Define the props for the CourseStudents component
 interface Props {
 	coursestudents: {
 		first_name: string;
@@ -8,14 +9,23 @@ interface Props {
 		studentnumber: string;
 	}[];
 	socket: Socket | null;
-	lectureid: number;
+	lectureid: string;
 }
-
+/**
+ * CourseStudents component.
+ *
+ * @param {Object} props - The props for the component.
+ * @param {Array} props.coursestudents - The students in the course.
+ * @param {Socket | null} props.socket - The socket connection.
+ * @param {string} props.lectureid - The ID of the lecture.
+ * @returns {React.FC<Props>} The CourseStudents component.
+ */
 const CourseStudents: React.FC<Props> = ({
 	coursestudents,
 	socket,
 	lectureid,
 }) => {
+	// Define state and refs const
 	const scrollContainerRef = useRef<HTMLDivElement | null>(null);
 	const scrollPositionRef = useRef<number>(0);
 	const [bounceGroup, setBounceGroup] = useState<number | null>(0);
@@ -25,71 +35,120 @@ const CourseStudents: React.FC<Props> = ({
 	const [startX, setStartX] = useState<number>(0);
 	const [scrollLeft, setScrollLeft] = useState<number>(0);
 	const [remainingTime, setRemainingTime] = useState<number>(60);
+	const scrollInterval = useRef<number | null>(null);
+	const scrollDirectionRef = useRef(1);
 
+	// Use an effect hook to handle countdown and lecture finish
 	useEffect(() => {
+		// Declare a variable to hold the ID of the timer
 		let timerId;
 
+		// If there are no students in the course and the remaining time is greater than 0
 		if (coursestudents.length === 0 && remainingTime > 0) {
+			// Start a timer that decreases the remaining time by 1 every second
 			timerId = setInterval(() => {
 				setRemainingTime(prevTime => prevTime - 1);
 			}, 1000);
-		} else if (remainingTime === 0) {
+		}
+		// If the remaining time is 0
+		else if (remainingTime === 0) {
+			// If the socket is defined, emit a 'lecturefinishedwithbutton' event with the lecture ID
 			if (socket) {
 				socket.emit('lecturefinishedwithbutton', lectureid);
 			}
-			setRemainingTime(60); // Reset the timer
+			// Reset the remaining time to 5
+			setRemainingTime(5);
 		}
 
+		// Return a cleanup function that clears the timer when the component unmounts
 		return () => {
 			if (timerId) {
 				clearInterval(timerId);
 			}
 		};
-	}, [coursestudents, remainingTime, socket, lectureid]);
+	}, [coursestudents, remainingTime, socket, lectureid]); // This effect depends on the coursestudents, remainingTime, socket, and lectureid variables
 
+	// Function to handle the mouse down event
 	const onMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+		// Check if scrollContainerRef is defined
 		if (scrollContainerRef.current) {
+			// If the user presses the mouse button, set the isDragging state to true
 			setIsDragging(true);
+
+			// Calculate the initial X position of the mouse when the button is pressed
+			// Subtract the left offset of the scroll container to get the position relative to the container
 			setStartX(e.pageX - scrollContainerRef.current.offsetLeft);
+
+			// Store the current scroll position of the scroll container
 			setScrollLeft(scrollContainerRef.current.scrollLeft);
 		}
 	};
 
+	// Function to handle the mouse end event
 	const onMouseEnd = () => {
+		// When the mouse button is released, set the isDragging state to false
 		setIsDragging(false);
 	};
+
+	// Use an effect hook to handle bounce group changes
 	useEffect(() => {
+		// Set an interval to change the bounce group every 4 seconds
 		const interval = window.setInterval(() => {
+			// Update the bounce group state
+			// If the previous group is not null, increment it by 1 and take the remainder when divided by 2
+			// This ensures that the bounce group alternates between 0 and 1
+			// If the previous group is null, set it to 0
 			setBounceGroup(prevGroup => (prevGroup !== null ? (prevGroup + 1) % 2 : 0));
 		}, 4000); // Change every 4 seconds
 
+		// Return a cleanup function that clears the interval when the component unmounts
 		return () => clearInterval(interval);
-	}, []);
+	}, []); // Empty dependency array means this effect runs once on mount and cleanup on unmount
+
+	// Function to handle mouse move event
 	const onMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+		// If not dragging or scrollContainerRef is not set, exit the function
 		if (!isDragging || !scrollContainerRef.current) return;
+
+		// Prevent the default event behavior
 		e.preventDefault();
+
+		// Calculate the new x position of the mouse
 		const x = e.pageX - scrollContainerRef.current.offsetLeft;
-		const walk = (x - startX) * 3; //scroll-fast
+
+		// Calculate the distance the mouse has moved (walk)
+		// Multiply by 3 to increase the speed of the scroll (scroll-fast)
+		const walk = (x - startX) * 3;
+
+		// Update the scroll position of the scroll container
 		scrollContainerRef.current.scrollLeft = scrollLeft - walk;
 	};
-	const scrollInterval = useRef<number | null>(null);
 
-	const scrollDirectionRef = useRef(1);
+	// Function to make the scroll container scroll slowly
 	const scrollSlowly = () => {
 		const element = scrollContainerRef.current;
 		if (!element) return;
+		// Set an interval to continuously scroll the container
 		scrollInterval.current = window.setInterval(() => {
+			// If we've reached the right end of the container, change direction to left
 			if (scrollPositionRef.current >= element.scrollWidth - element.clientWidth) {
-				scrollDirectionRef.current = -1; // Change direction to left
-			} else if (scrollPositionRef.current <= 0) {
-				scrollDirectionRef.current = 1; // Change direction to right
+				scrollDirectionRef.current = -1;
 			}
+			// If we've reached the left end of the container, change direction to right
+			else if (scrollPositionRef.current <= 0) {
+				scrollDirectionRef.current = 1;
+			}
+			// Update the scroll position
 			scrollPositionRef.current += scrollDirectionRef.current;
+			// Apply the new scroll position to the container
 			element.scrollLeft = scrollPositionRef.current;
 		}, 70);
 	};
+
+	// Effect hook to start the slow scroll when the component mounts
 	useEffect(() => {
 		scrollSlowly();
+		// Cleanup function to clear the interval when the component unmounts
 		return () => {
 			if (scrollInterval.current !== null) {
 				clearInterval(scrollInterval.current);
@@ -129,7 +188,7 @@ const CourseStudents: React.FC<Props> = ({
 				<p className="">
 					{remainingTime > 0
 						? `All students are here. Auto closing in ${remainingTime} seconds`
-						: 'All students saved'}
+						: 'Closing lecture...'}
 				</p>
 			) : (
 				<div className={`   whitespace-nowrap `}>
@@ -164,7 +223,6 @@ const CourseStudents: React.FC<Props> = ({
 										const studentName = `${student.first_name} ${student.last_name}`;
 										const confirmMessage = `Are you sure you want to add ${studentName} as attended?`;
 										if (window.confirm(confirmMessage)) {
-											console.log(socket);
 											socket.emit('manualstudentinsert', student.studentnumber, lectureid);
 										}
 									}
