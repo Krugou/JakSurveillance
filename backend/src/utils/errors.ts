6019c37b5d828ddd45ec1ff7f248df77cbd2ff87
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-27 21:31:42 +0200

    Add HttpError class to handle HTTP errors

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -1,8 +1,16 @@
+class HttpError extends Error {
+	status: number;
+
+	constructor(message: string, status: number) {
+		super(message); // Call the parent constructor with the message parameter
+		this.status = status; // Add the status property
+	}
+}
+
 const httpError = (message: string, status: number) => {
-  console.log('error used');
-  const err = new Error(message);
-  (err as any).status = status;
-  return err as Error & { status: number };
+	console.log('error used');
+	const err = new HttpError(message, status);
+	return err;
 };
 
 export default httpError;
