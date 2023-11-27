8559095116f1effeea0d0d31e7efd2fc867f0c4a
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-27 17:06:24 +0200

    Add export functionality for attendance data

--- frontend/src/views/main/teacher/Students/TeacherStudentAttendances.tsx
+++ frontend/src/views/main/teacher/Students/TeacherStudentAttendances.tsx
@@ -92,19 +92,19 @@ const TeacherStudentCourseAttendance: React.FC = () => {
 		setSortOption(event.target.value);
 	};
 
+	// Function to create tables for PDF or Excel
 	const createTables = mode => {
-		let tableHeaders;
 		let tableData;
+		const tableHeaders = [
+			'Date',
+			'Student',
+			'Teacher',
+			'Time of Day',
+			'Topic',
+			'Status',
+		];
+		// If mode is 'pdf', set the table headers and data accordingly
 		if (mode === 'pdf') {
-			tableHeaders = [
-				'Date',
-				'Student',
-				'Teacher',
-				'Time of Day',
-				'Topic',
-				'Status',
-			];
-
 			tableData = filteredAttendanceData.map(attendance => [
 				new Date(attendance.start_date).toLocaleDateString(),
 				student ? `${student.first_name} ${student.last_name}` : '',
@@ -114,14 +114,7 @@ const TeacherStudentCourseAttendance: React.FC = () => {
 				attendance.status === 1 ? 'Present' : 'Absent',
 			]);
 		} else if (mode === 'excel') {
-			tableHeaders = [
-				'Date',
-				'Student',
-				'Teacher',
-				'Time of Day',
-				'Topic',
-				'Status',
-			];
+			// If mode is 'excel', set the table headers and data accordingly
 
 			tableData = filteredAttendanceData.map(attendance => ({
 				Date: new Date(attendance.start_date).toLocaleDateString(),
@@ -134,16 +127,22 @@ const TeacherStudentCourseAttendance: React.FC = () => {
 		}
 		return {tableHeaders, tableData};
 	};
+
+	// Function to export the attendance data to PDF
 	const exportToPDF = () => {
 		const doc = new jsPDF();
 		const imgWidth = 180; // Set the width as per your requirement
 		const imgHeight = (imgWidth * 1267) / 4961; // Calculate the height based on the aspect ratio
-		const imgX = 15;
-		const imgY = 10;
+		const imgX = 15; // Position the image X pixels from the left
+		const imgY = 10; // Position the image Y pixels from the top
 
+		// Add the Metropolia logo to the PDF
 		doc.addImage(metropolia_logo, 'PNG', imgX, imgY, imgWidth, imgHeight);
+
+		// Create the table
 		const {tableHeaders, tableData} = createTables('pdf');
 
+		// Add the table to the PDF
 		autoTable(doc, {
 			head: [tableHeaders],
 			body: tableData,
@@ -152,8 +151,9 @@ const TeacherStudentCourseAttendance: React.FC = () => {
 			didDrawPage: data => {
 				// Add header
 				doc.setFontSize(20);
-				doc.setTextColor(40);
-				doc.setFont('helvetica', 'normal');
+				doc.setTextColor(40); // Set text color to black
+				doc.setFont('helvetica', 'normal'); // Set font
+				// Add the course name and student name to the PDF
 				doc.text(
 					`${attendanceData[0].name} attendance for ${student?.first_name} ${student?.last_name}`,
 					data.settings.margin.left,
@@ -162,21 +162,29 @@ const TeacherStudentCourseAttendance: React.FC = () => {
 				doc.text(`Topics: ${sortOption}`, data.settings.margin.left, 85);
 			},
 		});
+		// Save the PDF
 		doc.save(`${student?.first_name} ${student?.last_name}'s attendance.pdf`);
 		toast.success(
 			`${student?.first_name} ${student?.last_name}'s attendance PDF for topics: ${sortOption} downloaded successfully. `,
 			{
-				position: toast.POSITION.TOP_CENTER, // or toast.POSITION.BOTTOM_CENTER
-				autoClose: 7000, // Display the toast for 5 seconds
+				position: toast.POSITION.TOP_CENTER, // position the toast at the top center
+				autoClose: 7000, // Display the toast for 7 seconds
 			},
 		);
 	};
 
+	// Function to export the attendance data to Excel
 	const exportToExcel = () => {
+		// Create the table
 		const {tableHeaders, tableData} = createTables('excel');
+		// Create a worksheet
 		const ws = XLSX.utils.json_to_sheet(tableData, {header: tableHeaders});
+		// Create a workbook
 		const wb = XLSX.utils.book_new();
+		// Add the worksheet to the workbook
 		XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+		// Save the workbook
 		XLSX.writeFile(
 			wb,
 			`${student?.first_name} ${student?.last_name}_${attendanceData[0].name}_${sortOption} attendance.xlsx`,
@@ -184,8 +192,8 @@ const TeacherStudentCourseAttendance: React.FC = () => {
 		toast.success(
 			`${student?.first_name} ${student?.last_name}'s attendance EXCEL for topics: ${sortOption} downloaded successfully. `,
 			{
-				position: toast.POSITION.TOP_CENTER, // or toast.POSITION.BOTTOM_CENTER
-				autoClose: 7000, // Display the toast for 5 seconds
+				position: toast.POSITION.TOP_CENTER, // position the toast at the top center
+				autoClose: 7000, // Display the toast for 7 secondss
 			},
 		);
 	};
