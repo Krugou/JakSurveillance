899bae962d61e284a81df71d232f8813015d9190
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-27 22:28:04 +0200

    Refactor AdminUserModify component to fetch user data and display it in EditUserView

--- frontend/src/views/main/admin/Users/AdminUserModify.tsx
+++ frontend/src/views/main/admin/Users/AdminUserModify.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-// import {useParams} from 'react-router-dom';
-// import MainViewButton from '../../../components/main/buttons/MainViewButton';
-// import TeacherStudentDetail from '../teacher/Students/TeacherStudentDetail';
+import React, {useContext, useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import EditUserView from '../../../../components/main/admin/EditUserView';
+import {UserContext} from '../../../../contexts/UserContext';
+import apiHooks from '../../../../hooks/ApiHooks';
 
 const AdminUserModify: React.FC = () => {
-	// const {id} = useParams<{id: string}>();
+	const {userid} = useParams<{userid: string}>(); // Change the type to string
+	const {user} = useContext(UserContext);
+	const [fetchUser, setFetchUser] = useState<any>(null);
 
-	// Replace with actual data fetching
+	useEffect(() => {
+		if (user) {
+			// Get token from local storage
+			const token: string | null = localStorage.getItem('userToken');
+			if (!token) {
+				throw new Error('No token available');
+			}
 
-	return <div className="bg-gray-100 p-5">sacd</div>;
+			// Create an async function inside the effect
+			const fetchUserData = async () => {
+				const fetchedUser = await apiHooks.fetchUserById(Number(userid), token); // Convert userid to number
+				setFetchUser(fetchedUser[0]);
+				console.log(fetchedUser[0]);
+			};
+
+			// Call the async function
+			fetchUserData();
+		}
+	}, [user, userid]);
+
+	return fetchUser && <EditUserView user={fetchUser} />;
 };
 
 export default AdminUserModify;
