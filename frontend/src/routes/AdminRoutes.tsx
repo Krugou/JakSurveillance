6d96d9f723359def925d7a3416aa67c596133069
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-28 21:21:35 +0200

    Add AdminHelpVideos route to AdminRoutes

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import AdminHelpVideos from '../views/main/admin/AdminHelpVideos.tsx';
 import AdminMainView from '../views/main/admin/AdminMainView.tsx';
-import AdminCoursesRoutes from './admin/AdminCourseRoutes';
-import AdminUserRoutes from './admin/AdminUserRoutes';
 import AdminProfile from '../views/main/admin/AdminProfile.tsx';
+import AdminCoursesRoutes from './admin/AdminCourseRoutes';
 import AdminSettingsRoutes from './admin/AdminSettingsRoutes.tsx';
+import AdminUserRoutes from './admin/AdminUserRoutes';
 const AdminRoutes = () => {
 	return (
 		<Routes>
@@ -12,6 +13,7 @@ const AdminRoutes = () => {
 			<Route path="courses/*" element={<AdminCoursesRoutes />} />
 			<Route path="users/*" element={<AdminUserRoutes />} />
 			<Route path="profile" element={<AdminProfile />} />
+			<Route path="helpvideos" element={<AdminHelpVideos />} />
 			<Route path="settings/*" element={<AdminSettingsRoutes />} />
 			<Route path="*" element={<AdminMainView />} />
 		</Routes>
