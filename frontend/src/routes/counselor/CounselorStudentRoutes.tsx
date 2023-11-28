7e010901fde544d91071aa08e71030af86546b0a
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-11-28 18:56:40 +0200

    Add route for CounselorStudentAttendances component

--- frontend/src/routes/counselor/CounselorStudentRoutes.tsx
+++ frontend/src/routes/counselor/CounselorStudentRoutes.tsx
@@ -3,12 +3,17 @@ import {Route, Routes} from 'react-router-dom';
 import CounselorStudentsView from '../../views/main/counselor/students/CounselorStudentsView';
 import CounselorStudentDetail from '../../views/main/counselor/students/CounselorStudentDetail';
 import CounselorMainView from '../../views/main/counselor/CounselorMainView';
+import CounselorStudentAttendances from '../../views/main/counselor/students/CounselorStudentAttendances';
 const CounselorStudentRoutes: React.FC = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<CounselorStudentsView />} />
 			<Route path=":id" element={<CounselorStudentDetail />} />
 			<Route path="*" element={<CounselorMainView />} />
+			<Route
+				path="attendance/:usercourseid"
+				element={<CounselorStudentAttendances />}
+			/>
 		</Routes>
 	);
 };
---
+++ frontend/src/views/main/counselor/students/CounselorStudentAttendances.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import TeacherStudentCourseAttendance from '../../teacher/Students/TeacherStudentAttendances';
+const CounselorStudentAttendances: React.FC = () => {
+	return <TeacherStudentCourseAttendance />;
+};
+
+export default CounselorStudentAttendances;
