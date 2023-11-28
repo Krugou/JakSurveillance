514a0b5fe66c81cc0cbe1488b13ec1b670db7783
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-28 21:19:53 +0200

    Remove "My Courses" button from AdminProfile component

--- frontend/src/views/main/admin/AdminProfile.tsx
+++ frontend/src/views/main/admin/AdminProfile.tsx
@@ -17,12 +17,7 @@ const AdminProfile: React.FC = () => {
 			<div className="text-md sm:text-xl mb-4">
 				<ProfileInfo user={user} />
 			</div>
-			<button
-				className="px-4 py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-700"
-				onClick={() => navigate('/admin/courses')}
-			>
-				My Courses
-			</button>
+		
 		</div>
 	);
 };
