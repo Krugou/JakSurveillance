6318d54ddde042c6af3a13c09083a92b9bbc9742
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-28 21:21:58 +0200

    Update title in AdminHelpVideos component

--- frontend/src/views/main/admin/AdminHelpVideos.tsx
+++ frontend/src/views/main/admin/AdminHelpVideos.tsx
@@ -5,7 +5,7 @@ const AdminHelpVideos: React.FC = () => {
 	return (
 		<div className="bg-gray-100 p-5">
 			<h1 className="text-2xl font-semibold mb-10 text-center">
-				Teacher Help Videos
+				Admin Help Videos
 			</h1>
 			<div className="space-y-6 flex flex-col">
 				<VideoDropdown title="How do I create a course?" src="video-url-1.mp4" />
