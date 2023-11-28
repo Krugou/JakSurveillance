b1338f1fa95d69ee0186b18213a6ba9c7d302835
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-29 01:56:10 +0200

    Refactor secure routes to check user role before fetching students

--- backend/src/routes/secureroutes.ts
+++ backend/src/routes/secureroutes.ts
@@ -1,30 +1,25 @@
 import express, {Request, Response, Router} from 'express';
 import usermodel from '../models/usermodel.js';
+import checkUserRole from '../utils/checkRole.js';
 const router: Router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
 	//console.log('🚀 ~ file: secureroutes.ts:8 ~ router.get ~ req.user:', req.user);
 	res.json(req.user);
 });
-router.get('/students', async (req: Request, res: Response) => {
-	try {
-		console.log('asdasdad');
-		console.log(req.user.role);
-		if (
-			req.user.role !== 'admin' &&
-			req.user.role !== 'counselor' &&
-			req.user.role !== 'teacher'
-		) {
-			console.log('forbidden');
-			return res.status(403).json({message: 'Forbidden'});
+router.get(
+	'/students',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (req: Request, res: Response) => {
+		try {
+			const users = await usermodel.fetchAllStudents();
+			console.log(users, 'users');
+			res.send(users);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({message: 'Internal server error'});
 		}
-		const users = await usermodel.fetchAllStudents();
-		console.log(users, 'users');
-		res.send(users);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({message: 'Internal server error'});
-	}
-});
+	},
+);
 
 export default router;
