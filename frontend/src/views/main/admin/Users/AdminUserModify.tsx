2b3a1c4ebd5d38facc8f391f6abcda1db3f29974
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-28 20:52:07 +0200

    Refactor AdminUserModify component and add save functionality

--- frontend/src/views/main/admin/Users/AdminUserModify.tsx
+++ frontend/src/views/main/admin/Users/AdminUserModify.tsx
@@ -1,13 +1,27 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
+import {toast} from 'react-toastify';
 import EditUserView from '../../../../components/main/admin/EditUserView';
 import {UserContext} from '../../../../contexts/UserContext';
 import apiHooks from '../../../../hooks/ApiHooks';
-
+interface User {
+	userid: number;
+	username: string | null;
+	email: string;
+	staff: number;
+	first_name: string;
+	last_name: string;
+	created_at: string;
+	studentnumber: number;
+	studentgroupid: number;
+	roleid: number;
+	GDPR: number;
+	role: string;
+}
 const AdminUserModify: React.FC = () => {
 	const {userid} = useParams<{userid: string}>(); // Change the type to string
 	const {user} = useContext(UserContext);
-	const [fetchUser, setFetchUser] = useState<any>(null);
+	const [modifyUser, setModifyUser] = useState<User | null>(null);
 
 	useEffect(() => {
 		if (user) {
@@ -18,18 +32,38 @@ const AdminUserModify: React.FC = () => {
 			}
 
 			// Create an async function inside the effect
-			const fetchUserData = async () => {
-				const fetchedUser = await apiHooks.fetchUserById(Number(userid), token); // Convert userid to number
-				setFetchUser(fetchedUser[0]);
-				console.log(fetchedUser[0]);
+			const ModifyUserData = async () => {
+				try {
+					const modifyUser = await apiHooks.fetchUserById(Number(userid), token);
+					setModifyUser(modifyUser[0]);
+					console.log(modifyUser[0]);
+				} catch (error) {
+					console.error('Failed to fetch user data:', error);
+					// handle the error appropriately, e.g., show a message to the user
+				}
 			};
 
 			// Call the async function
-			fetchUserData();
+			ModifyUserData();
 		}
 	}, [user, userid]);
+	const handleSave = async (editedUser: User) => {
+		// Get token from local storage
+		const token: string | null = localStorage.getItem('userToken');
+		if (!token) {
+			throw new Error('No token available');
+		}
 
-	return fetchUser && <EditUserView user={fetchUser} />;
+		try {
+			const response = await apiHooks.updateUser(token, editedUser);
+			toast.success(response.message);
+		} catch (error) {
+			toast.error('Failed to update user: ' + error.toString());
+			// handle the error appropriately, e.g., show a message to the user
+		}
+	};
+
+	return modifyUser && <EditUserView user={modifyUser} onSave={handleSave} />;
 };
 
 export default AdminUserModify;
