4ce6f46f65b7800c180ba5538033d8c714f1abb2
Krugou <59641229+Krugou@users.noreply.github.com>
2023-11-29 14:19:33 +0200

    Remove unused code and update type declaration in AdminUserDetail component

--- frontend/src/views/main/admin/Users/AdminUserDetail.tsx
+++ frontend/src/views/main/admin/Users/AdminUserDetail.tsx
@@ -1,7 +1,5 @@
-import EditIcon from '@mui/icons-material/Edit';
-import Button from '@mui/material/Button';
 import React, {useContext, useEffect, useState} from 'react';
-import {useNavigate, useParams} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import SimpleUserView from '../../../../components/main/admin/SimpleUserView';
 import {UserContext} from '../../../../contexts/UserContext';
 import apiHooks from '../../../../hooks/ApiHooks';
@@ -10,7 +8,6 @@ const AdminUserDetail: React.FC = () => {
 	const {userid} = useParams<{userid: string}>(); // Change the type to string
 	const {user} = useContext(UserContext);
 	const [fetchUser, setFetchUser] = useState<any>(null);
-	const navigate = useNavigate();
 
 	useEffect(() => {
 		if (user) {
@@ -32,22 +29,10 @@ const AdminUserDetail: React.FC = () => {
 		}
 	}, [user, userid]);
 
-	const handleEdit = () => {
-		navigate(`./modify`);
-	};
-
 	return (
 		fetchUser && (
 			<div className="flex flex-col md:space-x-4 justify-center items-center">
 				<SimpleUserView user={fetchUser} />
-				<Button
-					variant="contained"
-					color="primary"
-					startIcon={<EditIcon />}
-					onClick={handleEdit}
-				>
-					Edit
-				</Button>
 			</div>
 		)
 	);
