8bbb79157a9cf4e3e0f07cace7bc602adeba3924
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-04 22:13:33 +0200

    Refactor user authentication and routes

---
+++ backend/src/utils/auth.ts
@@ -0,0 +1,45 @@
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import usermodel from '../models/usermodel.js';
+import {User} from '../types.js';
+
+export const authenticate = (
+	req: Request,
+	res: Response,
+	next: (err?: any) => void,
+	newUsername: string,
+) => {
+	passport.authenticate(
+		'local',
+		{session: false},
+		(err: Error, user: User, _info: any) => {
+			if (err || !user) {
+				console.error(err);
+				return res.status(403).json({
+					message: 'User is not assigned to any courses, contact your teacher',
+				});
+			}
+			req.login(user, {session: false}, async err => {
+				if (err) {
+					console.error(err);
+					return res.status(403).json({
+						message: 'User is not assigned to any courses, contact your teacher',
+					});
+				}
+				if (user && !user.username) {
+					try {
+						console.log('USERNAME WAS UPDATED');
+						await usermodel.updateUsernameByEmail(user.email, newUsername);
+					} catch (error) {
+						console.error(error);
+					}
+					user.username = newUsername;
+				}
+				const token = jwt.sign(user as User, process.env.JWT_SECRET as string, {
+					expiresIn: '2h',
+				});
+				res.json({user, token});
+			});
+		},
+	)(req, res, next);
+};
