114ad821dd72884c2a2b8798ce6d309d9204a32d
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-04 21:37:25 +0200

    Add checkUserRole middleware to topicRoutes

--- backend/src/routes/course/topicRoutes.ts
+++ backend/src/routes/course/topicRoutes.ts
@@ -2,6 +2,7 @@ import express, {Request, Response, Router} from 'express';
 
 import TopicGroupController from '../../controllers/topicgroupcontroller.js';
 import TopicGroup from '../../models/topicgroupmodel.js';
+import checkUserRole from '../../utils/checkRole.js';
 
 const router: Router = express.Router();
 
@@ -26,59 +27,56 @@ router.post('/', async (req: Request, res: Response) => {
 		res.status(500).send('Server error: ' + err);
 	}
 });
-router.post('/update', async (req: Request, res: Response) => {
-	try {
-		const {topicGroup, topics, email} = req.body;
-		console.log(
-			'🚀 ~ file: topicRoutes.ts:32 ~ router.post ~ req.body:',
-			req.body,
-		);
-		if (!topicGroup) {
-			return res.status(400).send({message: 'Topic group is required'});
-		}
-		if (
-			!topics ||
-			topics.length === 0 ||
-			topics.every(topic => topic.trim() === '')
-		) {
-			return res.status(400).send({message: 'Topics are required'});
-		}
-		const topicGroupData = await TopicGroupController.updateTopicGroup(
-			topicGroup,
-			topics,
-			email,
-		);
-		res.status(200).send(topicGroupData);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Server error: ' + err);
-	}
-});
-router.post('/update/:usercourseid', async (req: Request, res: Response) => {
-	try {
-		if (
-			req.user?.role !== 'admin' &&
-			req.user?.role !== 'teacher' &&
-			req.user?.role !== 'counselor'
-		) {
-			return res.status(401).send({message: 'Unauthorized'});
-		}
+router.post(
+    '/update',
+    checkUserRole(['admin', 'counselor', 'teacher']),
+    async (req: Request, res: Response) => {
+        try {
+            const {topicGroup, topics, email} = req.body;
 
-		const {usercourseid} = req.params;
-		const {modifiedTopics} = req.body;
-		if (!modifiedTopics) {
-			return res.status(400).send({message: 'Topics are required'});
-		}
+            if (!topicGroup) {
+                return res.status(400).send({message: 'Topic group is required'});
+            }
+            if (
+                !topics ||
+                topics.length === 0 ||
+                topics.every((topic: string) => topic.trim() === '')
+            ) {
+                return res.status(400).send({message: 'Topics are required'});
+            }
+            const topicGroupData = await TopicGroupController.updateTopicGroup(
+                topicGroup,
+                topics,
+                email,
+            );
+            res.status(200).send(topicGroupData);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Server error: ' + err);
+        }
+    },
+);
+router.post(
+    '/update/:usercourseid',
+    checkUserRole(['admin', 'counselor', 'teacher']),
+    async (req: Request, res: Response) => {
+        try {
+            const usercourseid = parseInt(req.params.usercourseid);
+            const {modifiedTopics} = req.body;
+            if (!modifiedTopics) {
+                return res.status(400).send({message: 'Topics are required'});
+            }
 
-		const topicResponse = await TopicGroupController.updateUserCourseTopics(
-			usercourseid,
-			modifiedTopics,
-		);
-		res.status(200).send(topicResponse);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Server error: ' + err);
-	}
-});
+            const topicResponse = await TopicGroupController.updateUserCourseTopics(
+                usercourseid,
+                modifiedTopics,
+            );
+            res.status(200).send(topicResponse);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Server error: ' + err);
+        }
+    },
+);
 
 export default router;
