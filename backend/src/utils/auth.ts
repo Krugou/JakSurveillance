a78d2fa1f90d026b632205452276fefc8029867f
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-05 01:00:27 +0200

    Update authentication and passport modules

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,45 +1,42 @@
-import passport from 'passport';
+import {Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
+import passport from 'passport';
 import usermodel from '../models/usermodel.js';
 import {User} from '../types.js';
 
 export const authenticate = (
 	req: Request,
 	res: Response,
-	next: (err?: any) => void,
+	next: (err?: Error | null) => void,
 	newUsername: string,
 ) => {
-	passport.authenticate(
-		'local',
-		{session: false},
-		(err: Error, user: User, _info: any) => {
-			if (err || !user) {
+	passport.authenticate('local', {session: false}, (err: Error, user: User) => {
+		if (err || !user) {
+			console.error(err);
+			return res.status(403).json({
+				message: 'User is not assigned to any courses, contact your teacher',
+			});
+		}
+		req.login(user, {session: false}, async err => {
+			if (err) {
 				console.error(err);
 				return res.status(403).json({
 					message: 'User is not assigned to any courses, contact your teacher',
 				});
 			}
-			req.login(user, {session: false}, async err => {
-				if (err) {
-					console.error(err);
-					return res.status(403).json({
-						message: 'User is not assigned to any courses, contact your teacher',
-					});
+			if (user && !user.username) {
+				try {
+					console.log('USERNAME WAS UPDATED');
+					await usermodel.updateUsernameByEmail(user.email, newUsername);
+				} catch (error) {
+					console.error(error);
 				}
-				if (user && !user.username) {
-					try {
-						console.log('USERNAME WAS UPDATED');
-						await usermodel.updateUsernameByEmail(user.email, newUsername);
-					} catch (error) {
-						console.error(error);
-					}
-					user.username = newUsername;
-				}
-				const token = jwt.sign(user as User, process.env.JWT_SECRET as string, {
-					expiresIn: '2h',
-				});
-				res.json({user, token});
+				user.username = newUsername;
+			}
+			const token = jwt.sign(user as User, process.env.JWT_SECRET as string, {
+				expiresIn: '2h',
 			});
-		},
-	)(req, res, next);
+			res.json({user, token});
+		});
+	})(req, res, next);
 };
--- backend/src/utils/pass.ts
+++ backend/src/utils/pass.ts
@@ -1,5 +1,6 @@
 'use strict';
 import {config} from 'dotenv';
+import {User} from '../types.js';
 config();
 
 // Import necessary modules and dependencies
@@ -10,25 +11,40 @@ import UserModel from '../models/usermodel.js'; // Import the UserModel without 
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
+interface DoneFunction {
+	(
+		error: null | Error,
+		user?: false | User | undefined,
+		options?: {message: string},
+	): void;
+}
 // Define a local strategy for email and password login
 passport.use(
-	new Strategy(async (email: string, _password, done: any) => {
+	new Strategy(async (email: string, _password: string, done: DoneFunction) => {
 		try {
 			// Find a user in the database with the provided email
-			const user: any = await UserModel.getAllUserInfo(email);
+			const user: User | null = await UserModel.getAllUserInfo(email);
 
 			// Check if the user exists
 			console.log(email, 'IAWJDUOIAWDIOJAWD ');
-			if (user === undefined) {
+			if (user === null || user === undefined) {
 				return done(null, false, {message: 'Incorrect username.'});
 			}
 
 			return done(null, user, {message: 'Logged In Successfully'});
 		} catch (err) {
-			return done(err);
+			return done(err instanceof Error ? err : new Error(String(err)));
 		}
 	}),
 );
+
+interface JwtPayload {
+	[key: string]: unknown;
+}
+
+interface DoneJwtFunction {
+	(error: unknown, user?: JwtPayload): void;
+}
 
 // Define a JWT strategy for handling JSON Web Tokens
 passport.use(
@@ -37,19 +53,11 @@ passport.use(
 			jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
 			secretOrKey: process.env.JWT_SECRET as string, // Assert process.env.JWT_SECRET as a string
 		},
-		(jwtPayload: any, done: Function) => {
+		(jwtPayload: JwtPayload, done: DoneJwtFunction) => {
 			//console.log('JWTStrategy', jwtPayload); // Log the JWT payload
 			done(null, jwtPayload); // Pass the JWT payload as the authenticated user
 		},
 	),
 );
-
-export interface User {
-	username: string;
-	email: string;
-	staff: number;
-	first_name: string;
-	last_name: string;
-}
 
 export default passport; // Export passport as the default export
