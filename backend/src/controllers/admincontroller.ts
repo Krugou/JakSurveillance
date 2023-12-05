a44ea2fac62ee2504883e930f2ebb55b869ed540
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-05 03:07:20 +0200

    Refactor adminController interface and methods
    This commit refactors the `adminController` interface and methods in the `admincontroller.ts` file. The `getServerSettings` method now returns the `serverSettings` variable instead of sending a response. The `updateServerSettings` method now accepts parameters directly instead of using `req.body`. These changes improve code readability and maintainability.

--- backend/src/controllers/admincontroller.ts
+++ backend/src/controllers/admincontroller.ts
@@ -1,26 +1,35 @@
-import {Request, Response} from 'express';
 import createPool from '../config/createPool.js';
 import ServerSettingsModel from '../models/serversettingsmodel.js';
 
 const pool = createPool('ADMIN');
+
 interface AdminController {
-	getServerSettings: (_req: Request, res: Response) => Promise<any>;
-	updateServerSettings: (req: Request, res: Response) => Promise<any>;
+	getServerSettings: () => Promise<any>;
+	updateServerSettings: (
+		speedofhash: any,
+		leewayspeed: any,
+		timeouttime: any,
+		attendancethreshold: any,
+	) => Promise<any>;
 }
+
 const adminController: AdminController = {
-	async getServerSettings(_req: Request, res: Response) {
+	async getServerSettings() {
 		try {
 			const serverSettings = await ServerSettingsModel.getServerSettings(pool);
-			res.status(200).send(serverSettings[0][0]);
+			return serverSettings; // use the serverSettings variable
 		} catch (error) {
 			console.error(error);
-			res.status(500).json({message: 'Internal server error'});
+			throw error;
 		}
 	},
-	async updateServerSettings(req: Request, res: Response) {
+	async updateServerSettings(
+		speedofhash,
+		leewayspeed,
+		timeouttime,
+		attendancethreshold,
+	) {
 		try {
-			const {speedofhash, leewayspeed, timeouttime, attendancethreshold} =
-				req.body;
 			await ServerSettingsModel.updateServerSettings(
 				pool,
 				speedofhash,
@@ -28,10 +37,9 @@ const adminController: AdminController = {
 				timeouttime,
 				attendancethreshold,
 			);
-			res.status(200).send({message: 'Server settings updated successfully'});
 		} catch (error) {
 			console.error(error);
-			res.status(500).json({message: 'Internal server error'});
+			throw error;
 		}
 	},
 };
