0b4354617497b1bbf9e7886b6407dbd57896f759
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-12-07 19:45:11 +0200

    Fix bug in login functionality

--- backend/src/routes/courseroutes.ts
+++ backend/src/routes/courseroutes.ts
@@ -18,204 +18,234 @@ const router: Router = express.Router();
 router.use('/attendance', attendanceRoutes);
 router.use('/topics', topicRoutes);
 
-router.post('/check', express.json(), async (req: Request, res: Response) => {
-	const {codes} = req.body;
+router.post(
+	'/check',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	express.json(),
+	async (req: Request, res: Response) => {
+		const {codes} = req.body;
 
-	try {
-		const data = await openData.checkOpenDataRealization(codes);
+		try {
+			const data = await openData.checkOpenDataRealization(codes);
 
-		// Check if message is "No results"
-		if ((data as {message?: string}).message === 'No results') {
-			res.status(404).json({
-				exists: false,
-			});
-			return;
+			// Check if message is "No results"
+			if ((data as {message?: string}).message === 'No results') {
+				res.status(404).json({
+					exists: false,
+				});
+				return;
+			}
+
+			res.status(200).json({
+				exists: true,
+			}); // send the data as the response
+		} catch (error) {
+			console.error('Error:', error);
+			res.status(500).send('Internal server error');
 		}
+	},
+);
+router.post(
+	'/checkcode/:code',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (req: Request, res: Response) => {
+		const {code} = req.params;
 
-		res.status(200).json({
-			exists: true,
-		}); // send the data as the response
-	} catch (error) {
-		console.error('Error:', error);
-		res.status(500).send('Internal server error');
-	}
-});
-router.post('/checkcode/:code', async (req: Request, res: Response) => {
-	const {code} = req.params;
+		try {
+			const exists = await course.findByCode(code);
+			res.status(200).json({exists});
+		} catch (error) {
+			console.error('Error:', error);
+			res.status(500).send('Internal server error');
+		}
+	},
+);
+router.post(
+	'/checkreservations/',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (req: Request, res: Response) => {
+		const {code = '', studentGroup = ''} = req.body;
 
-	try {
-		const exists = await course.findByCode(code);
-		res.status(200).json({exists});
-	} catch (error) {
-		console.error('Error:', error);
-		res.status(500).send('Internal server error');
-	}
-});
-router.post('/checkreservations/', async (req: Request, res: Response) => {
-	const {code = '', studentGroup = ''} = req.body;
-
-	try {
-		const reservations = await openData.CheckOpenDataReservations(
-			code,
-			studentGroup,
-		);
-		res.json(reservations);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Server error');
-	}
-});
-router.post('/create', async (req: Request, res: Response) => {
-	const {
-		courseName,
-		courseCode,
-		studentGroup,
-		startDate,
-		endDate,
-		studentList,
-		instructors,
-		topicGroup,
-		topics,
-	} = req.body;
-
-	try {
-		const response = await courseController.insertIntoCourse(
+		try {
+			const reservations = await openData.CheckOpenDataReservations(
+				code,
+				studentGroup,
+			);
+			res.json(reservations);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Server error');
+		}
+	},
+);
+router.post(
+	'/create',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (req: Request, res: Response) => {
+		const {
 			courseName,
-			startDate,
-			endDate,
 			courseCode,
 			studentGroup,
+			startDate,
+			endDate,
 			studentList,
 			instructors,
-			topics,
 			topicGroup,
-		);
+			topics,
+		} = req.body;
 
-		res.status(200).send({
-			message: 'File uploaded and data logged successfully',
-			courseId: response,
-		});
-	} catch (error) {
-		console.error(error);
-		if (error instanceof Error) {
-			res.status(500).json({
-				message: error.message,
+		try {
+			const response = await courseController.insertIntoCourse(
+				courseName,
+				startDate,
+				endDate,
+				courseCode,
+				studentGroup,
+				studentList,
+				instructors,
+				topics,
+				topicGroup,
+			);
+
+			res.status(200).send({
+				message: 'File uploaded and data logged successfully',
+				courseId: response,
 			});
-		} else {
-			res.status(500).json({
-				message: 'An unknown error occurred',
-			});
+		} catch (error) {
+			console.error(error);
+			if (error instanceof Error) {
+				res.status(500).json({
+					message: error.message,
+				});
+			} else {
+				res.status(500).json({
+					message: 'An unknown error occurred',
+				});
+			}
 		}
-	}
-});
-router.post('/excelinput', upload.single('file'), async (req, res) => {
-	if (!req.file) {
-		console.error('No file uploaded');
-		res.status(400).send('No file uploaded');
-		return;
-	}
-	const {checkCourseDetails, instructorEmail} = req.body;
-	// Read the Excel file from the buffer
-	const workbook = XLSX.read(req.file.buffer, {type: 'buffer'});
-	console.log('Loaded workbook'); // Debugging line
-
-	// Get the first worksheet
-	const worksheetName = workbook.SheetNames[0];
-	const worksheet = workbook.Sheets[worksheetName];
-
-	if (!worksheet) {
-		console.error('Worksheet not found');
-		res.status(500).send('Internal server error');
-		return;
-	}
-	const jsonData = XLSX.utils.sheet_to_json(worksheet);
-	const createCourse = (data: Item[]): CourseDetails => {
-		const fullCourseName = Object.keys(data[0])[0]; // get the first key
-		const [courseName, courseCode] = fullCourseName.split(' (');
-		const studentList = data
-			.filter(item => item.__EMPTY !== 'Etunimi')
-			.map(item => {
-				const last_name = item[fullCourseName];
-				const first_name = item.__EMPTY;
-				const name = item.__EMPTY_1;
-				const email = item.__EMPTY_2;
-				const studentnumber = item.__EMPTY_3;
-				const arrivalgroup = item.__EMPTY_4;
-				const admingroups = item.__EMPTY_5;
-				const program = item.__EMPTY_6;
-				const educationform = item.__EMPTY_7;
-				const registration = item.__EMPTY_8;
-				const evaluation = item.__EMPTY_9;
-
-				return {
-					last_name,
-					first_name,
-					name,
-					email,
-					studentnumber,
-					arrivalgroup,
-					admingroups,
-					program,
-					educationform,
-					registration,
-					evaluation,
-				};
-			});
-
-		return {
-			courseName,
-			courseCode: courseCode.replace('(', '').replace(')', ''),
-			studentList,
-			instructorEmail: '', // default value
-			startDate: new Date(), // default value
-			endDate: new Date(), // default value
-			studentGroup: '', // default value
-		};
-	};
-
-	const courseDetails = createCourse(jsonData as Item[]);
-	courseDetails.instructorEmail = instructorEmail;
-	if (checkCourseDetails === 'true') {
-		const data = (await openData.checkOpenDataRealization(
-			courseDetails.courseCode,
-		)) as IData;
-		// Extract startDate and endDate from data and convert them to Date objects
-		courseDetails.startDate = new Date(data.realizations[0].startDate);
-		courseDetails.endDate = new Date(data.realizations[0].endDate);
-		const studentGroup = data.realizations[0].studentGroups[0];
-		courseDetails.studentGroup = studentGroup ? studentGroup.code : '';
-	} else {
-		courseDetails.studentGroup = 'please enter student group';
-		courseDetails.startDate = new Date();
-		courseDetails.endDate = new Date();
-	}
-	res.send(courseDetails);
-});
-router.get('/instructor/:email', async (req: Request, res: Response) => {
-	try {
-		const courses = await course.getCoursesByInstructorEmail(req.params.email);
-		console.log(courses);
-
-		res.send(courses);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Server error');
-	}
-});
-router.get('/coursesbyid/:id', async (req: Request, res: Response) => {
-	try {
-		const courseId = Number(req.params.id);
-		if (isNaN(courseId)) {
-			res.status(400).send('Invalid course ID');
+	},
+);
+router.post(
+	'/excelinput',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	upload.single('file'),
+	async (req, res) => {
+		if (!req.file) {
+			console.error('No file uploaded');
+			res.status(400).send('No file uploaded');
 			return;
 		}
-		const courses = await course.getCoursesByCourseId(courseId);
-		res.json(courses);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Server error');
-	}
-});
+		const {checkCourseDetails, instructorEmail} = req.body;
+		// Read the Excel file from the buffer
+		const workbook = XLSX.read(req.file.buffer, {type: 'buffer'});
+		console.log('Loaded workbook'); // Debugging line
+
+		// Get the first worksheet
+		const worksheetName = workbook.SheetNames[0];
+		const worksheet = workbook.Sheets[worksheetName];
+
+		if (!worksheet) {
+			console.error('Worksheet not found');
+			res.status(500).send('Internal server error');
+			return;
+		}
+		const jsonData = XLSX.utils.sheet_to_json(worksheet);
+		const createCourse = (data: Item[]): CourseDetails => {
+			const fullCourseName = Object.keys(data[0])[0]; // get the first key
+			const [courseName, courseCode] = fullCourseName.split(' (');
+			const studentList = data
+				.filter(item => item.__EMPTY !== 'Etunimi')
+				.map(item => {
+					const last_name = item[fullCourseName];
+					const first_name = item.__EMPTY;
+					const name = item.__EMPTY_1;
+					const email = item.__EMPTY_2;
+					const studentnumber = item.__EMPTY_3;
+					const arrivalgroup = item.__EMPTY_4;
+					const admingroups = item.__EMPTY_5;
+					const program = item.__EMPTY_6;
+					const educationform = item.__EMPTY_7;
+					const registration = item.__EMPTY_8;
+					const evaluation = item.__EMPTY_9;
+
+					return {
+						last_name,
+						first_name,
+						name,
+						email,
+						studentnumber,
+						arrivalgroup,
+						admingroups,
+						program,
+						educationform,
+						registration,
+						evaluation,
+					};
+				});
+
+			return {
+				courseName,
+				courseCode: courseCode.replace('(', '').replace(')', ''),
+				studentList,
+				instructorEmail: '', // default value
+				startDate: new Date(), // default value
+				endDate: new Date(), // default value
+				studentGroup: '', // default value
+			};
+		};
+
+		const courseDetails = createCourse(jsonData as Item[]);
+		courseDetails.instructorEmail = instructorEmail;
+		if (checkCourseDetails === 'true') {
+			const data = (await openData.checkOpenDataRealization(
+				courseDetails.courseCode,
+			)) as IData;
+			// Extract startDate and endDate from data and convert them to Date objects
+			courseDetails.startDate = new Date(data.realizations[0].startDate);
+			courseDetails.endDate = new Date(data.realizations[0].endDate);
+			const studentGroup = data.realizations[0].studentGroups[0];
+			courseDetails.studentGroup = studentGroup ? studentGroup.code : '';
+		} else {
+			courseDetails.studentGroup = 'please enter student group';
+			courseDetails.startDate = new Date();
+			courseDetails.endDate = new Date();
+		}
+		res.send(courseDetails);
+	},
+);
+router.get(
+	'/instructor/:email',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (req: Request, res: Response) => {
+		try {
+			const courses = await course.getCoursesByInstructorEmail(req.params.email);
+			console.log(courses);
+
+			res.send(courses);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Server error');
+		}
+	},
+);
+router.get(
+	'/coursesbyid/:id',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (req: Request, res: Response) => {
+		try {
+			const courseId = Number(req.params.id);
+			if (isNaN(courseId)) {
+				res.status(400).send('Invalid course ID');
+				return;
+			}
+			const courses = await course.getCoursesByCourseId(courseId);
+			res.json(courses);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Server error');
+		}
+	},
+);
 
 declare module 'express-serve-static-core' {
 	interface Request {
@@ -265,24 +295,28 @@ router.delete(
 		}
 	},
 );
-router.get('/students/:userid', async (req: Request, res: Response) => {
-	// Get the instructor ID from the request
-	const userid = Number(req.params.userid);
+router.get(
+	'/students/:userid',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (req: Request, res: Response) => {
+		// Get the instructor ID from the request
+		const userid = Number(req.params.userid);
 
-	if (isNaN(userid)) {
-		res.status(400).send('Invalid user ID');
-		return;
-	}
+		if (isNaN(userid)) {
+			res.status(400).send('Invalid user ID');
+			return;
+		}
 
-	try {
-		// Get the students for the instructor
-		const students = await usermodel.getStudentsByInstructorId(userid);
-		res.json(students);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Server error');
-	}
-});
+		try {
+			// Get the students for the instructor
+			const students = await usermodel.getStudentsByInstructorId(userid);
+			res.json(students);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Server error');
+		}
+	},
+);
 
 router.put(
 	'/update/:courseid',
