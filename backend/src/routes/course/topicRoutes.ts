da21bd35257863f956c76fdf0a483bef3e3cb3b2
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-12-07 19:48:49 +0200

    Refactor topicRoutes.ts to include role-based access control

--- backend/src/routes/course/topicRoutes.ts
+++ backend/src/routes/course/topicRoutes.ts
@@ -6,27 +6,35 @@ import checkUserRole from '../../utils/checkRole.js';
 
 const router: Router = express.Router();
 
-router.get('/', async (_req: Request, res: Response) => {
-	try {
-		const topicData = await TopicGroup.fetchAllTopicGroupsWithTopics();
+router.get(
+	'/',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (_req: Request, res: Response) => {
+		try {
+			const topicData = await TopicGroup.fetchAllTopicGroupsWithTopics();
 
-		res.send(topicData);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Server error');
-	}
-});
-router.post('/', async (req: Request, res: Response) => {
-	try {
-		const {email} = req.body;
-		const topicGroupData =
-			await TopicGroupController.getAllUserTopicGroupsAndTopics(email);
-		res.send(topicGroupData);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send('Server error: ' + err);
-	}
-});
+			res.send(topicData);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Server error');
+		}
+	},
+);
+router.post(
+	'/',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	async (req: Request, res: Response) => {
+		try {
+			const {email} = req.body;
+			const topicGroupData =
+				await TopicGroupController.getAllUserTopicGroupsAndTopics(email);
+			res.send(topicGroupData);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Server error: ' + err);
+		}
+	},
+);
 router.post(
 	'/update',
 	checkUserRole(['admin', 'counselor', 'teacher']),
