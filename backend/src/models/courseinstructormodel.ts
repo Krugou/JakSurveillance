014e9ffb2954eb69f394fe43f6f1d3c033492459
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-08 22:23:56 +0200

    Add insertCourseInstructor method to courseinstructorsModel

--- backend/src/models/courseinstructormodel.ts
+++ backend/src/models/courseinstructormodel.ts
@@ -3,6 +3,13 @@ import createPool from '../config/createPool.js';
 
 const pool = createPool('ADMIN');
 const courseinstructorsModel = {
+	/**
+	 * Inserts a new course instructor.
+	 *
+	 * @param {number} instructoruserid - The ID of the instructor.
+	 * @param {number} courseId - The ID of the course.
+	 * @returns {Promise<ResultSetHeader>} A promise that resolves with the result of the insertion.
+	 */
 	async insertCourseInstructor(instructoruserid: number, courseId: number) {
 		const [instructorResult] = await pool
 			.promise()
