963c5445b914572fe7f285554271abc720ff0621
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-08 22:02:41 +0200

    Add middleware to check user's role and create HTTP error class

--- backend/src/utils/checkRole.ts
+++ backend/src/utils/checkRole.ts
@@ -1,5 +1,10 @@
 import {NextFunction, Request, Response} from 'express';
-
+/**
+ * Middleware to check if the user's role is authorized.
+ *
+ * @param {string[]} roles - The list of authorized roles.
+ * @returns {Function} Middleware function that checks the user's role.
+ */
 const checkUserRole = (roles: string[]) => {
 	return (req: Request, res: Response, next: NextFunction) => {
 		if (!req.user || !roles.includes(req.user.role)) {
--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -1,12 +1,28 @@
+/**
+ * Class representing an HTTP error.
+ */
 class HttpError extends Error {
 	status: number;
 
+	/**
+	 * Create an HTTP error.
+	 *
+	 * @param {string} message - The error message.
+	 * @param {number} status - The HTTP status code.
+	 */
 	constructor(message: string, status: number) {
 		super(message); // Call the parent constructor with the message parameter
 		this.status = status; // Add the status property
 	}
 }
 
+/**
+ * Create and log an HTTP error.
+ *
+ * @param {string} message - The error message.
+ * @param {number} status - The HTTP status code.
+ * @returns {HttpError} The created HTTP error.
+ */
 const httpError = (message: string, status: number) => {
 	console.log('error used');
 	const err = new HttpError(message, status);
