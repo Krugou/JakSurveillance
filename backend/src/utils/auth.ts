e53165b9c89c481cfc95bf398b9578f628bfbfc5
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-08 21:59:38 +0200

    Add newUsername parameter to authenticate function

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -3,7 +3,14 @@ import jwt from 'jsonwebtoken';
 import passport from 'passport';
 import usermodel from '../models/usermodel.js';
 import {User} from '../types.js';
-
+/**
+ * Authenticates a user and generates a JWT token for them.
+ *
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ * @param {(err?: Error | null) => void} next - The next middleware function.
+ * @param {string} newUsername - The new username for the user.
+ */
 export const authenticate = (
 	req: Request,
 	res: Response,
