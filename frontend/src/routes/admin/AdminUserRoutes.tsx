acc5a70a89dc66dfc4a0be50d2d73f5198edfe95
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-08 11:41:32 +0200

    Remove unused components and routes

--- frontend/src/routes/admin/AdminUserRoutes.tsx
+++ frontend/src/routes/admin/AdminUserRoutes.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import AdminMainView from '../../views/main/admin/AdminMainView';
 import AdminUsers from '../../views/main/admin/AdminUsers';
-import AdminUserDetail from '../../views/main/admin/Users/AdminUserDetail';
 import AdminUserModify from '../../views/main/admin/Users/AdminUserModify';
 
 const AdminUserRoutes: React.FC = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<AdminUsers />} />
-			<Route path="/:userid" element={<AdminUserDetail />} />
 			<Route path="/:userid/modify" element={<AdminUserModify />} />
 			<Route path="*" element={<AdminMainView />} />
 		</Routes>
