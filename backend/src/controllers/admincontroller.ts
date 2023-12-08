0cc6e9b3019b4eb618e6202d99f31f78337a51ee
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-08 21:18:24 +0200

    Add server settings and attendance controller methods

--- backend/src/controllers/admincontroller.ts
+++ backend/src/controllers/admincontroller.ts
@@ -35,6 +35,11 @@ export interface AdminController {
  * @type {AdminController}
  */
 const adminController: AdminController = {
+	/**
+	 * Gets the server settings.
+	 *
+	 * @returns {Promise<any>} A promise that resolves to the server settings.
+	 */
 	async getServerSettings() {
 		try {
 			const serverSettings = await ServerSettingsModel.getServerSettings(pool);
@@ -44,6 +49,15 @@ const adminController: AdminController = {
 			throw error;
 		}
 	},
+	/**
+	 * Updates the server settings.
+	 *
+	 * @param {any} speedofhash - The speed of hash.
+	 * @param {any} leewayspeed - The leeway speed.
+	 * @param {any} timeouttime - The timeout time.
+	 * @param {any} attendancethreshold - The attendance threshold.
+	 * @returns {Promise<any>} A promise that resolves when the server settings have been updated.
+	 */
 	async updateServerSettings(
 		speedofhash,
 		leewayspeed,
