72276ecc11375a54846b459aab31f92bace902c8
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-08 20:53:26 +0200

    Add AdminController interface and adminController object

--- backend/src/controllers/admincontroller.ts
+++ backend/src/controllers/admincontroller.ts
@@ -2,9 +2,25 @@ import createPool from '../config/createPool.js';
 import ServerSettingsModel from '../models/serversettingsmodel.js';
 
 const pool = createPool('ADMIN');
-
+/**
+ * AdminController interface represents the structure of the admin controller.
+ */
 interface AdminController {
+	/**
+	 * Gets the server settings.
+	 *
+	 * @returns {Promise<any>} A promise that resolves to the server settings.
+	 */
 	getServerSettings: () => Promise<any>;
+	/**
+	 * Updates the server settings.
+	 *
+	 * @param {any} speedofhash - The speed of hash.
+	 * @param {any} leewayspeed - The leeway speed.
+	 * @param {any} timeouttime - The timeout time.
+	 * @param {any} attendancethreshold - The attendance threshold.
+	 * @returns {Promise<any>} A promise that resolves when the server settings have been updated.
+	 */
 	updateServerSettings: (
 		speedofhash: any,
 		leewayspeed: any,
@@ -12,7 +28,12 @@ interface AdminController {
 		attendancethreshold: any,
 	) => Promise<any>;
 }
-
+/**
+ * adminController is an object that implements the AdminController interface.
+ * It provides methods to get and update the server settings.
+ *
+ * @type {AdminController}
+ */
 const adminController: AdminController = {
 	async getServerSettings() {
 		try {
