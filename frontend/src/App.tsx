7a59814ad033ec8ee32f660f1525724b1e957cea
Krugou <59641229+Krugou@users.noreply.github.com>
2023-12-11 08:54:49 +0200

    Add service worker update log

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,6 +27,7 @@ const App = () => {
 				console.log('Service worker registered successfully');
 				// Update service worker every hour
 				setInterval(() => {
+					console.log('Updating service worker');
 					r.update();
 				}, intervalMS);
 			} else {
