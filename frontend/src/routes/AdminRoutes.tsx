f123c9574f83ba78fc0ba39ddee4ef281c498284
KaarleH <kaarleh@metropolia.fi>
2023-12-11 16:20:28 +0200

    adminInstructionsDelete

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import AdminHelpVideos from '../views/main/admin/AdminHelpVideos.tsx';
 import AdminMainView from '../views/main/admin/AdminMainView.tsx';
 import AdminProfile from '../views/main/admin/AdminProfile.tsx';
 import AdminStats from '../views/main/admin/AdminStats.tsx';
@@ -23,7 +22,6 @@ const AdminRoutes = () => {
 			<Route path="courses/*" element={<AdminCoursesRoutes />} />
 			<Route path="users/*" element={<AdminUserRoutes />} />
 			<Route path="profile" element={<AdminProfile />} />
-			<Route path="helpvideos" element={<AdminHelpVideos />} />
 			<Route path="settings/*" element={<AdminSettingsRoutes />} />
 			<Route path="*" element={<AdminMainView />} />
 			<Route path="stats" element={<AdminStats />} />
