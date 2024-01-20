72c03eb32a91fd2c93baa10d51f0cd580ba81723
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-20 09:15:23 +0200

    Add AllRoutes component to handle routing
    This commit adds a new file `AllRoutes.tsx` which contains the implementation of the `AllRoutes` component. This component handles the routing logic for different user roles (`student`, `admin`, `counselor`, `teacher`). It also sets the document title based on the current user role. The component is wrapped in a `BackgroundContainer` component and uses the `react-router-dom` library for routing.

---
+++ frontend/src/routes/AllRoutes.tsx
@@ -0,0 +1,41 @@
+import React, {useContext, useEffect} from 'react';
+import {Route, Routes, useLocation} from 'react-router-dom';
+import BackgroundContainer from '../components/main/background/BackgroundContainer';
+import {UserContext} from '../contexts/UserContext';
+import Logout from '../views/Logout';
+import Gdpr from '../views/main/Gdpr';
+import Login from '../views/main/Login';
+import StartView from '../views/main/StartView';
+import AdminRoutes from './AdminRoutes';
+import CounselorRoutes from './CounselorRoutes';
+import StudentRoutes from './StudentRoutes';
+import TeacherRoutes from './TeacherRoutes';
+
+const AllRoutes = () => {
+	const location = useLocation();
+	const {user} = useContext(UserContext);
+
+	// Update document title based on current path
+	useEffect(() => {
+		const title = user ? `JakSec - ${user.role} ` : `JakSec - `;
+		document.title = title;
+	}, [user, location]);
+
+	return (
+		<BackgroundContainer>
+			<Routes>
+				<Route path="/" element={<StartView />} />
+				<Route path="student/*" element={<StudentRoutes />} />
+				<Route path="admin/*" element={<AdminRoutes />} />
+				<Route path="counselor/*" element={<CounselorRoutes />} />
+				<Route path="teacher/*" element={<TeacherRoutes />} />
+				<Route path="logout" element={<Logout />} />
+				<Route path="login" element={<Login />} />
+				<Route path="gdpr" element={<Gdpr />} />
+				<Route path="*" element={<StartView />} />
+			</Routes>
+		</BackgroundContainer>
+	);
+};
+
+export default AllRoutes;
