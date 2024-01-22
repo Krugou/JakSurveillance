bad1e12e0d9179a5c562e19ccd075198141050fb
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-22 23:02:01 +0200

    Add timestamp column to user_feedback table

--- backend/database/feedback.sql
+++ backend/database/feedback.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `user_feedback` (
   `userid` int(11) NOT NULL,
   `topic` varchar(255) NOT NULL,
   `text` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`feedbackId`),
   KEY `userid` (`userid`),
   CONSTRAINT `user_feedback_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
