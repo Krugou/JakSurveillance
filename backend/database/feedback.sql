e7d70e24a3297efc548dd62e3b5b5af68976fc1f
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-22 22:19:30 +0200

    Add user_feedback table to database

---
+++ backend/database/feedback.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `user_feedback` (
+  `feedbackId` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `topic` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  PRIMARY KEY (`feedbackId`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `user_feedback_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
