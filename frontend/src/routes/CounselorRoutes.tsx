625ab3860c636b4e7fab1be413d8d7ae81fbb280
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-23 20:45:22 +0200

    Add new routes for Feedback, CounselorCourseStats, and CounselorHelpVideos

--- frontend/src/routes/CounselorRoutes.tsx
+++ frontend/src/routes/CounselorRoutes.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import Feedback from '../views/main/Feedback.tsx';
+import CounselorCourseStats from '../views/main/counselor/CounselorCourseStats.tsx';
+import CounselorHelpVideos from '../views/main/counselor/CounselorHelpVideos.tsx';
 import CounselorMainView from '../views/main/counselor/CounselorMainView.tsx';
 import CounselorProfile from '../views/main/counselor/CounselorProfile.tsx';
-import CounselorHelpVideos from '../views/main/counselor/CounselorHelpVideos.tsx';
 import CounselorStudentRoutes from './counselor/CounselorStudentRoutes.tsx';
-import CounselorCourseStats from '../views/main/counselor/CounselorCourseStats.tsx';
 /**
  * CounselorRoutes component.
  * This component is responsible for defining the routes for the counselor section of the application.
