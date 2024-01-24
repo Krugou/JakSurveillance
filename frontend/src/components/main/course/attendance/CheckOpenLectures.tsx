2708e295fb083373732c9d565b1f23dad97f9e35
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-24 15:56:07 +0200

    Refactor CheckOpenLectures component to use Card component for open lectures

--- frontend/src/components/main/course/attendance/CheckOpenLectures.tsx
+++ frontend/src/components/main/course/attendance/CheckOpenLectures.tsx
@@ -1,18 +1,19 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
+import Card from '../../../../components/main/cards/Card';
 import {UserContext} from '../../../../contexts/UserContext';
 import apiHooks from '../../../../hooks/ApiHooks';
 
 interface Lecture {
 	lectureid: number;
 	topicname: string;
-	[key: string]: any; // for other properties of lecture
+	code: string;
+	[key: string]: any;
 }
 
 const CheckOpenLectures: React.FC = () => {
 	const {user} = useContext(UserContext);
 	const [openLectures, setOpenLectures] = useState<Lecture[]>([]);
-	const navigate = useNavigate();
 
 	useEffect(() => {
 		const token: string | null = localStorage.getItem('userToken');
@@ -26,6 +27,7 @@ const CheckOpenLectures: React.FC = () => {
 						user.userid,
 						token,
 					);
+					console.log('🚀 ~ fetchOpenLectures ~ lectures:', lectures);
 					setOpenLectures(lectures);
 				} catch (error) {
 					console.error('Failed to fetch open lectures:', error);
@@ -36,26 +38,21 @@ const CheckOpenLectures: React.FC = () => {
 		}
 	}, [user]);
 
-	const handleOpenLecture = (lectureId: number) => {
-		navigate(`/teacher/attendance/${lectureId}`);
-	};
-
 	return (
-		<div className="flex flex-col items-center justify-center space-y-4">
+		<div className="flex flex-row items-center justify-center animate-pulse">
 			{openLectures.map((lecture: Lecture) => (
-				<div
+				<Card
 					key={lecture.lectureid}
-					className="flex flex-col items-center justify-center bg-blue-200 p-4 rounded-lg shadow-md w-full max-w-md"
-				>
-					<h2 className="text-2xl font-bold mb-2">{lecture.topicname}</h2>
-
-					<button
-						onClick={() => handleOpenLecture(lecture.lectureid)}
-						className="mt-4 bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition-colors duration-200"
-					>
-						Click to continue your lecture
-					</button>
-				</div>
+					path={`/teacher/attendance/${lecture.lectureid}`}
+					title={
+						'Lecture open code: ' +
+						lecture.code +
+						' topic: ' +
+						lecture.topicname +
+						' !'
+					}
+					description="Click to continue"
+				/>
 			))}
 		</div>
 	);
