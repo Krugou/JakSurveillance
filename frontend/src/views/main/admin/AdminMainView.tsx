4da6a0e729bf5d3326d8cd2c76859375c8b7078a
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-26 10:26:15 +0200

    Update admin dashboard cards

--- frontend/src/views/main/admin/AdminMainView.tsx
+++ frontend/src/views/main/admin/AdminMainView.tsx
@@ -17,45 +17,49 @@ const AdminMainView: React.FC = () => {
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-fit mr-auto ml-auto p-5 gap-4">
 				<Card
 					path="/teacher/mainview"
-					title="Teacher view"
-					description="Teacher view"
+					title="Teacher Dashboard"
+					description="Access and manage your teaching modules"
 				/>
 				<Card
 					path="/counselor/mainview"
-					title="Counselor view"
-					description="Counselor view"
+					title="Counselor Dashboard"
+					description="Access and manage student counseling services"
 				/>
 				<Card
 					path="/admin/courses/"
-					title="Courses"
-					description="Manage any course"
+					title="Course Management"
+					description="Create, update, and delete courses"
 				/>
-				<Card path="/admin/users/" title="Users" description="Manage any user" />
+				<Card
+					path="/admin/users/"
+					title="User Management"
+					description="Manage user accounts and permissions"
+				/>
 				<Card
 					path="/admin/newuser/"
-					title="New Student User"
-					description="Add new student user"
+					title="Student Registration"
+					description="Register a new student user"
 				/>
 				<Card
 					path="/admin/lectures/"
-					title="Lectures"
-					description="View all lectures to check if they are active or not"
+					title="Lecture Management"
+					description="View and manage all lectures"
 				/>
 				<Card
 					path="/admin/settings/"
-					title="Settings"
-					description="Manage your settings"
+					title="Server Configuration"
+					description="Manage server settings and configurations"
 				/>
 
 				<Card
 					path="/admin/stats/"
 					title="Statistics"
-					description="See statistics"
+					description="View detailed statistics"
 				/>
 				<Card
 					path="/admin/feedback/"
-					title="Feedback"
-					description="See all feedback"
+					title="User Feedback"
+					description="View and manage user feedback"
 				/>
 			</div>
 		</>
