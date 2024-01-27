c3b9987c967eaea58f418e5d046f906f3f44b039
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-27 15:46:50 +0200

    Refactor AdminStats component to use useEffect and separate data fetching

--- frontend/src/views/main/admin/AdminStats.tsx
+++ frontend/src/views/main/admin/AdminStats.tsx
@@ -1,4 +1,3 @@
-import {Button, Stack} from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 
 import {
@@ -10,10 +9,9 @@ import {
 	Title,
 	Tooltip,
 } from 'chart.js';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Bar} from 'react-chartjs-2';
 import apiHooks from '../../../hooks/ApiHooks';
-
 ChartJS.register(
 	CategoryScale,
 	LinearScale,
@@ -39,103 +37,68 @@ const options = {
  *
  * @returns {JSX.Element} The rendered AdminStats component.
  */
+
 const AdminStats = () => {
-	type ChartData = {
-		labels: string[];
-		datasets: {
-			label: string;
-			data: number[];
-			backgroundColor: string;
-			hidden: boolean;
-		}[];
-	};
+	const [userData, setUserData] = useState(null);
+	const [lectureData, setLectureData] = useState(null);
 
-	const [chartData, setChartData] = useState<ChartData>({
-		labels: [],
-		datasets: [],
-	});
-	const [loading, setLoading] = useState(false);
-
-	const fetchUserData = async () => {
-		setLoading(true);
-		const token: string | null = localStorage.getItem('userToken');
-		if (!token) {
-			throw new Error('No token available');
-		}
-		const rows = await apiHooks.getRoleCounts(token);
-
-		const roleNames = rows.map(row => row.role_name);
-		const userCounts = rows.map(row => row.user_count);
-
-		setChartData({
-			labels: roleNames,
-			datasets: [
-				{
-					label: 'User Counts',
-					data: userCounts,
-					backgroundColor: 'rgba(75, 192, 192, 0.6)',
-					hidden: false,
-				},
-			],
-		});
-
-		setLoading(false);
-	};
-
-	const fetchLectureAttendanceData = async () => {
-		setLoading(true);
-		const token: string | null = localStorage.getItem('userToken');
-		if (!token) {
-			throw new Error('No token available');
-		}
-		const lectureAttendanceCounts = await apiHooks.getLectureAndAttendanceCount(
-			token,
-		);
-
-		const labels = Object.keys(lectureAttendanceCounts).map(label => {
-			if (label === 'notattended') {
-				return 'Not Attended';
+	useEffect(() => {
+		const fetchData = async () => {
+			const token: string | null = localStorage.getItem('userToken');
+			if (!token) {
+				throw new Error('No token available');
 			}
-			return label.charAt(0).toUpperCase() + label.slice(1);
-		});
-		const data = Object.values(lectureAttendanceCounts) as number[];
 
-		setChartData({
-			labels: labels,
-			datasets: [
-				{
-					label: 'Counts',
-					data: data,
-					backgroundColor: 'rgba(255, 25, 2, 0.6)',
-					hidden: false,
-				},
-			],
-		});
+			const rows = await apiHooks.getRoleCounts(token);
+			const roleNames = rows.map(row => row.role_name);
+			const userCounts = rows.map(row => row.user_count);
+			setUserData({
+				labels: roleNames,
+				datasets: [
+					{
+						label: 'User Counts',
+						data: userCounts,
+						backgroundColor: 'rgba(75, 192, 192, 0.6)',
+						hidden: false,
+					},
+				],
+			});
 
-		setLoading(false);
-	};
+			const lectureAttendanceCounts = await apiHooks.getLectureAndAttendanceCount(
+				token,
+			);
+			const labels = Object.keys(lectureAttendanceCounts).map(label => {
+				if (label === 'notattended') {
+					return 'Not Attended';
+				}
+				return label.charAt(0).toUpperCase() + label.slice(1);
+			});
+			const data = Object.values(lectureAttendanceCounts) as number[];
+			setLectureData({
+				labels: labels,
+				datasets: [
+					{
+						label: 'Counts',
+						data: data,
+						backgroundColor: 'rgba(255, 25, 2, 0.6)',
+						hidden: false,
+					},
+				],
+			});
+		};
 
-	if (loading) {
+		fetchData();
+	}, []);
+
+	if (!userData || !lectureData) {
 		return <CircularProgress />;
 	}
+
 	return (
 		<div className="flex flex-col items-center justify-center bg-white p-5 w-1/2">
 			<h2 className="mb-4 text-xl">Admin Stats</h2>
-			<div className="flex">
-				<Stack direction="row" spacing={2}>
-					<Button variant="contained" color="primary" onClick={fetchUserData}>
-						Show User Count
-					</Button>
-					<Button
-						variant="contained"
-						color="primary"
-						onClick={fetchLectureAttendanceData}
-					>
-						Show Lecture and Attendance Count
-					</Button>
-				</Stack>
-			</div>
-			<Bar options={options} data={chartData} />
+			<Bar options={options} data={userData} />
+			<Bar options={options} data={lectureData} />
 		</div>
 	);
 };
