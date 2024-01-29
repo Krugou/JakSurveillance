c273717baca023fe25356cae1af6b2edc6b47ddd
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-29 23:51:45 +0200

    Add TeacherLateEnrollment component

---
+++ frontend/src/views/main/teacher/Students/TeacherLateEnrollment.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewStudentUser from '../../../../components/main/NewStudentUser';
+
+const TeacherLateEnrollment: React.FC = () => {
+	return (
+		<div>
+			<NewStudentUser />
+		</div>
+	);
+};
+
+export default TeacherLateEnrollment;
