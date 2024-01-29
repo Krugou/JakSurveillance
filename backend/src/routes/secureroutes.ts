dccdfddc495283c5554a2655c6a78be9ff97547a
Krugou <59641229+Krugou@users.noreply.github.com>
2024-01-29 23:48:40 +0200

    Add student group and user course routes

--- backend/src/routes/secureroutes.ts
+++ backend/src/routes/secureroutes.ts
@@ -1,10 +1,14 @@
 import express, {Request, Response, Router} from 'express';
+import {param} from 'express-validator';
 import createPool from '../config/createPool.js';
 import serverSettingsModel from '../models/serversettingsmodel.js';
+import studentgroupmodel from '../models/studentgroupmodel.js';
+import usercoursesModel from '../models/usercoursemodel.js';
 import usermodel from '../models/usermodel.js';
 import checkUserRole from '../utils/checkRole.js';
-import {param} from 'express-validator';
 import validate from '../utils/validate.js';
+
+import {body} from 'express-validator';
 const pool = createPool('ADMIN');
 /**
  * Router for secure routes.
@@ -81,6 +85,93 @@ router.get(
 			} else {
 				res.json({exists: false});
 			}
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({message: 'Internal server error'});
+		}
+	},
+);
+/**
+ * Route that fetches all student groups.
+ *
+ * @returns {Promise<StudentGroup[]>} A promise that resolves with all student groups.
+ */
+router.get(
+	'/studentgroups',
+	checkUserRole(['admin', 'teacher', 'counselor']),
+	async (_req: Request, res: Response) => {
+		try {
+			const groups = await studentgroupmodel.fetchAllStudentGroups();
+			res.send(groups);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({message: 'Internal server error'});
+		}
+	},
+);
+router.post(
+	'/insert-student-user-course/',
+	checkUserRole(['admin', 'counselor', 'teacher']),
+	[
+		body('email').isEmail().withMessage('Email must be valid'),
+		body('first_name').isString().withMessage('First name must be a string'),
+		body('last_name').isString().withMessage('Last name must be a string'),
+		body('studentnumber')
+			.isString()
+			.withMessage('Student number must be a string'),
+	],
+	validate,
+	async (req: Request, res: Response) => {
+		console.log('🚀 ~ req:', req);
+		const {
+			email,
+			first_name,
+			last_name,
+			studentnumber,
+			studentGroupId,
+			courseId,
+		} = req.body;
+
+		try {
+			const existingUserByNumber =
+				await usermodel.checkIfUserExistsByStudentNumber(studentnumber);
+			if (existingUserByNumber.length > 0) {
+				res
+					.status(400)
+					.json({message: 'User with this student number already exists'});
+				return;
+			}
+
+			const existingUserByEmail = await usermodel.checkIfUserExistsByEmail(email);
+			if (existingUserByEmail.length > 0) {
+				res.status(400).json({message: 'User with this email already exists'});
+				return;
+			}
+
+			const userResult = await usermodel.insertStudentUser(
+				email,
+				first_name,
+				last_name,
+				studentnumber,
+				studentGroupId,
+			);
+
+			// Check if course connection exists
+			const existingUserCourse = await usercoursesModel.checkIfUserCourseExists(
+				userResult.insertId,
+				courseId,
+			);
+			if (existingUserCourse.length === 0) {
+				// If course connection does not exist, add it
+				await usercoursesModel.insertUserCourse(userResult.insertId, courseId);
+			}
+
+			res
+				.status(200)
+				.send({message: 'Student user inserted successfully', userResult});
+			console.log(
+				`Student user successfully inserted. Email: ${email}, Student Number: ${studentnumber}`,
+			);
 		} catch (error) {
 			console.error(error);
 			res.status(500).json({message: 'Internal server error'});
