e23a3d48cd7f71e655f9fa9ec859ab38328032eb
Krugou <59641229+Krugou@users.noreply.github.com>
2024-02-04 16:59:15 +0200

    Update team member name formatting

--- frontend/src/views/main/Team.tsx
+++ frontend/src/views/main/Team.tsx
@@ -4,7 +4,7 @@ const Team: React.FC = () => {
 	return (
 		<div className="flex flex-col m-4 p-4 rounded-md items-center justify-between bg-white h-1/2">
 			<h1 className="text-4xl mb-4">Development Team</h1>
-			<h1 className="text-3xl mb-4">Joonas Aleksi Kaarle</h1>
+			<h1 className="text-3xl mb-4">Joonas. Aleksi. Kaarle.</h1>
 			<h2 className="text-2xl mb-4">JAKSEC</h2>
 		</div>
 	);
