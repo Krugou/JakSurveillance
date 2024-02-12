32d9757e9dc381457fb2b683337493a76592513c
Krugou <59641229+Krugou@users.noreply.github.com>
2024-02-12 23:00:42 +0200

    Refactor AdminStats component to fetch user and attendance statistics***

--- frontend/src/views/main/admin/AdminStats.tsx
+++ frontend/src/views/main/admin/AdminStats.tsx
@@ -41,74 +41,107 @@ interface LectureAttendanceCount {
 }
 
 const AdminStats = () => {
-	const [userData, setUserData] = useState<{
+	const [userStatistics, setUserStatistics] = useState<{
 		labels: string[];
 		datasets: ChartDataset<'bar', number[]>[];
 	} | null>(null);
+	const [attendanceStatistics, setAttendanceStatistics] = useState<
+		number[] | null
+	>(null);
+	const [lectureStatistics, setLectureStatistics] = useState<{
+		labels: string[];
+		datasets: ChartDataset<'bar', number[]>[];
+	} | null>(null);
+	const [error, setError] = useState<string | null>(null);
 
-	const [lectureData, setLectureData] = useState<{
-		labels: string[];
-		datasets: ChartDataset<'bar', number[]>[];
-	} | null>(null);
+	const fetchUserStatistics = async (token: string) => {
+		const roleCounts: RoleCount[] = await apiHooks.getRoleCounts(token);
+		const roleNames = roleCounts.map(row => row.role_name);
+		const userCounts = roleCounts.map(row => row.user_count);
+		setUserStatistics({
+			labels: roleNames,
+			datasets: [
+				{
+					label: 'User Counts',
+					data: userCounts,
+					backgroundColor: 'rgba(75, 192, 192, 0.6)',
+					hidden: false,
+				},
+			],
+		});
+	};
+
+	const fetchLectureStatistics = async (token: string) => {
+		const lectureAttendanceCounts: LectureAttendanceCount =
+			await apiHooks.getLectureAndAttendanceCount(token);
+		const labels = Object.keys(lectureAttendanceCounts).map(label => {
+			if (label === 'notattended') {
+				return 'Not Attended';
+			}
+			return label.charAt(0).toUpperCase() + label.slice(1);
+		});
+		const data = Object.values(lectureAttendanceCounts) as number[];
+		setAttendanceStatistics(data);
+		setLectureStatistics({
+			labels: labels,
+			datasets: [
+				{
+					label: 'Attendance Counts',
+					data: data,
+					backgroundColor: 'rgba(255, 25, 2, 0.6)',
+					hidden: false,
+				},
+			],
+		});
+	};
 
 	useEffect(() => {
 		const fetchData = async () => {
 			const token: string | null = localStorage.getItem('userToken');
 			if (!token) {
-				throw new Error('No token available');
+				setError('No token available');
+				return;
 			}
 
-			const rows: RoleCount[] = await apiHooks.getRoleCounts(token);
-			console.log('🚀 ~ fetchData ~ rows:', rows);
-			const roleNames = rows.map(row => row.role_name);
-			const userCounts = rows.map(row => row.user_count);
-			console.log('🚀 ~ fetchData ~ userCounts:', userCounts);
-			setUserData({
-				labels: roleNames,
-				datasets: [
-					{
-						label: 'User Counts',
-						data: userCounts,
-						backgroundColor: 'rgba(75, 192, 192, 0.6)',
-						hidden: false,
-					},
-				],
-			});
-
-			const lectureAttendanceCounts: LectureAttendanceCount =
-				await apiHooks.getLectureAndAttendanceCount(token);
-			const labels = Object.keys(lectureAttendanceCounts).map(label => {
-				if (label === 'notattended') {
-					return 'Not Attended';
-				}
-				return label.charAt(0).toUpperCase() + label.slice(1);
-			});
-			const data = Object.values(lectureAttendanceCounts) as number[];
-			setLectureData({
-				labels: labels,
-				datasets: [
-					{
-						label: 'Attendance Counts',
-						data: data,
-						backgroundColor: 'rgba(255, 25, 2, 0.6)',
-						hidden: false,
-					},
-				],
-			});
+			try {
+				await fetchUserStatistics(token);
+				await fetchLectureStatistics(token);
+			} catch (error) {
+				setError('Failed to fetch data');
+			}
 		};
 
 		fetchData();
 	}, []);
 
-	if (!userData || !lectureData) {
+	if (error) {
+		return <p>{error}</p>;
+	}
+
+	if (!userStatistics || !lectureStatistics) {
 		return <CircularProgress />;
 	}
 
 	return (
-		<div className="flex flex-col items-center justify-center bg-white p-5 w-1/2">
-			<h2 className="mb-4 text-xl">Admin Stats</h2>
-			<Bar options={options} data={userData} />
-			<Bar options={options} data={lectureData} />
+		<div className="flex flex-col items-center justify-center bg-white p-5 md:w-1/2 w-full">
+			<h2 className="mb-4 text-2xl">Administrator Statistics</h2>
+			<h2 className="mb-4 text-xl">User Statistics</h2>
+			<div className="w-full">
+				<Bar options={options} data={userStatistics} />
+			</div>
+			<h2 className="mb-4 text-xl">Attendance Statistics</h2>
+			{attendanceStatistics && (
+				<p>
+					{`Total lectures: ${attendanceStatistics[0]}. Attendance ratio: ${(
+						(attendanceStatistics[2] /
+							(attendanceStatistics[2] + attendanceStatistics[1])) *
+						100
+					).toFixed(2)}%`}
+				</p>
+			)}
+			<div className="w-full">
+				<Bar options={options} data={lectureStatistics} />
+			</div>
 		</div>
 	);
 };
