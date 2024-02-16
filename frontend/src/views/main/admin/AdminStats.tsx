633125a942a8bd7ade086b2bc13dc832347db572
Krugou <59641229+Krugou@users.noreply.github.com>
2024-02-16 10:57:13 +0200

    Add lecture data fetching and display in AdminStats

--- frontend/src/views/main/admin/AdminStats.tsx
+++ frontend/src/views/main/admin/AdminStats.tsx
@@ -11,6 +11,8 @@ import {
 } from 'chart.js';
 import React, {useEffect, useState} from 'react';
 import {Bar} from 'react-chartjs-2';
+import {toast} from 'react-toastify';
+import LecturesByDayChart from '../../../components/main/admin/LecturesByDayChart';
 import apiHooks from '../../../hooks/ApiHooks';
 
 ChartJS.register(
@@ -40,6 +42,20 @@ interface LectureAttendanceCount {
 	[key: string]: number;
 }
 
+interface Lecture {
+	lectureid: number;
+	start_date: string;
+	attended: number;
+	notattended: number;
+	teacheremail: string;
+	timeofday: string;
+	coursename: string;
+	state: string;
+	topicname: string;
+	coursecode: string;
+	courseid: string;
+	actualStudentCount: number;
+}
 const AdminStats = () => {
 	const [userStatistics, setUserStatistics] = useState<{
 		labels: string[];
@@ -79,7 +95,33 @@ const AdminStats = () => {
 			],
 		});
 	};
+	const [lectures, setLectures] = useState<Lecture[] | null>(null);
 
+	const getLectures = async () => {
+		const token: string | null = localStorage.getItem('userToken');
+		if (!token) {
+			toast.error('No token available');
+			return false;
+		}
+		try {
+			const result = await apiHooks.fetchAllLectures(token);
+			if (!Array.isArray(result)) {
+				toast.error('Expected an array from fetchAllLectures');
+				return false;
+			}
+
+			setLectures(result);
+
+			return true;
+		} catch (error) {
+			toast.error('Error fetching lectures');
+			return false;
+		}
+	};
+
+	useEffect(() => {
+		getLectures();
+	}, []);
 	const fetchLectureStatistics = async (token: string) => {
 		const lectureAttendanceCounts: LectureAttendanceCount =
 			await apiHooks.getLectureAndAttendanceCount(token);
@@ -132,32 +174,40 @@ const AdminStats = () => {
 	}
 
 	return (
-		<div className="flex flex-col items-center justify-center bg-white p-5 w-full">
-			<h2 className="mb-4 text-2xl md:text-3xl">Administrator Statistics</h2>
-			<div className="flex flex-col xl:flex-row w-full">
-				<div className="w-full justify-start mx-4">
-					<h2 className="mb-4 text-xl md:text-2xl">User Statistics</h2>
-					<p className="text-sm md:text-base">{`Percentage of students who have logged in at least once: ${userStatisticsPercentage.toFixed(
-						2,
-					)}%`}</p>
-					<div className="w-full">
-						<Bar options={options} data={userStatistics} />
-					</div>
+		<div className="grid grid-cols-1 xl:grid-cols-2 gap-4 bg-white p-5 w-full">
+			<h2 className="mb-4 text-2xl md:text-3xl col-span-full">
+				Administrator Statistics
+			</h2>
+			<div className="w-full justify-start mx-4">
+				<h2 className="mb-4 text-xl md:text-2xl">User Statistics</h2>
+				<p className="text-sm md:text-base">{`Percentage of students who have logged in at least once: ${userStatisticsPercentage.toFixed(
+					2,
+				)}%`}</p>
+				<div className="w-full">
+					<Bar options={options} data={userStatistics} />
 				</div>
-				<div className="w-full justify-start mx-4">
-					<h2 className="mb-4 text-xl md:text-2xl">Attendance Statistics</h2>
-					{attendanceStatistics && (
-						<p className="text-sm md:text-base">
-							{`Total lectures: ${attendanceStatistics[0]}. Attendance ratio: ${(
-								(attendanceStatistics[2] /
-									(attendanceStatistics[2] + attendanceStatistics[1])) *
-								100
-							).toFixed(2)}%`}
-						</p>
-					)}
-					<div className="w-full">
-						<Bar options={options} data={lectureStatistics} />
-					</div>
+			</div>
+			<div className="w-full justify-start mx-4">
+				<h2 className="mb-4 text-xl md:text-2xl">Attendance Statistics</h2>
+				{attendanceStatistics && (
+					<p className="text-sm md:text-base">
+						{`Total lectures: ${attendanceStatistics[0]}. Attendance ratio: ${(
+							(attendanceStatistics[2] /
+								(attendanceStatistics[2] + attendanceStatistics[1])) *
+							100
+						).toFixed(2)}%`}
+					</p>
+				)}
+				<div className="w-full">
+					<Bar options={options} data={lectureStatistics} />
+				</div>
+			</div>
+			<div className="w-full justify-start mx-4">
+				<h2 className="mb-4 text-xl md:text-2xl">
+					Weekly Distribution of Saved Lectures Count by Day
+				</h2>
+				<div className="w-full">
+					<LecturesByDayChart lectures={lectures} />
 				</div>
 			</div>
 		</div>
